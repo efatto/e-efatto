be6dab13dc22217af9d6c7da7ff7aac508b804e0
sergio <sergiocorato@gmail.com>
2017-08-16 12:33:17 +0200

    [IMP] add product template listprice zero default

---
+++ product_template_listprice_zero_default/models/product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    list_price = fields.Float(default=0)
