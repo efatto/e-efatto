8cd9d80de87770424d57efd1ead40a5861f29e0e
sergio <sergiocorato@gmail.com>
2018-08-12 10:12:13 +0200

    [10.0] port web view usability

--- web_view_usability/__manifest__.py
+++ web_view_usability/__manifest__.py
@@ -19,7 +19,7 @@
 #
 {
     'name': 'Web view usability',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'other',
     'author': 'Sergio Corato',
     'description': """
@@ -29,9 +29,10 @@ Web view usability
 Usability issue resolved on web view: 
     * enlarge pop-up view,
     * enlarge open button,
-    * enlarge drop-down button.
+    * enlarge drop-down button,
+    * block and trasparency of column headers.
     """,
-    'website': 'http://www.efatto.it',
+    'website': 'https//efatto.it',
     'license': 'AGPL-3',
     'depends': [
         'web',
@@ -39,5 +40,5 @@ Usability issue resolved on web view:
     'data': [
         'views/web.xml',
     ],
-    'installable': False
+    'installable': True
 }
