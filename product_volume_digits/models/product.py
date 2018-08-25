76677077cb044bfcbd09795ec606bf8244d643ba
sergio <sergiocorato@gmail.com>
2018-08-25 14:58:21 +0200

    [IMP] add module for product volume digits splitted from other module

---
+++ product_volume_digits/models/product.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from odoo import models, fields
+import odoo.addons.decimal_precision as dp
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    volume = fields.Float(
+        digits_compute=dp.get_precision('Stock Volume'),
+        digits=dp.get_precision('Stock Volume'),
+    )
