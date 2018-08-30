416677425cf450e672fe4e9b28415f3035f113e3
sergio <sergiocorato@gmail.com>
2018-08-31 00:15:12 +0200

    [9.0] port balance line progressive

--- account_balance_line_progressive/models/account_move_line.py
+++ account_balance_line_progressive/models/account_move_line.py
@@ -2,7 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from odoo import models, fields, api
+from openerp import models, fields, api
 
 
 class AccountMoveLine(models.Model):
