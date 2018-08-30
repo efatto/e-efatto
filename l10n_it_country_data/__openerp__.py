3430c85c084c72f503e04192e9ededf586824b51
sergio <sergiocorato@gmail.com>
2018-08-31 00:28:37 +0200

    [9.0] port country data

--- l10n_it_country_data/__openerp__.py
+++ l10n_it_country_data/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Italy - Country data',
-    'version': '10.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'other',
     'description': """
     With this module Italian country data will be displayed as usual in Italy.
