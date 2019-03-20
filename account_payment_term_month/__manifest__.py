70f8f98c3dc9b5a565b29cc60744a7a8f8c3699c
sergiocorato <sergiocorato@gmail.com>
2019-03-20 10:14:16 +0100

    [FIX] manifest

--- account_payment_term_month/__manifest__.py
+++ account_payment_term_month/__manifest__.py
@@ -1,29 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
-#    Copyright (C) 2014 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#    Copyright (C) 2014 Didotech SRL
-#    (<http://www.didotech.com>)
-#    Copyright (C) 2015 SimplERP Srl
-#    Copyright (C) 2016-2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+# Copyright 2014 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2014 Didotech SRL (<http://www.didotech.com>)
+# Copyright 2015 SimplERP Srl
+# Copyright 2016-2018-2019 Sergio Corato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Payment terms - Commercial month',
     'version': '10.0.1.0.0',
