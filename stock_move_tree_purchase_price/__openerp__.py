fb6296af95a535f4815e1f4e049f6c0588b92417
sergiocorato <sergiocorato@gmail.com>
2019-03-22 11:32:32 +0100

    [FIX] manifest

--- stock_move_tree_purchase_price/__openerp__.py
+++ stock_move_tree_purchase_price/__openerp__.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Copyright (C) 2018 Sergio Corato
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2018 Sergio Corato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Stock move tree purchase net price',
     'version': '8.0.1.0.0',
@@ -25,7 +9,7 @@
     Add purchase net price to stock move tree
     """,
     'author': 'Sergio Corato',
-    'website': 'http://www.efatto.it',
+    'website': 'https://efatto.it',
     'license': 'AGPL-3',
     "depends": [
         'purchase_order_price_unit_net',
