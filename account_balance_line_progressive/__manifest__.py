3fc53d3131184690af31865d2ab9b2ae17751229
sergiocorato <sergiocorato@gmail.com>
2019-08-12 10:45:04 +0200

    [FIX] license

--- account_balance_line_progressive/__manifest__.py
+++ account_balance_line_progressive/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016-2019 Sergio Corato
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'Account balance line progressive',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'category': 'Account',
     'description': 'View balance in account line tree. '
     'Instead of module account_balance_line, wich show the balance only of the'
     ' single line, it compute progressive balance for the account selected.',
     'author': 'Sergio Corato',
     'website': 'https://efatto.it',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'depends': [
         'account',
     ],
