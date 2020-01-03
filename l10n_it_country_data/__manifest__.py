bee9db6429e62ff6c09cc180e40e141eb7704049
sergiocorato <sergiocorato@gmail.com>
2020-01-03 09:08:56 +0100

    index on 12.0: 98c4556 [UPD]

---
+++ l10n_it_country_data/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2018-2019 Sergio Corato.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Italy - Country data",
+    "version": "10.0.1.0.2",
+    "category": "other",
+    "description": """
+    With this module Italian country data will be displayed as usual in Italy.
+    """,
+    "author": "Sergio Corato",
+    "website": "https://efatto.it",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "data/res_country_data.xml",
+    ],
+    "installable": True,
+}
