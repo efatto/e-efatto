c23061ebb07634e218120da2e76dcd3af767a494
sergiocorato <sergiocorato@gmail.com>
2020-01-09 22:48:36 +0100

    [CLEAN]

--- l10n_it_account_invoice_statement/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import account_tax_code
-from . import res_partner
-from . import invoice_statement_attachment
-from . import invoice_statement
