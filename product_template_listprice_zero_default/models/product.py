f40a635d25346b1a4d0043f1a6edc3876bdc1e47
sergiocorato <sergiocorato@gmail.com>
2020-02-24 16:41:52 +0100

    [PORT] product template default

--- product_template_listprice_zero_default/models/product.py
+++ product_template_listprice_zero_default/models/product.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
 from odoo import models, fields
 
 
