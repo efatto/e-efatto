c8b4e6fb38c4cd2089720d6cdbf30b62bce52142
sergiocorato <sergiocorato@gmail.com>
2020-02-24 16:39:19 +0100

    index on 12.0: 50348b5 [IMP] setup

---
+++ product_template_listprice_zero_default/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2018-2019 Sergio Corato
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Product template default list price zero',
+    'version': '10.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Product template default list price zero',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'data': [
+    ],
+    'installable': True
+}
