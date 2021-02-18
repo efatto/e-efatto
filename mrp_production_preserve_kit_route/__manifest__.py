2a5223ce60159af5e12b03c4ddffc62cc392b967
sergiocorato <sergiocorato@gmail.com>
2021-02-18 22:57:52 +0100

    [IMP] move module from OCA PR

---
+++ mrp_production_preserve_kit_route/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'MRP Preserve route for kit components',
+    'version': '12.0.1.0.1',
+    'development_status': 'Beta',
+    'license': 'AGPL-3',
+    'category': 'Manufacturing',
+    'author': 'Sergio Corato Efatto.it',
+    'website': 'https://efatto.it',
+    'depends': [
+        'mrp',
+        'purchase_stock',
+    ],
+    'installable': True,
+}
