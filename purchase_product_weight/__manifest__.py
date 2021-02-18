caccf1131bda54182ee3a4fc4b56074430007ea9
sergiocorato <sergiocorato@gmail.com>
2021-02-18 23:06:55 +0100

    [IMP] move from OCA PR

---
+++ purchase_product_weight/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Purchase product on weight',
+    'version': '12.0.1.0.1',
+    'category': 'Purchase',
+    'license': 'AGPL-3',
+    'author': 'Sergio Corato Efatto.it',
+    'website': 'https://efatto.it',
+    'maintainers': ['sergiocorato'],
+    'depends': [
+        'purchase',
+        'stock',
+    ],
+    'data': [
+        'views/purchase.xml',
+        'views/product.xml',
+        'views/product_supplierinfo.xml',
+    ],
+    'installable': True,
+}
