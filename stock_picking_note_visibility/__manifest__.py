61b739e0cb5e3c1b341cd2a9e6d18776f4b737dd
sergiocorato <sergiocorato@gmail.com>
2021-05-07 09:42:40 +0200

    [ADD] stock picking note visibility

---
+++ stock_picking_note_visibility/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock picking note visibility',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'other',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    'description': "Show note after origin instead in dedicated tab",
+    'installable': True,
+}
