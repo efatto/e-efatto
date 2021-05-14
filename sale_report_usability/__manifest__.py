046ac0fe63a221ef16ea95e0d1fb4d339a02ad97
sergiocorato <sergiocorato@gmail.com>
2021-05-14 17:45:43 +0200

    [ADD] module to compare sale order by date

---
+++ sale_report_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale report usability',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'other',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'report/sale_report.xml',
+    ],
+    'description': "Add generic date to compare",
+    'installable': True,
+}
---
+++ sale_report_usability/report/sale_report.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    generic_date = fields.Date(
+        'Order Date (without year)', readonly=True)
+    generic_confirmation_date = fields.Date(
+        'Confirmation Date (without year)', readonly=True)
+
+    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+        fields = fields or {}
+        fields['generic_date'] = \
+            ", make_date(date_part('year', CURRENT_DATE)::int, " \
+            "date_part('month', date_order)::int, " \
+            "date_part('day', date_order)::int) as generic_date"
+        fields['generic_confirmation_date'] = \
+            ", make_date(date_part('year', CURRENT_DATE)::int, " \
+            "date_part('month', confirmation_date)::int, " \
+            "date_part('day', confirmation_date)::int) as generic_confirmation_date"
+        return super(SaleReport, self)._query(
+            with_clause, fields, groupby, from_clause
+        )
