1d14f679e1c20d31e6aa572359c30166e5c3f63c
sergiocorato <sergiocorato@gmail.com>
2021-05-14 15:26:46 +0200

    [ADD] pricelist usability

---
+++ product_pricelist_usability/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product pricelist usability',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'other',
+    'depends': [
+        'product',
+    ],
+    'data': [
+    ],
+    'description': "Show complete name in pricelist item form",
+    'installable': True,
+}
---
+++ product_pricelist_usability/models/product_pricelist.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models, fields, api, _
+
+
+class PricelistItem(models.Model):
+    _inherit = "product.pricelist.item"
+
+    @api.one
+    @api.depends(
+        'categ_id', 'product_tmpl_id', 'product_id', 'compute_price', 'fixed_price',
+        'pricelist_id', 'percent_price', 'price_discount', 'price_surcharge')
+    def _get_pricelist_item_name_price(self):
+        super()._get_pricelist_item_name_price()
+        if self.categ_id:
+            self.name = self.categ_id.complete_name
