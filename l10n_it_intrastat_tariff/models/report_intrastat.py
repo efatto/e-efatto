fb32f3be57beea5175f2ea6ff225de74fb4a2ba8
sergiocorato <sergiocorato@gmail.com>
2021-05-17 10:55:06 +0200

    [ADD] customs tariff

---
+++ l10n_it_intrastat_tariff/models/report_intrastat.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class ReportIntrastatCode(models.Model):
+    _inherit = "report.intrastat.code"
+
+    tariff_id = fields.Many2one(
+        comodel_name="report.intrastat.tariff",
+        string="Customs Tariff")
+
+
+class ReportIntrastatTariff(models.Model):
+    _name = "report.intrastat.tariff"
+    _description = "Intrastat tariff"
+
+    name = fields.Char()
+    active = fields.Boolean(default=True)
+    tariff_percentage = fields.Float()
+    tariff_fixed_value = fields.Float()
