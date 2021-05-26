df7ff8edadaf572babff86b45359950349cea269
sergiocorato <sergiocorato@gmail.com>
2021-05-26 10:40:04 +0200

    [WIP] add replenishment cost for product

---
+++ product_managed_replenishment_cost/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product Managed Replenishment Cost',
+    'description': "Add costs from Customs Tariff and Country Group of Seller",
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'Products',
+    'depends': [
+        'l10n_it_intrastat_tariff',
+        'product',
+        'res_country_logistic_charge',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/replenishment_cost_security.xml',
+        'views/product_product.xml',
+        'views/product_template.xml',
+        'views/replenishment_menu.xml',
+    ],
+    'installable': True,
+}
