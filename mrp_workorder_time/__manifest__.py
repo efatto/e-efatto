01a074abe83da83c8eda895c97a29acd5a2af650
sergiocorato <sergiocorato@gmail.com>
2021-06-03 11:13:11 +0200

    [IMP] add module to insert worked time in work order

---
+++ mrp_workorder_time/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'MRP Workcenter Productivity Time',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Manufacturing',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'description': 'Add facility to set worked time on workorder.',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_workorder.xml',
+    ],
+    'installable': True,
+}
