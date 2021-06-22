6fbf78f99ca843da16f4cd56cca1f669d5fecfdd
sergiocorato <sergiocorato@gmail.com>
2021-06-22 09:37:46 +0200

    [ADD] report timesheet productivity

---
+++ hr_timesheet_productivity/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "Timesheets/productivity reporting",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Manufacturing',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'description': 'Add view of timesheet combined with productivity times.',
+    'depends': [
+        'hr_timesheet',
+        'mrp_employee_productivity'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/hr_timesheet_productivity_report_view.xml',
+    ],
+    'installable': True,
+}
