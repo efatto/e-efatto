a9c734f30eb74c23442e881db9e83c39838b3200
sergiocorato <sergiocorato@gmail.com>
2021-07-05 17:39:11 +0200

    [IMP] stock product deviation

--- mrp_production_deviation/report/mrp_production_deviation_report.py
+++ mrp_production_deviation/report/mrp_production_deviation_report.py
@@ -16,6 +16,10 @@ class MrpProductionDeviationReport(models.Model):
     duration_expected = fields.Float('Expected Duration', readonly=True)
     duration = fields.Float('Duration', readonly=True)
     duration_deviation = fields.Float('Duration Deviation', readonly=True)
+    product_id = fields.Many2one('product.product', readonly=True)
+    quantity_expected = fields.Float('Qty Expected', readonly=True)
+    product_qty = fields.Float('Qty', readonly=True)
+    quantity_deviation = fields.Float('Qty Deviation', readonly=True)
     # expected_cost = fields.Float(string="Previsional Cost")
     # cost = fields.Float(string="Cost")
 
@@ -24,17 +28,49 @@ class MrpProductionDeviationReport(models.Model):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self._cr.execute("""CREATE OR REPLACE VIEW %s AS (
             SELECT
-                max(w.id) AS id,
+                MIN(id) AS id,
+                sub.date,
+                sub.production_id,
+                sub.product_id,
+                coalesce(sum(sub.duration_expected), 0) AS duration_expected,
+                coalesce(sum(sub.duration), 0) AS duration,
+                coalesce(sum(sub.duration), 0) - 
+                    coalesce(sum(sub.duration_expected), 0) AS duration_deviation,
+                coalesce(SUM(sub.quantity_expected), 0) AS quantity_expected,
+                coalesce(SUM(sub.product_qty), 0) AS product_qty,
+                coalesce(sum(sub.product_qty), 0) - 
+                    coalesce(sum(sub.quantity_expected), 0) AS quantity_deviation
+            FROM (
+            SELECT
+                w.id AS id,
                 p.name,
                 to_char(p.date_planned_start, 'YYYY-MM-DD') AS date,
-                w.production_id,
-                coalesce(sum(w.duration_expected), 0) AS duration_expected,
-                coalesce(sum(w.duration), 0) AS duration,
-                coalesce(sum(w.duration), 0) - 
-                    coalesce(sum(w.duration_expected), 0) AS duration_deviation
+                p.id AS production_id,
+                NULL AS product_id,
+                0 AS quantity_expected,
+                0 AS product_qty,
+                w.duration_expected,
+                w.duration AS duration
             FROM mrp_workorder w 
                 LEFT JOIN mrp_production p ON w.production_id = p.id
-            GROUP BY p.name, p.date_planned_start, w.production_id
-            ORDER BY p.name
+            UNION
+            SELECT
+                -MIN(s.id) AS id,
+                MIN(s.name),
+                to_char(p.date_planned_start, 'YYYY-MM-DD') AS date,
+                p.id AS production_id,
+                s.product_id,
+                coalesce(SUM(bl.product_qty), 0) AS quantity_expected,
+                coalesce(SUM(sml.qty_done), 0) AS product_qty,
+                0 AS duration_expected,
+                0 AS duration
+            FROM stock_move s
+                LEFT JOIN mrp_bom_line bl ON bl.id = s.bom_line_id
+                LEFT JOIN mrp_production p ON s.raw_material_production_id = p.id
+                LEFT JOIN stock_move_line sml ON sml.move_id = s.id
+            GROUP BY p.date_planned_start, p.id, s.product_id
+            )
+            AS sub
+            GROUP BY date, production_id, product_id
         )
         """ % self._table)
