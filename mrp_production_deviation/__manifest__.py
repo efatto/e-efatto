73b75cb6d41238789eff908a953ef1986d079b69
sergiocorato <sergiocorato@gmail.com>
2021-07-08 09:25:24 +0200

    [FIX] production deviation expected cost

--- mrp_production_deviation/__manifest__.py
+++ mrp_production_deviation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': "Production deviation info",
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'AGPL-3',
     'category': 'Manufacturing',
     'author': 'Sergio Corato',
