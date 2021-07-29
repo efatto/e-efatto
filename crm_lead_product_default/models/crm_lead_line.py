2e1f8a37a2c2ea5194e79b35ee20ebcb10309c91
sergiocorato <sergiocorato@gmail.com>
2021-07-29 15:30:32 +0200

    [IMP] automate code for products created from crm

--- crm_lead_product_default/models/crm_lead_line.py
+++ crm_lead_product_default/models/crm_lead_line.py
@@ -5,6 +5,12 @@ from odoo import api, fields, models
 
 class CrmLeadLine(models.Model):
     _inherit = "crm.lead.line"
+    _order = "sequence,id"
 
-    #todo impostare un codice progressivo sulla base del codice lead
+    sequence = fields.Integer("Sequence", default=1)
+    product_default_code = fields.Char()
 
+    @api.onchange('sequence')
+    def _compute_product_default_code(self):
+        self.product_default_code = '%s-%s' % (
+            self.lead_id.code, self.sequence)
