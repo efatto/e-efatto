df4970fbe93f50c7160c7747a081b74513eb242a
sergiocorato <sergiocorato@gmail.com>
2021-08-06 17:29:06 +0200

    [MOVE] module purchase_order_approved_vendor

---
+++ purchase_order_approved_vendor/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020-2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Approved Vendor",
+    "summary": "Add new states 'RFQ Confirmed' and 'RFQ Sent' in purchase orders.",
+    "version": "12.0.1.0.4",
+    "category": "Purchases",
+    "website": "https://efatto.it",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "purchase",
+        "purchase_order_approved",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+}
