c85dc9a4147a8452240aca685894ac1358a99b2d
sergiocorato <sergiocorato@gmail.com>
2021-09-24 15:41:53 +0200

    [IMP] add module to show picking note in tree

---
+++ stock_picking_note_tree/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock picking note in tree',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'other',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    'description': "Show note in stock picking tree",
+    'installable': True,
+}
