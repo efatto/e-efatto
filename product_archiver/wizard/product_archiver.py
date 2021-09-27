fff2e450597c816a9ce998b71627370da53bd9ff
sergiocorato <sergiocorato@gmail.com>
2021-09-27 11:52:09 +0200

    [ADD] module to archive products

---
+++ product_archiver/wizard/product_archiver.py
@@ -0,0 +1,43 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class ProductArchiver(models.TransientModel):
+    _name = 'product.archiver'
+
+    from_date = fields.Date(string='Inactive from date', required=True)
+
+    @api.multi
+    def archive(self):
+        for wizard in self:
+            from_date = wizard.from_date
+            unavailable_products = self.env['product.product'].search([
+                ('qty_available', '=', 0.0),
+                ('virtual_available', '=', 0.0),
+                ('incoming_qty', '=', 0.0),
+                ('outgoing_qty', '=', 0.0),
+            ])
+            # search moved product after from_date to exclude them
+            stock_moved_products = self.env['stock.move.line'].search([
+                ('date', '>=', from_date),
+                ('product_id', 'in', unavailable_products.ids),
+            ])
+            moved_product_ids = stock_moved_products.mapped('product_id')
+            products_to_archive = [
+                x.id for x in unavailable_products
+                if x not in moved_product_ids]
+            action = dict(
+                type='ir.actions.act_window',
+                name=_('Products to be archived'),
+                res_model='product.product',
+                view_mode='tree,form',
+                domain=[('id', 'in', products_to_archive)],
+                # 'auto_search': True,
+                # 'views': [
+                #     (self.env.ref('repair.view_repair_order_tree').id, 'tree'),
+                #     (self.env.ref('repair.view_repair_order_form').id, 'form')],
+                target='current',
+            )
+            return action
