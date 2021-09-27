331dccc400329c169bbde7c7ffd91dc87a5e74c6
sergiocorato <sergiocorato@gmail.com>
2021-09-27 10:46:13 +0200

    [FIX] manifest

--- stock_picking_note_visibility/__manifest__.py
+++ stock_picking_note_visibility/__manifest__.py
@@ -3,14 +3,15 @@
 {
     'name': 'Stock picking note visibility',
     'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
     'category': 'other',
+    'description': "Show note after origin instead in dedicated tab",
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
     'depends': [
         'stock',
     ],
     'data': [
         'views/stock_picking.xml',
     ],
-    'description': "Show note after origin instead in dedicated tab",
     'installable': True,
 }
