d01b861d82bd6fe645b934631f792df7dc2d3fd0
sergiocorato <sergiocorato@gmail.com>
2021-09-28 17:35:00 +0200

    [ADD] module to remove insertion of purchase ref on account invoice

---
+++ purchase_invoice_no_reference/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Invoice purchase autocomplete no reference',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'category': 'Products',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+    ],
+    'description': "Do not insert purchase order reference in seller invoice when "
+                   "using purchase order autocomplete.",
+    'installable': True,
+}
---
+++ purchase_invoice_no_reference/tests/__init__.py
@@ -0,0 +1,2 @@
+
+from . import test_purchase_invoice_no_reference
