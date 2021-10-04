55640c6c5225ac0a1154c048f2d5b4d49e382c1e
sergiocorato <sergiocorato@gmail.com>
2021-10-04 15:49:44 +0200

    [IMP] bump version

--- mrp_bom_operation_estimate/__manifest__.py
+++ mrp_bom_operation_estimate/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Mrp bom operation estimate',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'category': 'Manufacture',
     'license': 'AGPL-3',
     'description': """
