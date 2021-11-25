929194ce2da84c3c50de7ade5063d8ab7b5b2eca
sergiocorato <sergiocorato@gmail.com>
2021-11-25 11:54:11 +0100

    [FIX] module name

--- mrp_bom_attachment_export/__manifest__.py
+++ mrp_bom_attachment_export/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Mrp download Zip of components bom attachment',
+    'name': 'Mrp download Zip attachment',
     'version': '12.0.1.0.0',
     'category': 'Manufacture',
     'license': 'AGPL-3',
