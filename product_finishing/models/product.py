3967b52c408b5f99eea4fba34a9a55160ed9ee74
sergiocorato <sergiocorato@gmail.com>
2021-12-02 12:38:06 +0100

    [IMP] surcharge

--- product_finishing/models/product.py
+++ product_finishing/models/product.py
@@ -2,9 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
+import odoo.addons.decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_finishing = fields.Boolean()
+    finishing_surcharge = fields.Float(
+        digits=dp.get_precision('Stock Weight')
+    )
