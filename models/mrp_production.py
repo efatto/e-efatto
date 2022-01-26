3e98a68fa873ba051ba019b69561b06557d71773
sergiocorato <sergiocorato@gmail.com>
2022-01-26 09:04:41 +0100

    [FIX] lot mngt

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -1,11 +1,6 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import json
-import logging
-
 from odoo import api, models
-
-logger = logging.getLogger(__name__)
 
 
 class MrpProduction(models.Model):
@@ -15,41 +10,50 @@ class MrpProduction(models.Model):
     def input_produce(self, keys):
         res = False
         product_qty = 1
-        lot_id = False
+        product_lot = False
         domain = [('state', 'not in', ['done', 'cancel'])]
+        log_msg = ''
         for key, value in keys.items():
             if key == 'production_name':
                 production_name = value
                 domain.append(('name', '=', value))
                 productions = self.env['mrp.production'].search(domain)
-                if len(productions) != 1:
-                    logger.exception(
-                        'No or more production found for %s' % production_name and
-                        production_name or '')
+                if not productions:
+                    log_msg += 'Production %s not found' % production_name
+                    break
+                if len(productions) > 1:
+                    log_msg += 'More production %s found' % production_name
                 production = productions[0]
             if key == 'product_lot':
                 product_lot = value
-                lot_ids = self.env['stock.production.lot'].search([
-                    ('name', '=', product_lot)
-                ])
-                if len(lot_ids) != 1:
-                    logger.exception('No or more lot found for %s' % product_lot and
-                                     product_lot or '')
-                lot_id = lot_ids[0]
             if key == 'product_qty':
-                product_qty = value
+                product_qty = int(value)
         if production:
-            # TODO se la produzione è in draft serve avviarla?
             values = {
                 'production_id': production.id,
                 'product_id': production.product_id.id,
                 'product_qty': product_qty,
                 'product_uom_id': production.product_uom_id.id,
             }
-            if lot_id:
-                values.update(
-                    dict(lot_id=lot_id.id)
-                )
+            if product_lot:
+                lot_id = False
+                lot_ids = self.env['stock.production.lot'].search([
+                    ('name', '=', product_lot),
+                ])
+                if not lot_ids:
+                    lot_id = self.env['stock.production.lot'].create([{
+                        'product_id': production.product_id.id,
+                        'name': product_lot,
+                    }])
+                    log_msg += 'Created new lot %s' % product_lot
+                elif len(lot_ids) > 1:
+                    log_msg += 'More lot %s found' % product_lot
+                    lot_id = lot_ids[0]
+                if lot_id:
+                    values.update(
+                        dict(lot_id=lot_id.id)
+                    )
+            # TODO se la produzione è in draft serve avviarla?
             produce = self.env['mrp.product.produce'].with_context(
                 default_production_id=production.id,
                 active_id=production.id).create(values)
@@ -57,5 +61,5 @@ class MrpProduction(models.Model):
             res = produce.do_produce()
         if not res:
             return {'status': 'error',
-                    'message': 'Unable to produce'}
+                    'message': log_msg}
         return {'status': 'ok', 'message': 'Production done'}
