a7e761b765f705f05c9d5a8114b78ec1f4e0dbbb
sergiocorato <sergiocorato@gmail.com>
2022-02-04 08:29:25 +0100

    [IMP] more tests

--- purchase_sale_mrp_link/tests/test_purchase_sale_mrp_link.py
+++ purchase_sale_mrp_link/tests/test_purchase_sale_mrp_link.py
@@ -15,10 +15,8 @@ class TestPurchaseSaleMrpLink(SavepointCase):
         cls.vendor = cls.env.ref('base.res_partner_3')
         cls.partner = cls.env.ref('base.res_partner_2')
         cls.partner.customer = True
-        cls.product = cls.env.ref('product.product_product_1')
-        #todo creare un preventivo di vendita con prodotti e prodotti con bom
-        #todo creare un RDP a fornitore con alcuni prodotti collegabili e altri no
-        #todo lanciare il wizard
+        cls.product = cls.env.ref('product.product_delivery_01')
+        cls.product1 = cls.env.ref('product.product_delivery_02')
 
     def _create_sale_order_line(self, order, product, qty):
         vals = {
@@ -32,29 +30,63 @@ class TestPurchaseSaleMrpLink(SavepointCase):
         line._convert_to_write(line._cache)
         return line
 
-    def _create_purchase_order(self):
-        purchase_order = self.env['purchase.order'].create({
-            'partner_id': self.vendor.id,
-            'date_order': fields.Date.today(),
-            'order_line': [
-                (0, 0, {
-                    'product_id': self.product.id,
-                    'product_qty': 20,
-                    'product_uom': self.product.uom_po_id.id,
-                    'price_unit': self.product.list_price,
-                    'name': self.product.name,
-                    'date_planned': fields.Date.today(),
-                }),
-            ]
-        })
-        return purchase_order
+    def _create_purchase_order_line(self, order, product, qty):
+        vals = {
+            'order_id': order.id,
+            'product_id': product.id,
+            'product_qty': qty,
+            'product_uom': product.uom_po_id.id,
+            'price_unit': product.list_price,
+            'name': product.name,
+            'date_planned': fields.Date.today(),
+        }
+        line = self.env['purchase.order.line'].create(vals)
+        line.onchange_product_id()
+        line._convert_to_write(line._cache)
+        return line
 
     def test_sale_link_product_simple(self):
         sale_order = self.env['sale.order'].create({
             'partner_id': self.partner.id
         })
         self._create_sale_order_line(sale_order, self.product, 5.0)
-        purchase_order = self._create_purchase_order()
+        self._create_sale_order_line(sale_order, self.product1, 5.0)
+        purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.partner.id
+        })
+        self._create_purchase_order_line(purchase_order, self.product, 5.0)
+        purchase_order.order_line[0]._onchange_quantity()
+        self.assertEqual(
+            len(purchase_order.order_line), 1, msg="Order line was not created")
+        wizard_obj = self.env['purchase.sale.mrp.link.wizard']
+        wizard_vals = wizard_obj.with_context(
+                active_id=purchase_order.id,
+                active_ids=[purchase_order.id],
+                active_model='purchase.order',
+            ).default_get(['purchase_order_id'])
+        wizard_vals.update({
+            'sale_order_id': sale_order.id,
+        })
+        wizard = wizard_obj.create(wizard_vals)
+        wizard.action_done()
+        self.assertEqual(
+            purchase_order.sale_order_ids.ids, sale_order.ids
+        )
+        self.assertFalse(
+            sale_order.order_line.filtered(
+                lambda x: x.product_id == self.product1).purchase_line_id
+        )
+
+    def test_sale_link_product_bom(self):
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner.id
+        })
+        # todo add product with bom
+        self._create_sale_order_line(sale_order, self.product, 5.0)
+        purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.partner.id
+        })
+        self._create_purchase_order_line(purchase_order, self.product, 5.0)
         purchase_order.order_line[0]._onchange_quantity()
         self.assertEqual(
             len(purchase_order.order_line), 1, msg="Order line was not created")
