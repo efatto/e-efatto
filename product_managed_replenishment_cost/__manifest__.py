7000d79c91100e729f261eeb7a6e5f436f33b59d
sergiocorato <sergiocorato@gmail.com>
2022-03-25 19:07:38 +0100

    [FIX] bump version

--- product_managed_replenishment_cost/__manifest__.py
+++ product_managed_replenishment_cost/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Managed Replenishment Cost',
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'author': 'Sergio Corato',
     'category': 'Products',
     'license': 'AGPL-3',
