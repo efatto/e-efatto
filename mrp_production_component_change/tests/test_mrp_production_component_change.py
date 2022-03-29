0ad56ef04f057a54136ca4f966964765bf90857d
sergiocorato <sergiocorato@gmail.com>
2022-03-29 15:56:50 +0200

    [FIX] test unique

--- mrp_production_component_change/tests/test_mrp_production_component_change.py
+++ mrp_production_component_change/tests/test_mrp_production_component_change.py
@@ -36,21 +36,10 @@ class TestMrpProductionChangeQty(TestMrpCommon):
             'product_id': self.product_4.id,
         }).action_done()
         self.assertEqual(move_raw.product_id.id, self.product_4.id)
-
-    def test_01_update_product_production_running(self):
-        man_order = self.env['mrp.production'].create({
-            'name': 'MO-Test-to-update-1',
-            'product_id': self.product_6.id,
-            'product_uom_id': self.product_6.uom_id.id,
-            'product_qty': 1,
-            'bom_id': self.bom_3.id,
-        })
-        self.assertEqual(len(man_order.move_raw_ids), 4)
         man_order.action_assign()
         man_order.button_plan()
         self.assertEqual(man_order.state, 'planned')
         move_raw = man_order.move_raw_ids[1]
-        self.assertEqual(move_raw.product_uom_qty, 0.27)
         self.env['mrp.production.component.change'].with_context(
             active_id=move_raw.id,
             active_model='stock.move',
@@ -58,4 +47,4 @@ class TestMrpProductionChangeQty(TestMrpCommon):
             'product_uom_qty': move_raw.product_qty + 5,
         }).action_done()
         self.assertEqual(len(man_order.move_raw_ids), 4)
-        self.assertEqual(move_raw.product_uom_qty, 5.27)
+        self.assertEqual(move_raw.product_uom_qty, 10.27)
