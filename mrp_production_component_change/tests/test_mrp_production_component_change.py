3eda3ae769a5d07c1a4d89418c5c5744cadf797c
sergiocorato <sergiocorato@gmail.com>
2022-03-29 15:35:45 +0200

    [FIX] (c)

--- mrp_production_component_change/tests/test_mrp_production_component_change.py
+++ mrp_production_component_change/tests/test_mrp_production_component_change.py
@@ -1,5 +1,5 @@
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.addons.mrp.tests.common import TestMrpCommon
 
