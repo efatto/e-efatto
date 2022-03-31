544067789a950c26c1aaaaf913506479cfa1cd0c
sergiocorato <sergiocorato@gmail.com>
2022-03-31 09:41:51 +0200

    [FIX] tests

--- tests/test_iot_input_data.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tools import mute_logger
-from odoo.tests import Form
-
-# TODO: create tests
