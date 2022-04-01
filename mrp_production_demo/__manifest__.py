2fc14d5c2d81317e7bd793a6580703b150ce6898
sergiocorato <sergiocorato@gmail.com>
2022-04-01 10:32:46 +0200

    [ADD] demo data for mrp

---
+++ mrp_production_demo/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Production Demo',
+    'summary': 'Demo data',
+    'version': '12.0.1.0.1',
+    'category': 'MRP',
+    'author': 'Sergio Corato Efatto.it',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'maintainers': ['sergiocorato'],
+    'depends': ['mrp'],
+    'installable': True,
+    'demo': ['demo/product.xml'],
+}
