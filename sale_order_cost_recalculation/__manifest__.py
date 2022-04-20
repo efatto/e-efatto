8b9dd131493b34334a0c6a573fd63bcc28bd5d99
sergiocorato <sergiocorato@gmail.com>
2022-04-20 12:14:54 +0200

    [ADD] module to update cost with sale price

---
+++ sale_order_cost_recalculation/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Sale order cost recalculation',
+    'version': '12.0.1.0.0',
+    'category': 'other',
+    'author': 'Sergio Corato',
+    'description': 'Add recalculation of cost when recalculating prices on sale order.',
+    'website': 'https://efatto.it',
+    'depends': [
+        'sale_margin',
+        'sale_order_price_recalculation',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ sale_order_cost_recalculation/models/sale.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def recalculate_prices(self):
+        res = super().recalculate_prices()
+        for line in self.mapped('order_line'):
+            dict = line._convert_to_write(line.read()[0])
+            if 'product_tmpl_id' in line._fields:
+                dict['product_tmpl_id'] = line.product_tmpl_id
+            line2 = self.env['sale.order.line'].new(dict)
+            # we make this to isolate changed values:
+            line2.product_id_change_margin()
+            line.write({
+                'purchase_price': line2.purchase_price,
+            })
+        return res
