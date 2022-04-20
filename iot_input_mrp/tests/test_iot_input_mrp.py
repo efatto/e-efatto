61f3f5030adaa6a04661e5e7a793c9b7a13af017
sergiocorato <sergiocorato@gmail.com>
2022-04-20 10:51:21 +0200

    [FIX] test

--- iot_input_mrp/tests/test_iot_input_mrp.py
+++ iot_input_mrp/tests/test_iot_input_mrp.py
@@ -1,11 +1,13 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.addons.mrp.tests.common import TestMrpCommon
+import time
+
+from odoo.addons.mrp_production_demo.tests.common_data import TestProductionData
 from odoo.tools import mute_logger
-from odoo.tests import Form
+import datetime
 
 
-class TestIotInputMrp(TestMrpCommon):
+class TestIotInputMrp(TestProductionData):
 
     def setUp(self):
         super().setUp()
@@ -19,20 +21,12 @@ class TestIotInputMrp(TestMrpCommon):
             'passphrase': self.passphrase,
         }])
         self.address_1 = 'I0'
-        self.env['iot.device.input'].create([{
+        self.iot_device_input = self.env['iot.device.input'].create([{
             'name': 'Input',
             'device_id': self.device.id,
             'address': self.address_1,
-            'call_model_id': self.ref('mrp.model_mrp_production'),
-            'call_function': 'input_production_produce'
-        }])
-        self.address_2 = 'I1'
-        self.env['iot.device.input'].create([{
-            'name': 'Input 1',
-            'device_id': self.device.id,
-            'address': self.address_2,
-            'call_model_id': self.ref('mrp.model_mrp_production'),
-            'call_function': 'input_production_weight'
+            'call_model_id': self.ref('iot_input_data.model_iot_input_data'),
+            'call_function': 'input_data',
         }])
         self.uom_kgm = self.env.ref('uom.product_uom_kgm')
         self.product_weight = self.env['product.product'].create([{
@@ -48,11 +42,11 @@ class TestIotInputMrp(TestMrpCommon):
             'uom_id': self.uom_kgm.id,
             'uom_po_id': self.uom_kgm.id}])
         self.bom_weight = self.env['mrp.bom'].create([{
-            'product_id': self.product_6.id,
-            'product_tmpl_id': self.product_6.product_tmpl_id.id,
+            'product_id': self.top_product.id,
+            'product_tmpl_id': self.top_product.product_tmpl_id.id,
             'product_uom_id': self.uom_unit.id,
             'product_qty': 2.0,
-            'routing_id': self.routing_1.id,
+            'routing_id': self.routing1.id,
             'type': 'normal',
             'bom_line_ids': [
                 (0, 0, {'product_id': self.product_weight.id, 'product_qty': 2.55}),
@@ -60,56 +54,32 @@ class TestIotInputMrp(TestMrpCommon):
                 (0, 0, {'product_id': self.product_weight2.id, 'product_qty': 12.01})
             ]}])
         # total: (2.55 + 8.13 + 12.01) = 22.69 / 2 = 11.345
+        self.bag_variable_name = 'variable_bag_count'
+        self.weight_variable_name = 'variable_weight'
+        self.duration_variable_name = 'variable_duration'
+        self.workcenter1.write(dict(
+            bag_variable_name=self.bag_variable_name,
+            weight_variable_name=self.weight_variable_name,
+            duration_variable_name=self.duration_variable_name,
+            iot_device_input_id=self.iot_device_input.id,
+        ))
 
-    @mute_logger(
-        'odoo.models', 'odoo.models.unlink', 'odoo.addons.base.ir.ir_model'
-    )
-    def test_production_produce(self):
-        production = self.env['mrp.production'].create({
-            'name': 'MO-Test',
-            'product_id': self.product_6.id,
-            'product_uom_id': self.product_6.uom_id.id,
-            'product_qty': 1,
-            'bom_id': self.bom_3.id,
-        })
-        input_values = [{
-            'address': self.address_1,
-            'value': 'MO-Test',
-            'product_qty': 5,
-        }]
-        for response in self.device.parse_multi_input(
-            self.device.device_identification,
-            self.device.passphrase,
-            input_values
-        ):
-            self.assertEqual(response.get('message'), 'Production done')
-        self.assertEqual(production.state, 'progress')
-        self.assertEqual(production.qty_produced, 5.0)
-
-    @mute_logger(
-        'odoo.models', 'odoo.models.unlink', 'odoo.addons.base.ir.ir_model'
-    )
-    def test_production_produce_lot(self):
-        production = self.env['mrp.production'].create({
-            'name': 'MO-Test',
-            'product_id': self.product_6.id,
-            'product_uom_id': self.product_6.uom_id.id,
-            'product_qty': 1,
-            'bom_id': self.bom_3.id,
-        })
-        input_values = [{
-            'address': self.address_1,
-            'value': 'MO-Test',
-            'product_qty': 1,
-            'product_lot': '780'}]
-        for response in self.device.parse_multi_input(
-            self.device.device_identification,
-            self.device.passphrase,
-            input_values
-        ):
-            self.assertEqual(response.get('message'), 'Production done')
-        self.assertEqual(production.state, 'progress')
-        self.assertEqual(production.qty_produced, 1.0)
+    def input_data(self, variable_weight, variable_duration, variable_bag_count):
+        self.variable_weight = variable_weight
+        self.variable_duration = variable_duration
+        self.variable_bag_count = variable_bag_count
+        for var in ['variable_weight', 'variable_duration', 'variable_bag_count']:
+            iot_values = {
+                "name": var,
+                "value": getattr(self, var),
+                "iot_device_input_id": self.iot_device_input.id,
+                "timestamp": str(
+                    datetime.datetime.now(tz=datetime.timezone.utc).strftime(
+                        "%Y-%m-%dT%H:%M:%S.%fz"
+                    )
+                ),
+            }
+            self.env['iot.input.data'].create(iot_values)
 
     @mute_logger(
         'odoo.models', 'odoo.models.unlink', 'odoo.addons.base.ir.ir_model'
@@ -117,40 +87,29 @@ class TestIotInputMrp(TestMrpCommon):
     def test_production_weight_more(self):
         production = self.env['mrp.production'].create({
             'name': 'MO-Test',
-            'product_id': self.product_6.id,
-            'product_uom_id': self.product_6.uom_id.id,
+            'product_id': self.top_product.id,
+            'product_uom_id': self.top_product.uom_id.id,
             'product_qty': 1,
             'bom_id': self.bom_weight.id,
         })
         production.button_plan()
         production.action_assign()
         self.assertTrue(production.workorder_ids)
+        # production weight more than estimated: 12.787 instead of 11.345
+        self.input_data(
+            variable_weight=12.78,
+            variable_duration=600,
+            variable_bag_count=121)
         for workorder in production.workorder_ids:
             workorder.button_start()
-            workorder.button_done()
+            time.sleep(2)
         self.assertEqual(production.state, 'progress')
-        produce_form = Form(self.env['mrp.product.produce'].with_context(
-            active_id=production.id,
-            active_ids=[production.id],
-        ))
-        produce_form.product_qty = 1.0
-        wizard = produce_form.save()
-        wizard.do_produce()
-        self.assertTrue(production.check_to_done)
-        # production weight lower than estimated: 12.787 instead of 11.345
-        input_values = [{
-            'address': self.address_2,
-            'value': 'MO-Test',
-            'production_weight': 12.787,
-        }]
-        for response in self.device.parse_multi_input(
-            self.device.device_identification,
-            self.device.passphrase,
-            input_values
-        ):
-            self.assertEqual(response.get('message'), 'Production weighted')
+        last_workorder = production.workorder_ids.filtered(
+            lambda x: x.state == 'progress')
+        last_workorder.record_production()
+        production.button_mark_done()
         self.assertAlmostEqual(
-            sum(x.product_uom_qty for x in production.move_raw_ids), 12.787)
+            sum(x.quantity_done for x in production.move_raw_ids), 12.78)
 
     @mute_logger(
         'odoo.models', 'odoo.models.unlink', 'odoo.addons.base.ir.ir_model'
@@ -158,37 +117,28 @@ class TestIotInputMrp(TestMrpCommon):
     def test_production_weight_less(self):
         production = self.env['mrp.production'].create({
             'name': 'MO-Test',
-            'product_id': self.product_6.id,
-            'product_uom_id': self.product_6.uom_id.id,
+            'product_id': self.top_product.id,
+            'product_uom_id': self.top_product.uom_id.id,
             'product_qty': 1,
             'bom_id': self.bom_weight.id,
         })
         production.button_plan()
         production.action_assign()
         self.assertTrue(production.workorder_ids)
+        # production weight less than estimated: 10.143 instead of 11.345
+        self.input_data(
+            variable_weight=10.14,
+            variable_duration=600,
+            variable_bag_count=121)
         for workorder in production.workorder_ids:
             workorder.button_start()
-            workorder.button_done()
+            time.sleep(2)
         self.assertEqual(production.state, 'progress')
-        produce_form = Form(self.env['mrp.product.produce'].with_context(
-            active_id=production.id,
-            active_ids=[production.id],
-        ))
-        produce_form.product_qty = 1.0
-        wizard = produce_form.save()
-        wizard.do_produce()
-        self.assertTrue(production.check_to_done)
-        # production weight lower than estimated: 12.787 instead of 11.345
-        input_values = [{
-            'address': self.address_2,
-            'value': 'MO-Test',
-            'production_weight': 10.143,
-        }]
-        for response in self.device.parse_multi_input(
-            self.device.device_identification,
-            self.device.passphrase,
-            input_values
-        ):
-            self.assertEqual(response.get('message'), 'Production weighted')
+        last_workorder = production.workorder_ids.filtered(
+            lambda x: x.state == 'progress')
+        last_workorder.record_production()
+        production.button_mark_done()
         self.assertAlmostEqual(
-            sum(x.product_uom_qty for x in production.move_raw_ids), 10.143)
+            sum(x.quantity_done for x in production.move_raw_ids), 10.14)
+
+    # todo test bag count
