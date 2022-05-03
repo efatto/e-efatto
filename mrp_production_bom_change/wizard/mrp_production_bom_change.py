10a6d977a59ba2fb7c519e1a24896297d24bcfb7
sergiocorato <sergiocorato@gmail.com>
2022-05-03 22:39:11 +0200

    [IMP] change product too and filter bom

--- mrp_production_bom_change/wizard/mrp_production_bom_change.py
+++ mrp_production_bom_change/wizard/mrp_production_bom_change.py
@@ -10,6 +10,8 @@ class MrpProductionBomChange(models.TransientModel):
     _description = 'Change production BoM'
 
     bom_id = fields.Many2one(comodel_name='mrp.bom', required=True, string='BoM')
+    product_id = fields.Many2one(comodel_name='product.product', required=True,
+                                 string='Product')
 
     @api.model
     def default_get(self, fields_list):
@@ -19,6 +21,7 @@ class MrpProductionBomChange(models.TransientModel):
         if mo.state == "done":
             raise UserError(_("MO in state 'done' cannot be changed!"))
         defaults["bom_id"] = mo.bom_id.id
+        defaults["product_id"] = mo.product_id.id
         return defaults
 
     @api.multi
@@ -34,9 +37,11 @@ class MrpProductionBomChange(models.TransientModel):
             for move_line in move.move_line_ids:
                 move_line.write({"state": "draft", "qty_done": qty_done})
             move._action_cancel()
+            move.unlink()
         mo.write(
             {
                 "bom_id": self.bom_id.id,
+                "product_id": self.product_id.id,
             }
         )
         wizard = self.env['change.production.qty'].create({
