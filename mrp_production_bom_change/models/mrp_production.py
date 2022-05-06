3d1c498c16281d0abc8a77700dfb46f7fd952b63
sergiocorato <sergiocorato@gmail.com>
2022-05-06 17:25:31 +0200

    [IMP] hide button for started production

---
+++ mrp_production_bom_change/models/mrp_production.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    is_almost_partially_produced = fields.Boolean(
+        string="Is almost partially produced",
+        compute='_compute_is_almost_partially_produced')
+
+    @api.multi
+    def _compute_is_almost_partially_produced(self):
+        for production in self:
+            done_moves = production.move_finished_ids.filtered(
+                lambda x: x.state != 'cancel'
+                and x.product_id.id == production.product_id.id)
+            qty_produced = sum(done_moves.mapped('quantity_done'))
+            if qty_produced > 0:
+                production.is_almost_partially_produced = True
+            else:
+                production.is_almost_partially_produced = False
