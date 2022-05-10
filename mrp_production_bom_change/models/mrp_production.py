176e1b6c75b2c3fa27992ed400f2b80f4ebb169c
sergiocorato <sergiocorato@gmail.com>
2022-05-10 19:37:22 +0200

    [FIX] date

--- mrp_production_bom_change/models/mrp_production.py
+++ mrp_production_bom_change/models/mrp_production.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
