5d8151839683683b8e878c234d5be226b525637b
sergiocorato <sergiocorato@gmail.com>
2022-06-03 17:12:19 +0200

    [ADD] port stock orderpoint generator for sale

---
+++ stock_orderpoint_generator_sale/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2012-2016 Camptocamp SA
+# Copyright 2019 Tecnativa
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Order point generator sale',
+    'summary': 'Mass configuration of stock order points from customer moves',
+    'version': '12.0.1.0.0',
+    'category': 'Warehouse',
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'product_state',
+        'stock_orderpoint_generator',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ir_cron.xml',
+        'views/orderpoint_template_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
