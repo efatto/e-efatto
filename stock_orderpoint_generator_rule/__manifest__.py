cd01e394b5ecfe51a0d44b66a0829fc815b4d848
sergiocorato <sergiocorato@gmail.com>
2022-06-06 17:56:17 +0200

    [FIX] desc

--- stock_orderpoint_generator_rule/__manifest__.py
+++ stock_orderpoint_generator_rule/__manifest__.py
@@ -4,8 +4,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Order point generator sale',
-    'summary': 'Mass configuration of stock order points from customer moves',
+    'name': 'Order point generator rule',
+    'summary': 'Mass configuration of stock order points from rules',
     'version': '12.0.1.0.0',
     'category': 'Warehouse',
     'author': 'Sergio Corato',
