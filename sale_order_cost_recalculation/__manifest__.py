d8259109d90447d7d037a98d9878a60f8d76038e
sergiocorato <sergiocorato@gmail.com>
2022-06-15 08:58:27 +0200

    [IMP] add field purchase date

--- sale_order_cost_recalculation/__manifest__.py
+++ sale_order_cost_recalculation/__manifest__.py
@@ -2,16 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Sale order cost recalculation',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'other',
     'author': 'Sergio Corato',
     'summary': 'Add recalculation of cost when recalculating prices on sale order.',
     'website': 'https://efatto.it',
     'depends': [
-        'sale_margin',
+        'sale_margin_security',
         'sale_order_price_recalculation',
     ],
     'data': [
+        'views/sale_margin_view.xml',
     ],
     'installable': True,
     'auto_install': True,
