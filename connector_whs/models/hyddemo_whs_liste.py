b477bd9b0735aa8d507c86d6a2a9d7be93a8a465
sergiocorato <sergiocorato@gmail.com>
2022-06-27 19:03:10 +0200

    [IMP] add priority and activate test

--- connector_whs/models/hyddemo_whs_liste.py
+++ connector_whs/models/hyddemo_whs_liste.py
@@ -54,7 +54,7 @@ class HyddemoWhsListe(models.Model):
         ('2', 'Deposito'),
         ('3', 'Inventario')  # 5 su WHS, 6 trasferimento
     ], string='Tipo lista')
-    priorita = fields.Integer('Priorita', default=0)
+    priorita = fields.Integer('Priorita', default=0)  # 0=Bassa; 1=Media; 2=Urgente
     vettore = fields.Text('Vettore', size=30)
     cliente = fields.Text('Codice cliente', size=30,
                           help='Used as unique code in outher db, so spaces are '
