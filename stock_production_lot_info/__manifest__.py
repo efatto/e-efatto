650d3aa151ea7314d13484003473610812c5b9e8
sergiocorato <sergiocorato@gmail.com>
2022-07-04 08:52:58 +0200

    [ADD] module to show lot info on lot m2o field

---
+++ stock_production_lot_info/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock product lot qty info",
+    "summary": "Show product lot qty in name",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://efatto.it",
+    "author": "Sergio Corato, Efatto.it",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock_picking_filter_lot",
+    ],
+    "data": [
+    ],
+}
