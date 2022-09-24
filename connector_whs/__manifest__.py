ec6e50eae3a36c74f666ec3c45bfce45917bc94c
sergiocorato <sergiocorato@gmail.com>
2022-09-24 20:27:15 +0200

    [IMP] bump version

--- connector_whs/__manifest__.py
+++ connector_whs/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Connector WHS MSSQL',
-    'version': '12.0.1.0.24',
+    'version': '12.0.1.0.25',
     'author': 'Sergio Corato',
     'category': 'other',
     'depends': [
