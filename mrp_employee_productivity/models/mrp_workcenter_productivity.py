1f248ee6296ea07bb702b467e844b84fd90b77c3
sergiocorato <sergiocorato@gmail.com>
2022-09-26 11:30:32 +0200

    [FIX] unused imports

--- mrp_employee_productivity/models/mrp_workcenter_productivity.py
+++ mrp_employee_productivity/models/mrp_workcenter_productivity.py
@@ -1,8 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 
 
 class MrpWorkcenterProductivity(models.Model):
