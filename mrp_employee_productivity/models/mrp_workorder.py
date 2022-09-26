f247e4f14e1174d09aa429e57c7eeae354ec8c75
sergiocorato <sergiocorato@gmail.com>
2022-09-26 11:36:56 +0200

    [IMP] search workorder on SO

---
+++ mrp_employee_productivity/models/mrp_workorder.py
@@ -0,0 +1,21 @@
+from odoo import api, models
+
+
+class MrpWorkorder(models.Model):
+    _inherit = 'mrp.workorder'
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100,
+                     name_get_uid=None):
+        if not name == '' and operator in ('ilike', 'like', '=', '=like', '=ilike'):
+            args = [] if args is None else args.copy()
+            args += [
+                '|',
+                ('name', operator, name),
+                ('production_id.sale_id.name', operator, name),
+            ]
+            if operator == 'ilike':
+                # to exclude extension of args with and & domain
+                name = ''
+        return super()._name_search(name=name, args=args, operator=operator,
+                                    limit=limit, name_get_uid=name_get_uid)
