198f27552c419e068418624c1a093b528c3b9307
sergiocorato <sergiocorato@gmail.com>
2022-09-27 10:47:11 +0200

    [ADD] module to set eu date in picking report

---
+++ stock_picking_delivery_report_date/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Delivery Report Default EU Date",
+    "summary": "Set date in EU format",
+    "version": "12.0.1.0.0",
+    "category": "Stock",
+    "website": "https://efatto.it",
+    "author": "Sergio Corato Efatto.it",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/picking_report.xml",
+    ]
+}
