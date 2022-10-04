24a38fd48c6b35ad1c85875e8d53e0932dbb1577
sergiocorato <sergiocorato@gmail.com>
2022-10-04 10:20:59 +0200

    [FIX] compute price on replenishment cost only when needed

--- product_pricelist_replenishment_cost/models/sale.py
+++ product_pricelist_replenishment_cost/models/sale.py
@@ -7,13 +7,25 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def _compute_margin(self, order_id, product_id, product_uom_id):
-        frm_cur = self.env.user.company_id.currency_id
-        to_cur = order_id.pricelist_id.currency_id
-        purchase_price = product_id.managed_replenishment_cost
-        if product_uom_id != product_id.uom_id:
-            purchase_price = product_id.uom_id._compute_price(
-                purchase_price, product_uom_id)
-        price = frm_cur._convert(
-            purchase_price, to_cur, order_id.company_id or self.env.user.company_id,
-            order_id.date_order or fields.Date.today(), round=False)
+        price = super()._compute_margin(order_id, product_id, product_uom_id)
+        # find if exists a rule applicable on managed replenishment cost, then compute
+        # cost accordingly
+        product_context = dict(
+            self.env.context, partner_id=order_id.partner_id.id,
+            date=order_id.date_order, uom=product_uom_id.id)
+        # compute on qty 1 as qty is not available here
+        fake_price, rule_id = order_id.pricelist_id.with_context(
+            product_context).get_product_price_rule(
+                product_id, 1, order_id.partner_id)
+        rule = self.env['product.pricelist.item'].browse(rule_id)
+        if rule and rule.base == 'managed_replenishment_cost':
+            frm_cur = self.env.user.company_id.currency_id
+            to_cur = order_id.pricelist_id.currency_id
+            purchase_price = product_id.managed_replenishment_cost
+            if product_uom_id != product_id.uom_id:
+                purchase_price = product_id.uom_id._compute_price(
+                    purchase_price, product_uom_id)
+            price = frm_cur._convert(
+                purchase_price, to_cur, order_id.company_id or self.env.user.company_id,
+                order_id.date_order or fields.Date.today(), round=False)
         return price
