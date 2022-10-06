4519ce6d8d39bf8b358aa702a0b676054de3d4fc
sergiocorato <sergiocorato@gmail.com>
2022-10-06 09:41:46 +0200

    [FIX] tests with new configuration

--- connector_whs/tests/__init__.py
+++ connector_whs/tests/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+# n.b. execute only on local with dedicated vm
 # from . import test_connector_whs
