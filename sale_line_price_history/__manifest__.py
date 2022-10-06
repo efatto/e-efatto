52b0176804d96ec3759a683bbf021bf6a4296450
sergiocorato <sergiocorato@gmail.com>
2022-10-06 16:46:28 +0200

    [FIX] deprecate module

--- sale_line_price_history/__manifest__.py
+++ sale_line_price_history/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Sale line price history",
+    "name": "Sale line price history ## TROPPO LENTO!!!",
     "version": "12.0.1.0.0",
     "category": "Sales",
     "author": "Sergio Corato - Efatto.it",
@@ -12,5 +12,5 @@
     "data": [
         "views/sale.xml",
     ],
-    "installable": True
+    "installable": False,
 }
