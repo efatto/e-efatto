a60193df0e9485f5b7046b3c10f021009361bc51
sergiocorato <sergiocorato@gmail.com>
2022-10-07 16:08:29 +0200

    [FIX] search inherit

--- product_search_customer_code/models/product.py
+++ product_search_customer_code/models/product.py
@@ -8,18 +8,22 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False,
-                access_rights_uid=None):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
         # extends search with OR original search OR customer info code OR name
         args = (args or [])
-        if len(args) == 1:
-            if args[0][1] in ['ilike', 'like']:
-                operator = args[0][1]
-                name = args[0][2]
+        if len(args) >= 1:
+            name = False
+            for arg in args:
+                if (
+                    isinstance(arg, list) or isinstance(arg, tuple)
+                ) and arg[1] in ['like', 'ilike']:
+                    name = arg[2]
+                    break
+            if name:
                 customerinfo_ids = self.env['product.customerinfo'].search([
                     '|',
-                    ('product_code', operator, name),
-                    ('product_name', operator, name),
+                    ('product_code', 'like', name),
+                    ('product_name', 'like', name),
                 ], limit=limit)
                 product_tmpl_ids = customerinfo_ids.mapped('product_tmpl_id')
                 if product_tmpl_ids:
@@ -29,6 +33,5 @@ class ProductProduct(models.Model):
                         [('id', 'in', product_tmpl_ids.mapped(
                             'product_variant_ids').ids)]
                     ])
-        return super()._search(
-            args, offset=offset, limit=limit, order=order, count=count,
-            access_rights_uid=access_rights_uid)
+        return super().search(
+            args, offset=offset, limit=limit, order=order, count=count)
