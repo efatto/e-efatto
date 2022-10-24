70a48ae212732f30c3de0901af9e0f0794324354
sergiocorato <sergiocorato@gmail.com>
2022-10-24 12:38:56 +0200

    [ADD] module for expected qty production

---
+++ mrp_production_expected_qty/readme/CONFIGURE.rst
@@ -0,0 +1,6 @@
+Questo modulo aggiunge un campo che conserva il dato 'Da utilizzare' in modo che, anche se non utilizzato, sia possibile valutare le previsioni di utilizzo iniziali.
+
+.. image:: ../static/description/da_utilizzare_previsto.png
+    :alt: Da utilizzare previsto
+
+Questo in quanto di default se una riga viene impostato come quantità utilizzata pari a 0, in automatico la quantità da utilizzare viene allineata (al netto di eventuali moduli extra che permettano di modificare la quantità da utilizzare stessa).
---
+++ mrp_production_expected_qty/tests/test_mrp_production_lot_custom_assign.py
@@ -0,0 +1,52 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.addons.mrp_production_demo.tests.common_data import TestProductionData
+from odoo.tests import Form
+
+
+class TestMrpProductionLotCustomAssign(TestProductionData):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_create_production(self):
+        self.main_bom.routing_id = self.routing1
+        man_order = self.env['mrp.production'].create({
+            'name': 'MO-Test',
+            'product_id': self.top_product.id,
+            'product_uom_id': self.top_product.uom_id.id,
+            'product_qty': 5,
+            'bom_id': self.main_bom.id,
+        })
+        self.assertEqual(len(man_order.move_raw_ids), 3)
+        for move in man_order.move_raw_ids:
+            self.assertEqual(move.product_uom_qty, move.expected_product_uom_qty)
+        man_order.action_assign()
+        man_order.button_plan()
+        # procuce partially
+        produce_form = Form(
+            self.env['mrp.product.produce'].with_context(
+                active_id=man_order.id,
+                active_ids=[man_order.id],
+            )
+        )
+        produced_qty = 2.0
+        produce_form.product_qty = produced_qty
+        wizard = produce_form.save()
+        wizard.do_produce()
+        self.assertTrue(man_order.finished_move_line_ids)
+        self.assertAlmostEqual(
+            sum(man_order.mapped('finished_move_line_ids.qty_done')), 2.0)
+        move_raw = man_order.move_raw_ids[1]
+        self.assertEqual(move_raw.product_uom_qty, 40)
+        self.assertEqual(len(man_order.move_raw_ids), 3)
+
+        # set 0 to move_raw quantity_done unlink related move lines
+        sml = self.env['stock.move.line'].search([('move_id', '=', move_raw.id)])
+        self.assertAlmostEqual(move_raw.quantity_done, 16.0)
+        sml.unlink()
+        self.assertAlmostEqual(move_raw.quantity_done, 0.0)
+        man_order.button_mark_done()
+        self.assertAlmostEqual(move_raw.quantity_done, 0.0)
