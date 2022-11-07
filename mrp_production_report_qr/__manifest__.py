507a038c7f362b558a28b11946c5fb76a46d0801
sergiocorato <sergiocorato@gmail.com>
2022-11-07 19:30:19 +0100

    [FIX] remove unuseful module

---
+++ mrp_production_report_qr/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Mrp production qr code',
+    'version': '12.0.1.0.0',
+    'category': 'Manufacture',
+    'license': 'AGPL-3',
+    'description': """
+    Add qr code to production.
+    """,
+    'author': 'Sergio Corato',
+    'website': 'https://efatto.it',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'report/mrp_report_qr_code.xml',
+    ],
+    'installable': True,
+}
