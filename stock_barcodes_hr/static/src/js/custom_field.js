d668f5721faec93759f16d65b501c330aec8abd0
sergiocorato <sergiocorato@gmail.com>
2022-11-22 18:47:12 +0100

    [IMP] add widget time float to hour/minutes fields

--- stock_barcodes_hr/static/src/js/custom_field.js
+++ stock_barcodes_hr/static/src/js/custom_field.js
@@ -2,6 +2,7 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
 "use strict";
 
     var FieldFloatNumericMode = require('stock_barcodes.FieldFloatNumericMode').FieldFloatNumericMode;
+    var basic_fields = require('web.basic_fields');
     var field_registry = require('web.field_registry');
 
     var FieldFloatNumericModeOnReturn = FieldFloatNumericMode.extend({
@@ -16,6 +17,33 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
         },
     });
 
+    var FieldFloatTimeNumericModeOnReturn = basic_fields.FieldFloatTime.extend({
+        events: _.extend({}, basic_fields.FieldFloatTime.prototype.events, {
+            "focusin": "_onFocusIn",
+        }),
+        _onFocusIn: function () {
+            // Auto select all content when user enters into fields with this
+            // widget.
+            this.$input.select();
+        },
+        _prepareInput: function ($input) {
+            // Set numeric mode to display numeric keyboard in mobile devices
+            var $input_numeric = this._super($input);
+            $input_numeric.attr({'inputmode': 'numeric'});
+            return $input_numeric;
+        },
+        _onKeydown: function (ev) {
+            if (ev.which === $.ui.keyCode.ENTER) {
+                if (self.document.getElementsByClassName('btn-primary')) {
+                    const button = self.$('#action_done_button');
+                    button.click();
+                }
+            }
+            this._super.apply(this, arguments);
+        },
+    });
+
     field_registry.add('FieldFloatNumericModeOnReturn', FieldFloatNumericModeOnReturn);
+    field_registry.add('FieldFloatTimeNumericModeOnReturn', FieldFloatTimeNumericModeOnReturn);
 
 });
