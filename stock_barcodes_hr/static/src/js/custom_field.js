fbf5ae70371ff7a3c9d661baa929fac87e683b88
sergiocorato <sergiocorato@gmail.com>
2022-11-23 19:48:07 +0100

    [IMP] ignore not numeric value in float numeric field - to filter possible wrong scanned barcode

--- stock_barcodes_hr/static/src/js/custom_field.js
+++ stock_barcodes_hr/static/src/js/custom_field.js
@@ -2,7 +2,6 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
 "use strict";
 
     var FieldFloatNumericMode = require('stock_barcodes.FieldFloatNumericMode').FieldFloatNumericMode;
-    var basic_fields = require('web.basic_fields');
     var field_registry = require('web.field_registry');
 
     var FieldFloatNumericModeOnReturn = FieldFloatNumericMode.extend({
@@ -15,35 +14,14 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
             }
             this._super.apply(this, arguments);
         },
-    });
-
-    var FieldFloatTimeNumericModeOnReturn = basic_fields.FieldFloatTime.extend({
-        events: _.extend({}, basic_fields.FieldFloatTime.prototype.events, {
-            "focusin": "_onFocusIn",
-        }),
-        _onFocusIn: function () {
-            // Auto select all content when user enters into fields with this
-            // widget.
-            this.$input.select();
-        },
-        _prepareInput: function ($input) {
-            // Set numeric mode to display numeric keyboard in mobile devices
-            var $input_numeric = this._super($input);
-            $input_numeric.attr({'inputmode': 'numeric'});
-            return $input_numeric;
-        },
-        _onKeydown: function (ev) {
-            if (ev.which === $.ui.keyCode.ENTER) {
-                if (self.document.getElementsByClassName('btn-primary')) {
-                    const button = self.$('#action_done_button');
-                    button.click();
-                }
+        _parseValue: function (value) {
+            if (isNaN(value)) {
+                return false;
             }
-            this._super.apply(this, arguments);
+            return this._super.apply(this, arguments);
         },
     });
 
     field_registry.add('FieldFloatNumericModeOnReturn', FieldFloatNumericModeOnReturn);
-    field_registry.add('FieldFloatTimeNumericModeOnReturn', FieldFloatTimeNumericModeOnReturn);
 
 });
