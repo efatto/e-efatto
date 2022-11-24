c61d91b5393618b7bf7e98afaa0fbb2b53c5dc2e
sergiocorato <sergiocorato@gmail.com>
2022-11-24 10:47:10 +0100

    [IMP] ignore barcode on field float time too

--- stock_barcodes_hr/static/src/js/custom_field.js
+++ stock_barcodes_hr/static/src/js/custom_field.js
@@ -2,6 +2,7 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
 "use strict";
 
     var FieldFloatNumericMode = require('stock_barcodes.FieldFloatNumericMode').FieldFloatNumericMode;
+    var basic_fields = require('web.basic_fields');
     var field_registry = require('web.field_registry');
 
     var FieldFloatNumericModeOnReturn = FieldFloatNumericMode.extend({
@@ -22,6 +23,19 @@ odoo.define('stock_barcodes_hr.FieldFloatNumericModeOnReturn', function (require
         },
     });
 
+    var FieldFloatTimeIgnoreBarcode = basic_fields.FieldFloatTime.extend({
+        _parseValue: function (value) {
+            var regex = /([0-9]+):([0-9]+)/;
+            if (value.match(regex) && value.length > 5) {
+                var cleaned_value = value.match(regex)[0];
+                arguments[0] = cleaned_value;
+                this.$input[0].value = cleaned_value;
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+
     field_registry.add('FieldFloatNumericModeOnReturn', FieldFloatNumericModeOnReturn);
+    field_registry.add('FieldFloatTimeIgnoreBarcode', FieldFloatTimeIgnoreBarcode);
 
 });
