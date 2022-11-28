204f53d92f427f813f85645319d7c9eadbdc9140
sergiocorato <sergiocorato@gmail.com>
2022-11-28 16:10:35 +0100

    [ADD] module to highlight mo lines

---
+++ mrp_production_line_mto_manufacture/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Production Group Line to Produce MTO',
+    'summary': 'Highlight line to produce MTO on MO components',
+    'version': '12.0.1.0.0',
+    'category': 'MRP',
+    'author': 'Sergio Corato Efatto.it',
+    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'maintainers': ['sergiocorato'],
+    'depends': ['mrp'],
+    'data': [
+        'views/mrp_production.xml',
+    ],
+    'installable': True,
+}
---
+++ mrp_production_line_mto_manufacture/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module highlight production component when they are to produce and make to order.
