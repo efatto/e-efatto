ce277d1d6978d9b63c3865af4cd4f0bf778e5f29
sergiocorato <sergiocorato@gmail.com>
2022-11-28 16:10:35 +0100

    [ADD] module to highlight mo lines

---
+++ mrp_production_line_mto_manufacture/models/stock_move.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    is_to_produce_mto = fields.Boolean(
+        compute="_compute_is_to_produce_mto",
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('product_id.route_ids')
+    def _compute_is_to_produce_mto(self):
+        mto_route = self.env.ref('stock.route_warehouse0_mto')
+        man_route = self.env.ref('mrp.route_warehouse0_manufacture')
+        for move in self:
+            move.is_to_produce_mto = all(
+                x in move.product_id.route_ids for x in [mto_route, man_route]
+            )
---
+++ mrp_production_line_mto_manufacture/readme/CONFIGURE.rst
@@ -0,0 +1,1 @@
+Le righe della produzione sono evidenziate in grassetto quando il componente è da produrre su ordine e deve essere prodotto.
