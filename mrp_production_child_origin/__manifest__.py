42942675b339b4935f59e553048fff6a6a4aa208
sergiocorato <sergiocorato@gmail.com>
2022-11-30 09:41:51 +0100

    [ADD] module to pass parent mrp origin to child

---
+++ mrp_production_child_origin/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'MRP Child Origin',
+    'version': '12.0.1.0.0',
+    'author': 'Sergio Corato',
+    'description': 'Extend origin in child production to ease search.',
+    'license': 'AGPL-3',
+    'category': 'Manufacture',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
