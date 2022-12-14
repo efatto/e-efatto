0f19a3dbc4838aeb41018126e05e6efde6bc99d7
sergiocorato <sergiocorato@gmail.com>
2022-12-14 18:50:18 +0100

    [FIX] hook

--- mrp_workorder_timesheet_cost/hooks.py
+++ mrp_workorder_timesheet_cost/hooks.py
@@ -11,6 +11,13 @@ def set_productivity_timesheet_cost(cr, registry):
     _logger.info('Updating #%s productivity timesheet cost' % len(productivities))
     for x in range(100, len(productivities)+100, 100):
         for productivity in productivities[x-100:x]:
-            productivity.amount = productivity.employee_id.timesheet_cost * \
-                productivity.duration / 60.0
+            cost = productivity.employee_id.timesheet_cost or 0.0
+            amount = -productivity.duration / 60.0 * cost
+            if productivity.currency_id != env.user.company_id.currency_id:
+                amount = productivity.currency_id._convert(
+                    amount,
+                    productivity.currency_id,
+                    env.user.company_id,
+                    productivity.date_start)
+            productivity.amount = amount
         _logger.info('Created %s/%s supplier info' % (x, len(productivities)))
