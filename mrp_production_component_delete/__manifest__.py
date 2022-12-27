d35226c1c2983504697c822c70779ca6795e95b3
sergiocorato <sergiocorato@gmail.com>
2022-12-27 19:58:26 +0100

    [ADD] module to delete component

---
+++ mrp_production_component_delete/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'MRP Production Component Delete',
+    'version': '12.0.1.0.0',
+    'development_status': 'Beta',
+    'license': 'AGPL-3',
+    'category': 'Manufacturing',
+    'author': 'Sergio Corato Efatto.it',
+    'website': 'https://efatto.it',
+    'description': 'Delete production component not already transferred.',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp.xml',
+    ],
+    'installable': True,
+}
