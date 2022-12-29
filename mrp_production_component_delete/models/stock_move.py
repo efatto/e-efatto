7636c3929abf9c163723d6136e7691a4c081fa88
sergiocorato <sergiocorato@gmail.com>
2022-12-29 15:22:04 +0100

    [IMP] hide on not-locked mo

--- mrp_production_component_delete/models/stock_move.py
+++ mrp_production_component_delete/models/stock_move.py
@@ -1,11 +1,15 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
+
+    is_locked = fields.Boolean(
+        related='raw_material_production_id.is_locked'
+    )
 
     @api.multi
     def delete_production_component(self):
