bf7c48cc07c610024f20ce3b06828279c3a026e0
sergiocorato <sergiocorato@gmail.com>
2023-01-10 15:11:41 +0100

    [ADD] new module to show computed amount purchase order in company currency

---
+++ purchase_order_company_amount/models/purchase_order.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    company_currency_id = fields.Many2one(
+        related='company_id.currency_id'
+    )
+    amount_untaxed_company_currency = fields.Monetary(
+        compute="_compute_amount_untaxed_company_currency",
+        store=True,
+        currency_field='company_currency_id',
+    )
+
+    @api.multi
+    @api.depends('currency_id', 'amount_untaxed', 'date_planned')
+    def _compute_amount_untaxed_company_currency(self):
+        for order in self:
+            order.amount_untaxed_company_currency = order.currency_id._convert(
+                order.amount_untaxed,
+                order.company_id.currency_id,
+                company=order.company_id,
+                date=order.date_planned
+            )
---
+++ purchase_order_company_amount/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Add company currency amount in purchase orders.
