3ff2e0dda981ff2247cfde91adc02fab68c6eba6
sergiocorato <sergiocorato@gmail.com>
2023-01-25 18:16:57 +0100

    [FIX] total production duration with qty

--- mrp_production_calendar/models/mrp_production.py
+++ mrp_production_calendar/models/mrp_production.py
@@ -1,15 +1,25 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     estimated_days_duration = fields.Float(
-        related='routing_id.estimated_days_duration',
+        compute='_compute_estimated_duration',
         store=True,
         digits=(18, 6),
     )
     estimated_duration = fields.Float(
-        related='routing_id.estimated_duration',
+        compute='_compute_estimated_duration',
         store=True,
     )
+
+    @api.multi
+    @api.depends('routing_id.estimated_duration')
+    def _compute_estimated_duration(self):
+        for production in self:
+            production.estimated_duration = production.routing_id.estimated_duration * \
+                production.product_qty
+            production.estimated_days_duration = \
+                production.routing_id.estimated_days_duration * \
+                production.product_qty
