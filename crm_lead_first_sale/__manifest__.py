85f31754ef0b0c96f1dcd7bef5ec1d79b8879d7e
sergiocorato <sergiocorato@gmail.com>
2023-01-26 11:32:07 +0100

    [FIX] compute days

---
+++ crm_lead_first_sale/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'CRM lead first sale days',
+    'version': '12.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'summary': """
+    Add a field with days to first sale of crm lead.
+    """,
+    'author': "Sergio Corato",
+    'website': 'https://efatto.it',
+    'depends': [
+        'sale_crm',
+    ],
+    'data': [
+        'views/crm_lead.xml',
+    ],
+    'installable': True,
+}
---
+++ crm_lead_first_sale/models/crm_lead.py
@@ -0,0 +1,33 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    first_sale_days = fields.Integer(
+        compute="_compute_first_sale_days",
+        store=True,
+    )
+
+    @api.multi
+    @api.depends("order_ids.message_ids", "create_date")
+    def _compute_first_sale_days(self):
+        sent_subtype = self.env.ref("sale.mt_order_sent")
+        for lead in self:
+            first_sale_days = 0
+            if lead.order_ids:
+                messages = lead.order_ids.mapped("message_ids").filtered(
+                    lambda x: x.subtype_id == sent_subtype
+                )
+                if not messages:
+                    messages = lead.order_ids.mapped("message_ids").filtered(
+                        lambda x: x.subject and "Preventivo" in x.subject
+                    )
+                if messages:
+                    first_sale_days = (
+                        min(messages.mapped('date'))
+                        - lead.create_date
+                    ).days
+            lead.first_sale_days = first_sale_days
