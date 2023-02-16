96fd49975f495560f9a14498f253f6c59aa48629
sergiocorato <sergiocorato@gmail.com>
2023-02-16 16:52:42 +0100

    [FIX] excludes not is_assigned moves from scheduler

--- connector_whs/tests/__init__.py
+++ connector_whs/tests/__init__.py
@@ -2,4 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 # n.b. execute only on local with dedicated vm
-# from . import test_connector_whs
+from . import test_connector_whs
