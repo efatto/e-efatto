1e7c6d4fd660048215108126c27ea822aee0268f
sergiocorato <sergiocorato@gmail.com>
2023-03-01 10:42:39 +0100

    [FIX] transfer already assigned create directly whs lists

--- connector_whs/models/stock.py
+++ connector_whs/models/stock.py
@@ -90,6 +90,9 @@ class Picking(models.Model):
     @api.multi
     def action_confirm(self):
         res = super(Picking, self).action_confirm()
+        for picking in self:
+            if all(move.state == 'assigned' for move in picking.move_lines):
+                self.mapped('move_lines').create_whs_list()
         self.write({'state': 'waiting'})
         return res
 
