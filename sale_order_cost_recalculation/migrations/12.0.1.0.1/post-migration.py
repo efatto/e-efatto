c40ba8146a8bd2530f444421e3269e7424131910
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:48:00 +0100

    [FIX] pre-commit, pylint, black, sort

--- sale_order_cost_recalculation/migrations/12.0.1.0.1/post-migration.py
+++ sale_order_cost_recalculation/migrations/12.0.1.0.1/post-migration.py
@@ -5,7 +5,7 @@ from openupgradelib import openupgrade
 def set_default_purchase_date(env):
     openupgrade.logged_query(
         env.cr, """
-            UPDATE sale_order_line 
+            UPDATE sale_order_line
                 SET purchase_date = write_date
             WHERE write_date is not NULL;
             """
