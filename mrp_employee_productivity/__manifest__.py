61cd0ca93c3cae4ac6dcc41267cfb904cb41930a
sergiocorato <sergiocorato@gmail.com>
2023-03-08 17:34:26 +0100

    [IMP] copier template

--- mrp_employee_productivity/__manifest__.py
+++ mrp_employee_productivity/__manifest__.py
@@ -6,7 +6,7 @@
     'license': 'AGPL-3',
     'category': 'Manufacturing',
     'author': 'Sergio Corato',
-    'website': 'https://efatto.it',
+    'website': 'https://github.com/sergiocorato/e-efatto',
     'description': 'View employee productivity time in employee view.',
     'depends': [
         'hr',
