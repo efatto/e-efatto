e3d05d3c821c51aecbf8d94e3a707bd2d23dff03
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:48:00 +0100

    [FIX] pre-commit, pylint, black, sort

--- sale_report_usability/report/sale_report.py
+++ sale_report_usability/report/sale_report.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleReport(models.Model):
