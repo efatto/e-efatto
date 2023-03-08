41b86294775703436ac451a011ebac4489abcc6a
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:53:53 +0100

    [FIX] license

--- l10n_it_country_data/__manifest__.py
+++ l10n_it_country_data/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2015-2020 Sergio Corato.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Italy - Country data",
@@ -10,7 +10,7 @@
     """,
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "depends": [
         "sale",
     ],
