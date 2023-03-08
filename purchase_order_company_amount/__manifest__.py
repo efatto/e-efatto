b256ce1e82cf9ce53a3219b31f3e6d8df562bf4e
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:40:55 +0100

    [FIX] manifest files

--- purchase_order_company_amount/__manifest__.py
+++ purchase_order_company_amount/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Add company currency untaxed amount to purchase order.",
     "version": "12.0.1.0.0",
     "category": "Purchases",
-    "website": "https://efatto.it",
+    "website": "https://github.com/sergiocorato/e-efatto",
     "author": "Sergio Corato",
     "license": "AGPL-3",
     "application": False,
