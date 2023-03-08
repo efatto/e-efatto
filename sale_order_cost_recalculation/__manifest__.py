176fa35b1e73caf8ce3f98a5a55f3d2c2ec1b97a
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:40:55 +0100

    [FIX] manifest files

--- sale_order_cost_recalculation/__manifest__.py
+++ sale_order_cost_recalculation/__manifest__.py
@@ -5,8 +5,9 @@
     'version': '12.0.1.0.5',
     'category': 'other',
     'author': 'Sergio Corato',
+    'license': 'AGPL-3',
     'summary': 'Add recalculation of cost when recalculating prices on sale order.',
-    'website': 'https://efatto.it',
+    'website': 'https://github.com/sergiocorato/e-efatto',
     'depends': [
         'sale_margin_security',
         'sale_order_price_recalculation',
