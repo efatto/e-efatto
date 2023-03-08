333b87f6f2ec4cb4338cae5715185924448c7e41
sergiocorato <sergiocorato@gmail.com>
2023-03-08 18:40:55 +0100

    [FIX] manifest files

--- product_archiver/__manifest__.py
+++ product_archiver/__manifest__.py
@@ -4,7 +4,8 @@
     'name': 'Product archiver',
     'version': '12.0.1.0.0',
     'author': 'Sergio Corato',
-    'website': 'https://efatto.it',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/sergiocorato/e-efatto',
     'category': 'Inventory',
     'depends': [
         'mrp',
