e74b3bc78e4329f2b20038d6e29b1ad7780ac01f
sergiocorato <sergiocorato@gmail.com>
2023-03-21 16:55:40 +0100

    [IMP] set default for generic usage

--- hr_attendance_work_hour/models/resource_calendar_attendance.py
+++ hr_attendance_work_hour/models/resource_calendar_attendance.py
@@ -5,10 +5,12 @@ class ResourceCalendarAttendance(models.Model):
     _inherit = "resource.calendar.attendance"
 
     hour_from_step = fields.Float(
+        default=0.5,
         help="Increase the extraordinary worked hours if work starts before this step "
              "or increase/decrease the ordinary worked hours if work starts after this "
              "step."
     )
     hour_to_step = fields.Float(
+        default=0.5,
         help="Increase the extraordinary worked hours if work ends after this step."
     )
