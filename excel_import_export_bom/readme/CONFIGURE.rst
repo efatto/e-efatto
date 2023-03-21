f97b580e0714a31ffa41644ef9565b8992c908bc
sergiocorato <sergiocorato@gmail.com>
2023-03-21 17:11:21 +0100

    [ADD] module to export-import bom

---
+++ excel_import_export_bom/readme/CONFIGURE.rst
@@ -0,0 +1,22 @@
+Nelle azioni della distinta base sono disponibili l'esportazione e l'importazione di Xlsx all'interno della distinta base stessa:
+
+.. image:: ../static/description/azione_esporta_importa_da_diba.png
+    :alt: Esporta ed importa da Di.Ba.
+
+da cui la procedura di esportazione:
+
+.. image:: ../static/description/esporta.png
+    :alt: Esporta
+
+e quella di importazione:
+
+.. image:: ../static/description/importa.png
+    :alt: Importa
+
+In base al tracciato creato di default, che può essere sostituito con un altro di propria preferenza.
+N.B.: il tracciato è configurato per aggiungere le righe a quelle esistenti, con l'opzione _NODEL_ all'inizio del nome del campo relativo alle righe.
+
+Nel menu `Anagrafica` della produzione è inoltre disponibile una voce per creare una nuova Di.Ba.:
+
+.. image:: ../static/description/menu_importa_nuova_diba.png
+    :alt: Menu importa nuova Di.Ba.
---
+++ excel_import_export_bom/tests/test_xlsx_template.py
@@ -0,0 +1,51 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+from ast import literal_eval
+from .test_common import TestExcelImportExport
+
+
+class TestXLSXTemplate(TestExcelImportExport):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestExcelImportExport, cls).setUpClass()
+
+    def test_xlsx_template(self):
+        """ Test XLSX Template input and output instruction """
+        self.setUpXLSXTemplate()
+        instruction_dict = literal_eval(self.sample_template.instruction)
+        self.assertDictEqual(
+            instruction_dict,
+            {
+                '__EXPORT__': {
+                    'mrp_bom': {
+                        '_HEAD_': {
+                            'A2': 'product_tmpl_id${value and value.barcode or value.default_code or value.name or ""}#{style=text}#??',
+                            'B2': 'product_qty${value or 0}#{style=number}#??',
+                            'C2': 'product_uom_id.name${value or ""}#{style=text}#??',
+                            'D2': 'routing_id.name${value or ""}#{style=text}#??',
+                        },
+                        'bom_line_ids': {
+                            'E2': 'product_id.barcode${value or ""}#{style=text}#??',
+                            'F2': 'product_qty${value or 0}#{style=number}#??',
+                            'G2': 'product_uom_id.name${value or ""}#{style=text}#??',
+                        }
+                    }
+                },
+                '__IMPORT__': {
+                    'mrp_bom': {
+                        'bom_line_ids': {
+                            'E2': 'product_id',
+                            'F2': 'product_qty',
+                            'G2': 'product_uom_id',
+                        }
+                    }
+                },
+                '__POST_IMPORT__': False
+            }
+        )
+        # Finally load excel file into this new template
+        self.assertFalse(self.sample_template.datas)  # Not yet loaded
+        self.template_obj.load_xlsx_template([self.sample_template.id],
+                                             addon='excel_import_export_bom')
+        self.assertTrue(self.sample_template.datas)  # Loaded successfully
