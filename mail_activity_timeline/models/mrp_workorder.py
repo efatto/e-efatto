8f66ebc0356fcdc2f8af981a9e7730d536e58b6c
sergiocorato <sergiocorato@gmail.com>
2023-04-11 12:33:05 +0200

    [IMP] add computed field on workorder for dates activities - search activity as write not already performed

--- mail_activity_timeline/models/mrp_workorder.py
+++ mail_activity_timeline/models/mrp_workorder.py
@@ -10,6 +10,23 @@ class MrpWorkorder(models.Model):
         string='Assigned user',
         index=True,
     )
+    activity_date_start = fields.Datetime(
+        compute="_compute_dates",
+        store=True,
+    )
+    activity_date_end = fields.Datetime(
+        compute="_compute_dates",
+        store=True,
+    )
+
+    @api.multi
+    @api.depends('activity_ids.date_start', 'activity_ids.date_end')
+    def _compute_dates(self):
+        for workorder in self:
+            workorder.activity_date_start = min(
+                workorder.mapped("activity_ids.date_start") or [False])
+            workorder.activity_date_end = max(
+                workorder.mapped("activity_ids.date_end") or [False])
 
     @api.model
     def create(self, vals_list):
@@ -28,9 +45,11 @@ class MrpWorkorder(models.Model):
         res = super().write(values)
         if not self.env.context.get('bypass_resource_planner'):
             for workorder in self:
-                activity_ids = workorder.activity_ids.filtered(
-                    lambda x: x.is_resource_planner
-                )
+                # search as activities could be not linked at the moment of write
+                activity_ids = self.env['mail.activity'].search([
+                    ('res_model', '=', workorder._name),
+                    ('res_id', '=', workorder.id),
+                ])
                 if activity_ids:
                     if any(x in values for x in [
                         'date_planned_start',
