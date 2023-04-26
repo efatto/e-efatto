7a61c327f0a428e173c895b493315b2c4ec59358
sergiocorato <sergiocorato@gmail.com>
2023-04-26 17:44:04 +0200

    [FIX] remove logic from sale_order_approved_customer

--- sale_stock_partner_deposit/tests/test_sale_stock_partner_deposit.py
+++ sale_stock_partner_deposit/tests/test_sale_stock_partner_deposit.py
@@ -177,9 +177,6 @@ class TestSaleStockPartnerDeposit(SavepointCase):
         # Sell remaining 2 products in deposit
         self._create_sale_order_line(sale_order, self.product, 2.0)
         sale_order.action_confirm()
-        if sale_order.state != 'sale':
-            # do the second confirmation to comply extra state 'approved'
-            sale_order.action_confirm()
         picking = sale_order.picking_ids[0]
         self.assertEqual(picking.location_id, deposit_location)
         picking.action_assign()
