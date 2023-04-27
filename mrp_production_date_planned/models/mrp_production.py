849004874815975f39eb29e46e846c86c6026716
sergiocorato <sergiocorato@gmail.com>
2023-04-27 17:06:51 +0200

    [IMP] create workorder with sequential date on duration expected

--- mrp_production_date_planned/models/mrp_production.py
+++ mrp_production_date_planned/models/mrp_production.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, api
+from odoo import api, fields, models
 from odoo.tools.date_utils import relativedelta
 
 
@@ -10,11 +10,13 @@ class MrpProduction(models.Model):
     @api.multi
     def _generate_workorders(self, exploded_boms):
         workorders = super()._generate_workorders(exploded_boms)
-        if self.date_planned_start:
-            for workorder in workorders:
-                workorder.write(dict(
-                    date_planned_start=self.date_planned_start,
-                    date_planned_finished=self.date_planned_start +
-                    relativedelta(minutes=max(workorder.duration_expected or 0, 1))
-                ))
+        date_planned_start = self.date_planned_start or fields.Datetime.now()
+        for workorder in workorders.sorted(key="id"):
+            date_planned_finished = date_planned_start + \
+                relativedelta(minutes=max(workorder.duration_expected or 0, 1))
+            workorder.write(dict(
+                date_planned_start=date_planned_start,
+                date_planned_finished=date_planned_finished,
+            ))
+            date_planned_start = date_planned_finished
         return workorders
