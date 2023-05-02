849610a015d379cd38b45c907c42c911d976473a
sergiocorato <sergiocorato@gmail.com>
2023-05-02 15:49:49 +0200

    [IMP] removed cost_at_date

--- mrp_bom_evaluation/models/stock_move.py
+++ mrp_bom_evaluation/models/stock_move.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
@@ -8,26 +8,3 @@ class StockMove(models.Model):
         related='bom_line_id.price_unit',
         string="Bom Line Price Unit",
     )
-    cost_at_date = fields.Float(
-        compute='_compute_cost_at_date'
-    )
-
-    @api.multi
-    def _compute_cost_at_date(self):
-        # get last purchase price before move date
-        for move in self:
-            if move.product_id:
-                domain = [
-                    ('product_id', '=', move.product_id.id),
-                    ('date_order', '<=', move.date),
-                    ('state', 'in', ['purchase', 'done']),
-                    ('price_unit', '!=', 0),
-                ]
-                order_lines = self.env['purchase.order.line'].search(
-                    domain).sorted(key='date_order', reverse=True)
-                if order_lines:
-                    move.cost_at_date = order_lines[0]._get_discounted_price_unit()
-                else:
-                    move.cost_at_date = move.product_id.standard_price
-            else:
-                move.cost_at_date = move.product_id.standard_price
