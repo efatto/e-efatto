2965046a4f7e800015107499fe9083d136274f34
sergiocorato <sergiocorato@gmail.com>
2023-05-07 17:33:47 +0200

    [FIX] removed duplicated code from parent modules

--- procurement_purchase_analytic_grouping/models/stock_rule.py
+++ procurement_purchase_analytic_grouping/models/stock_rule.py
@@ -13,15 +13,6 @@ class StockRule(models.Model):
         ):
             grouping = product_id.categ_id.procured_purchase_grouping
             self_wc = self.with_context(grouping=grouping)
-            if values.get('group_id') and not values.get('account_analytic_id'):
-                group_id = values['group_id']
-                mrp_id = self.env['mrp.production'].search([
-                    ('name', '=', group_id.name),
-                ])
-                if mrp_id.analytic_account_id:
-                    values.update({
-                        'account_analytic_id': mrp_id.analytic_account_id.id,
-                    })
             return super(StockRule, self_wc)._run_buy(
                 product_id, product_qty, product_uom, location_id, name,
                 origin, values)
@@ -34,8 +25,10 @@ class StockRule(models.Model):
         domain = super()._make_po_get_domain(values, partner)
         if self.env.context.get('grouping', 'standard') == 'analytic':
             if values.get("account_analytic_id"):
-                domain += (
-                    ("order_line.account_analytic_id", "=",
-                     values["account_analytic_id"]),
+                domain_analytic = (
+                    "order_line.account_analytic_id", "=",
+                     values["account_analytic_id"]
                 )
+                if domain_analytic not in domain:
+                    domain += domain_analytic
         return domain
