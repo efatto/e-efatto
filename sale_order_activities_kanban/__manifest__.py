bf5a9c08d5af20b00a95408a492f98cab11945e1
sergiocorato <sergiocorato@gmail.com>
2023-05-16 16:19:38 +0200

    [ADD] module to show activities widget in sale order kanban

---
+++ sale_order_activities_kanban/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Activity widget on sale order kanban",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "Sergio Corato",
+    'license': 'AGPL-3',
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_view.xml",
+    ],
+    "installable": True
+}
---
+++ sale_order_activities_kanban/readme/USAGE.rst
@@ -0,0 +1,9 @@
+Questo è il widget aggiunto:
+
+.. image:: ../static/description/widget.png
+    :alt: Widget
+
+che apre il popup di default:
+
+.. image:: ../static/description/popup.png
+    :alt: Popup
