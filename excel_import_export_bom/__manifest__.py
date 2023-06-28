c185661d90c2ebf3bd624d14000b6dd801ecb335
sergiocorato <sergiocorato@gmail.com>
2023-06-28 15:24:37 +0200

    [FIX] license

--- excel_import_export_bom/__manifest__.py
+++ excel_import_export_bom/__manifest__.py
@@ -1,6 +1,5 @@
-# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Excel Import/Export Bom',
     'version': '12.0.1.0.1',
