1ecad57c3e52b8f040e18b0fc3b00d6b566011c2
sergiocorato <sergiocorato@gmail.com>
2023-07-05 23:56:05 +0200

    [WIP] remove compute function from sale_order_cost_recalculation

--- sale_order_cost_recalculation/__manifest__.py
+++ sale_order_cost_recalculation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Sale order cost recalculation',
-    'version': '12.0.1.0.5',
+    'version': '12.0.0.0.999',
     'category': 'other',
     'author': 'Sergio Corato',
     'license': 'AGPL-3',
