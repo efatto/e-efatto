c5b01e6b870c43cfb563ce2de3460074b575b12d
sergiocorato <sergiocorato@gmail.com>
2023-07-05 12:45:06 +0200

    [ADD] module to change production flow

---
+++ mrp_manual_procurement_subcontractor/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'MRP Manual Procurement Subcontractor',
+    'version': '12.0.1.0.0',
+    'development_status': 'Beta',
+    'license': 'AGPL-3',
+    'category': 'Manufacturing',
+    'author': 'Sergio Corato',
+    'website': 'https://github.com/sergiocorato/e-efatto',
+    'summary': 'Add ability to create procurement after production order '
+               'creation selecting a subcontractor.',
+    'depends': [
+        'mrp_production_demo',
+        'mrp_subcontracting_purchase_link',
+        'purchase_line_procurement_group',
+    ],
+    'data': [
+        'wizard/mrp_production_procure_subcontractor.xml',
+        'views/mrp.xml',
+    ],
+    'installable': True,
+}
