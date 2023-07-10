4126f5441803e79db51b999a18c6b34362fadb7d
sergiocorato <sergiocorato@gmail.com>
2023-07-10 10:12:09 +0200

    [WIP] test with subcontractor

--- mrp_manual_procurement_subcontractor/tests/test_mrp_manual_procurement_subcontractor.py
+++ mrp_manual_procurement_subcontractor/tests/test_mrp_manual_procurement_subcontractor.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo.addons.mrp_production_demo.tests.common_data import TestProductionData
+from odoo.tests import Form
 from odoo.tools import mute_logger
 
 
@@ -22,13 +23,17 @@ class TestMrpProductionManualProcurement(TestProductionData):
         cls.partner_1 = cls.env['res.partner'].create({
             'name': 'Test partner',
         })
-        cls.subcontractor_partner1 = cls.env.ref('base.res_partner_4')
+        cls.subcontractor_partner1 = cls.env['res.partner'].create({
+            'name': 'Subcontractor 1',
+        })
         cls.subcontractor_partner1.property_stock_subcontractor = (
             partner_subcontract_location.id)
         supplierinfo_1 = cls.env['product.supplierinfo'].create({
             'name': cls.subcontractor_partner1.id,
         })
-        cls.subcontractor_partner2 = cls.env.ref('base.res_partner_2')
+        cls.subcontractor_partner2 = cls.env['res.partner'].create({
+            'name': 'Subcontractor 2',
+        })
         cls.subcontractor_partner2.property_stock_subcontractor = (
             partner_subcontract_location.id)
         supplierinfo_2 = cls.env['product.supplierinfo'].create({
@@ -39,7 +44,9 @@ class TestMrpProductionManualProcurement(TestProductionData):
             'purchase_ok': True,
             'route_ids': [
                 (4, cls.env.ref('purchase_stock.route_warehouse0_buy').id),
-                (4, resupply_sub_on_order_route.id)],
+                (4, resupply_sub_on_order_route.id),
+                # (3, cls.env.ref('stock.route_warehouse0_mto').id),
+            ],
             'seller_ids': [(6, 0, [supplierinfo_1.id, supplierinfo_2.id])],
         })
         cls.main_bom_subcontracted = cls.main_bom.copy(
@@ -68,34 +75,9 @@ class TestMrpProductionManualProcurement(TestProductionData):
                 (4, resupply_sub_on_order_route.id)],
             'seller_ids': [(6, 0, [supplierinfo.id])],
         }])
-        cls.product_to_subcontract = cls.env['product.product'].create([{
-            'name': 'Subcontracted product',
-            'type': 'product',
-            'default_code': 'SUBCONTPROD',
-            'purchase_ok': True,
-            'route_ids': [
-                (4, cls.env.ref('purchase_stock.route_warehouse0_buy').id),
-                (4, cls.env.ref('mrp.route_warehouse0_manufacture').id),
-                (4, resupply_sub_on_order_route.id),
-            ],
-            'seller_ids': [(6, 0, [supplierinfo_1.id])],
-        }])
-        cls.subcontract_bom = cls.env['mrp.bom'].create({
-            'product_tmpl_id': cls.product_to_subcontract.product_tmpl_id.id,
-            'product_qty': 1,
-            'type': 'subcontract',
-            'subcontractor_ids': [(6, 0, [cls.subcontractor_partner1.id])],
-            'bom_line_ids': [
-                (0, 0, {
-                    'product_id': cls.product_to_purchase.id,
-                    'product_qty': 7,
-                    'product_uom_id': cls.product_to_purchase.uom_id.id,
-                })
-            ]
-        })
         cls.env['stock.warehouse.orderpoint'].create({
             'name': 'OPx',
-            'product_id': cls.product_to_subcontract.id,
+            'product_id': cls.top_product.id,
             'product_min_qty': 0,
             'product_max_qty': 0,
             'location_id': (
@@ -103,7 +85,7 @@ class TestMrpProductionManualProcurement(TestProductionData):
         })
         resupply_rule = resupply_sub_on_order_route.rule_ids.filtered(
             lambda l: (
-                l.location_id == cls.product_to_subcontract.property_stock_production
+                l.location_id == cls.top_product.property_stock_production
                 and l.location_src_id ==
                 cls.env.user.company_id.subcontracting_location_id))
         resupply_rule.copy({
@@ -131,7 +113,7 @@ class TestMrpProductionManualProcurement(TestProductionData):
         line._convert_to_write(line._cache)
         return line
 
-    def test_01_mo_from_sale_with_subcontracting(self):
+    def test_01_mo_from_sale_with_subcontracting_and_orderpoint(self):
         self.assertTrue(
             self.top_product.mapped('seller_ids.is_subcontractor')
         )
@@ -155,46 +137,68 @@ class TestMrpProductionManualProcurement(TestProductionData):
             ('order_line.product_id', 'in', self.top_product.ids),
         ])
         self.assertFalse(po_ids)
-        self.production.with_context(
-            test_mrp_manual_procurement_subcontractor=True
-        ).button_start_procurement()
-        # run scheduler to start orderpoint rule
-        with mute_logger('odoo.addons.stock.models.procurement'):
-            self.procurement_model.run_scheduler()
-        # todo select subcontrator in production
+        # continue production with subcontrator
+        procure_form = Form(
+            self.env["mrp.production.procure.subcontractor"].with_context(
+                active_id=self.production.id,
+                active_ids=[self.production.id],
+            )
+        )
+        procure_form.subcontractor_id = self.subcontractor_partner2
+        wizard = procure_form.save()
+        wizard.action_done()
         to_confirm_po_ids = self.env['purchase.order'].search([
             ('order_line.product_id', 'in', self.top_product.ids),
         ])
+        self.assertEqual(len(to_confirm_po_ids), 1)
+        # check vendor is equal to selected subcontractor
+        self.assertEqual(to_confirm_po_ids.partner_id, self.subcontractor_partner2)
+        self.assertEqual(len(to_confirm_po_ids.mapped('order_line')), 1)
+        to_confirm_po_ids.button_confirm()
+        self.assertEqual(to_confirm_po_ids.state, 'purchase')
+
+        # todo test
+        # continue with normal production
+        # self.production.with_context(
+        #     test_mrp_manual_procurement_subcontractor=True
+        # ).button_start_procurement()
+        # # run scheduler to start orderpoint rule
+        # with mute_logger('odoo.addons.stock.models.procurement'):
+        #     self.procurement_model.run_scheduler()
+        # # todo select subcontrator in production
+        # to_confirm_po_ids = self.env['purchase.order'].search([
+        #     ('order_line.product_id', 'in', self.top_product.ids),
+        # ])
         # self.assertEqual(len(to_confirm_po_ids), 1)  # FIXME
         # self.assertEqual(len(to_confirm_po_ids.mapped('order_line')), 1)
-        # todo check vendor is equal to selected subcontractor
+        # # todo check vendor is equal to selected subcontractor
         # to_confirm_po_ids.button_confirm()
         # self.assertEqual(to_confirm_po_ids.state, 'purchase')
 
-    def test_02_mo_from_sale_without_subcontracting(self):
-        product_qty = 3
-        sale_order = self.env['sale.order'].create({
-            'partner_id': self.partner_1.id,
-        })
-        self._create_sale_order_line(sale_order, self.top_product, product_qty)
-        sale_order.with_context(
-            test_mrp_manual_procurement_subcontractor=True
-        ).action_confirm()
-        # check procurement has not created RDP, even launching scheduler (which will
-        # do nothing anyway)
-        with mute_logger('odoo.addons.stock.models.procurement'):
-            self.procurement_model.run_scheduler()
-        self.production = self.env['mrp.production'].search(
-            [('origin', '=', sale_order.name)])
-        self.assertTrue(self.production)
-        po_ids = self.env['purchase.order'].search([
-            ('origin', '=', self.production.name),
-            ('state', '=', 'draft'),
-            ('order_line.product_id', 'not in', self.top_product.ids),
-        ])
-        self.assertTrue(po_ids)
-        self.assertEqual(len(po_ids), 1)
-        self.assertEqual(len(po_ids.mapped('order_line')), 3)
-        po_lines = po_ids.order_line.filtered(
-            lambda x: x.product_id == self.top_product)
-        self.assertFalse(po_lines)
+    # def test_02_mo_from_sale_without_subcontracting(self):
+    #     product_qty = 3
+    #     sale_order = self.env['sale.order'].create({
+    #         'partner_id': self.partner_1.id,
+    #     })
+    #     self._create_sale_order_line(sale_order, self.top_product, product_qty)
+    #     sale_order.with_context(
+    #         test_mrp_manual_procurement_subcontractor=True
+    #     ).action_confirm()
+    #     # check procurement has not created RDP, even launching scheduler (which will
+    #     # do nothing anyway)
+    #     with mute_logger('odoo.addons.stock.models.procurement'):
+    #         self.procurement_model.run_scheduler()
+    #     self.production = self.env['mrp.production'].search(
+    #         [('origin', '=', sale_order.name)])
+    #     self.assertTrue(self.production)
+    #     po_ids = self.env['purchase.order'].search([
+    #         ('origin', '=', self.production.name),
+    #         ('state', '=', 'draft'),
+    #         ('order_line.product_id', 'not in', self.top_product.ids),
+    #     ])
+    #     self.assertTrue(po_ids)
+    #     self.assertEqual(len(po_ids), 1)
+    #     self.assertEqual(len(po_ids.mapped('order_line')), 3)
+    #     po_lines = po_ids.order_line.filtered(
+    #         lambda x: x.product_id == self.top_product)
+    #     self.assertFalse(po_lines)
