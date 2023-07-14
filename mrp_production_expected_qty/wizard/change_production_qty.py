b50e29fd0b126c742fe2895967f1f690eef56684
sergiocorato <sergiocorato@gmail.com>
2023-07-14 17:52:34 +0200

    [FIX] change logic to get qty expected from original bom line

--- mrp_production_expected_qty/wizard/change_production_qty.py
+++ mrp_production_expected_qty/wizard/change_production_qty.py
@@ -7,15 +7,14 @@ class ChangeProductionQty(models.TransientModel):
     @api.multi
     def change_prod_qty(self):
         for wizard in self:
+            # recompute qty for expected bom lines with original linked bom qty
             production = wizard.mo_id
-            if wizard.product_qty != production.product_qty:
-                rate = wizard.product_qty / production.product_qty
-                # factor = production.product_uom_id._compute_quantity(
-                #     wizard.product_qty, production.bom_id.product_uom_id
-                # ) / production.bom_id.product_qty
-                for move in production.move_raw_ids.filtered(
-                    'expected_product_uom_qty'
-                ):
-                    new_qty = move.expected_product_uom_qty * rate
-                    move.expected_product_uom_qty = new_qty
+            factor = production.product_uom_id._compute_quantity(
+                    wizard.product_qty, production.bom_id.product_uom_id
+                ) / production.bom_id.product_qty
+            for move in production.move_raw_ids.filtered(
+                lambda x: x.expected_product_uom_qty and x.bom_line_id
+            ):
+                new_qty = move.bom_line_id.product_qty * factor
+                move.expected_product_uom_qty = new_qty
         return super().change_prod_qty()
