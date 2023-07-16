4e320d15ba2d00bc4544eda67dc1bd514ab14bd9
sergiocorato <sergiocorato@gmail.com>
2023-07-16 19:21:07 +0200

    [FIX] expected qty with sub bom

--- mrp_production_expected_qty/wizard/change_production_qty.py
+++ mrp_production_expected_qty/wizard/change_production_qty.py
@@ -1,4 +1,5 @@
-from odoo import api, models
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
 
 
 class ChangeProductionQty(models.TransientModel):
@@ -9,12 +10,18 @@ class ChangeProductionQty(models.TransientModel):
         for wizard in self:
             # recompute qty for expected bom lines with original linked bom qty
             production = wizard.mo_id
-            factor = production.product_uom_id._compute_quantity(
-                wizard.product_qty, production.bom_id.product_uom_id
-            ) / production.bom_id.product_qty
             for move in production.move_raw_ids.filtered(
                 lambda x: x.expected_product_uom_qty and x.bom_line_id
             ):
+                factor = production.product_uom_id._compute_quantity(
+                    wizard.product_qty, production.bom_id.product_uom_id
+                ) / production.bom_id.product_qty
+                if move.expected_product_uom_qty != move.bom_line_id.product_qty:
+                    factor *= (
+                        move.expected_product_uom_qty
+                        / move.bom_line_id.product_qty
+                        / production.product_qty
+                    )
                 new_qty = move.bom_line_id.product_qty * factor
                 move.expected_product_uom_qty = new_qty
         return super().change_prod_qty()
