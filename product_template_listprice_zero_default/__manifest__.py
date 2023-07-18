0299bf7e34d71626c1dae8211e0f7785a3f3fdd9
sergiocorato <sergiocorato@gmail.com>
2023-07-18 18:22:00 +0200

    [IMP] product_template_listprice_zero_default: black, isort, prettier

--- product_template_listprice_zero_default/__manifest__.py
+++ product_template_listprice_zero_default/__manifest__.py
@@ -1,17 +1,16 @@
 # Copyright 2016-2020 Sergio Corato
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Product template list_price default zero',
-    'version': '12.0.1.0.0',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'description': 'Product template set default list price to zero',
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'license': 'AGPL-3',
-    'depends': [
-        'product',
+    "name": "Product template list_price default zero",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "summary": "Product template set default list price to zero",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
     ],
-    'data': [
-    ],
-    'installable': True
+    "data": [],
+    "installable": True,
 }
