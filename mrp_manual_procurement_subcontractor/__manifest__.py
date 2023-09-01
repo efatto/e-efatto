d29b7147aa3572000195b8d37b057bd0a66f6209
sergiocorato <sergiocorato@gmail.com>
2023-09-01 14:17:36 +0200

    [FIX] remove excludes

--- mrp_manual_procurement_subcontractor/__manifest__.py
+++ mrp_manual_procurement_subcontractor/__manifest__.py
@@ -16,21 +16,6 @@
         'purchase_line_procurement_group',
         'sale',  # for test purposes
     ],
-    'excludes': [
-        # mrp_bom_attachment_export
-        # mrp_bom_evaluation
-        # mrp_bom_evaluation_simple
-        # mrp_bom_line_all_attachments
-        # mrp_bom_line_group
-        # mrp_bom_operation_estimate
-        # mrp_bom_sale_pricelist
-        # mrp_employee_productivity
-        # mrp_production_active_move_line_fix
-        # mrp_production_bom_create
-        # mrp_production_calendar
-        # mrp_production_change_product_fix
-        # mrp_production_component_delete
-    ],
     'data': [
         'wizard/mrp_production_procure_subcontractor.xml',
         'views/mrp.xml',
