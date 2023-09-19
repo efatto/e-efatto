b8321d0b47bb341d09b33256671de0d4758e6306
sergiocorato <sergiocorato@gmail.com>
2023-09-19 09:18:35 +0200

    [IMP] purchase_autocomplete_no_invoice: black, isort, prettier and [MIG] to 14.0

--- purchase_autocomplete_no_invoice/report/purchase_bill.py
+++ purchase_autocomplete_no_invoice/report/purchase_bill.py
@@ -1,16 +1,16 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, tools
+from odoo import models, tools
 
 
 class PurchaseBillUnion(models.Model):
-    _inherit = 'purchase.bill.union'
+    _inherit = "purchase.bill.union"
 
-    @api.model_cr
     def init(self):
-        tools.drop_view_if_exists(self.env.cr, 'purchase_bill_union')
-        self.env.cr.execute("""
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute(
+            """
             CREATE OR REPLACE VIEW purchase_bill_union AS (
                 SELECT
                     id, name, partner_ref as reference, partner_id,
@@ -20,4 +20,5 @@ class PurchaseBillUnion(models.Model):
                 WHERE
                     state in ('purchase', 'done') AND
                     invoice_status in ('to invoice', 'no')
-            )""")
+            )"""
+        )
