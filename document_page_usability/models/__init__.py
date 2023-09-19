0577a6195c80e70cb4d057753121636c60c8ca3b
sergiocorato <sergiocorato@gmail.com>
2023-09-19 14:48:05 +0200

    [IMP] document_page_usability: black, isort, prettier and [MIG] to 14.0

--- document_page_usability/models/__init__.py
+++ document_page_usability/models/__init__.py
@@ -1,2 +1,1 @@
-
 from . import document_page
--- document_page_usability/models/document_page.py
+++ document_page_usability/models/document_page.py
@@ -1,19 +1,19 @@
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2023 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class DocumentPage(models.Model):
     _inherit = "document.page"
 
-    @api.multi
     def _inverse_content(self):
         for rec in self:
-            if rec.type == 'content' and \
-                    rec.content != rec.history_head.content:
-                rec._create_history({
-                    'name': rec.draft_name or '',
-                    'summary': rec.draft_summary or '',
-                    'content': rec.content,
-                })
+            if rec.type == "content" and rec.content != rec.history_head.content:
+                rec._create_history(
+                    {
+                        "name": rec.draft_name or "",
+                        "summary": rec.draft_summary or "",
+                        "content": rec.content,
+                    }
+                )
