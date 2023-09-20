362ff0f4fc707b2d73dc9fa61da45989d6f437b6
sergiocorato <sergiocorato@gmail.com>
2023-09-20 11:25:34 +0200

    [IMP] l10n_it_intrastat_tariff: black, isort, prettier and [MIG] to 14.0

--- l10n_it_intrastat_tariff/__manifest__.py
+++ l10n_it_intrastat_tariff/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Intrastat Customs Tariff',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
+    "name": "Intrastat Customs Tariff",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    'license': 'AGPL-3',
-    'category': 'other',
-    'depends': [
-        'l10n_it_intrastat',
+    "license": "AGPL-3",
+    "category": "other",
+    "depends": [
+        "l10n_it_intrastat",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/report_intrastat.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/report_intrastat.xml",
     ],
-    'description': "Add Customs Tariff to Intrastat Code",
-    'installable': True,
+    "summary": "Add Customs Tariff to Intrastat Code",
+    "installable": True,
 }
