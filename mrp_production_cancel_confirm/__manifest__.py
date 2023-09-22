b89c29bdf9366c7d8d7df01ba6304ad74f064d99
sergiocorato <sergiocorato@gmail.com>
2023-09-22 08:58:30 +0200

    [IMP] mrp_production_cancel_confirm: black, isort, prettier and [MIG] to 14.0

--- mrp_production_cancel_confirm/__manifest__.py
+++ mrp_production_cancel_confirm/__manifest__.py
@@ -1,20 +1,20 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Mrp production cancel confirm',
-    'version': '12.0.1.0.0',
-    'category': 'Manufacture',
-    'license': 'AGPL-3',
-    'description': """
-    Add confirm button to mrp.
+    "name": "Mrp production cancel confirm",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "license": "AGPL-3",
+    "summary": """
+    Add confirm button to action of cancel an mrp production.
     """,
-    'author': "Sergio Corato",
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'depends': [
-        'mrp',
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "depends": [
+        "mrp",
     ],
-    'data': [
-        'views/mrp_production.xml',
+    "data": [
+        "views/mrp_production.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
