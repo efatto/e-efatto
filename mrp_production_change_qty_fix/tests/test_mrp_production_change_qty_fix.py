aca5d64a27cc10110c558e5fb8a226f287ce9208
sergiocorato <sergiocorato@gmail.com>
2023-09-22 09:06:13 +0200

    [IMP] mrp_production_change_qty_fix black isort prettier

--- mrp_production_change_qty_fix/tests/test_mrp_production_change_qty_fix.py
+++ mrp_production_change_qty_fix/tests/test_mrp_production_change_qty_fix.py
@@ -5,7 +5,6 @@ from odoo.addons.mrp_production_demo.tests.common_data import TestProductionData
 
 
 class TestMrpProductionChangeQty(TestProductionData):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -15,38 +14,49 @@ class TestMrpProductionChangeQty(TestProductionData):
         # -> bom_2 (product_5):2 = product_3:3 [phantom]
         # -> bom_1 (product_4):2 = product_2:2 + subproduct_1_1:4 [normal]
         # total 4 mo lines: product_2:12, product_4:8, product_4:4, product_3:6
-        man_order = self.env['mrp.production'].create({
-            'name': 'MO-Test',
-            'product_id': self.top_product.id,
-            'product_uom_id': self.top_product.uom_id.id,
-            'product_qty': 1,
-            'bom_id': self.main_bom.id,
-        })
+        man_order = self.env["mrp.production"].create(
+            {
+                "name": "MO-Test",
+                "product_id": self.top_product.id,
+                "product_uom_id": self.top_product.uom_id.id,
+                "product_qty": 1,
+                "bom_id": self.main_bom.id,
+            }
+        )
         self.assertEqual(len(man_order.move_raw_ids), 3)
-        self.env['change.production.qty'].create({
-            'mo_id': man_order.id,
-            'product_qty': man_order.product_qty,
-        }).change_prod_qty()
+        self.env["change.production.qty"].create(
+            {
+                "mo_id": man_order.id,
+                "product_qty": man_order.product_qty,
+            }
+        ).change_prod_qty()
         self.assertEqual(len(man_order.move_raw_ids), 3)
         # add 1 line in sub-bom phantom
-        self.sub_bom1.write({
-            'bom_line_ids': [
-                (0, 0, {'product_id': self.subproduct_1_1.id, 'product_qty': 4})
-            ]
-        })
-        self.env['change.production.qty'].create({
-            'mo_id': man_order.id,
-            'product_qty': man_order.product_qty,
-        }).change_prod_qty()
+        self.sub_bom1.write(
+            {
+                "bom_line_ids": [
+                    (0, 0, {"product_id": self.subproduct_1_1.id, "product_qty": 4})
+                ]
+            }
+        )
+        self.env["change.production.qty"].create(
+            {
+                "mo_id": man_order.id,
+                "product_qty": man_order.product_qty,
+            }
+        ).change_prod_qty()
         self.assertEqual(len(man_order.move_raw_ids), 4)
 
         # remove 1 line in sub-bom phantom
         self.sub_bom2.bom_line_ids.filtered(
-            lambda x: x.product_id.id == self.subproduct_1_1.id).unlink()
-        self.env['change.production.qty'].create({
-            'mo_id': man_order.id,
-            'product_qty': man_order.product_qty,
-        }).change_prod_qty()
-        self.assertEqual(len(man_order.move_raw_ids.filtered(
-            lambda x: x.state != "cancel"
-        )), 3)
+            lambda x: x.product_id.id == self.subproduct_1_1.id
+        ).unlink()
+        self.env["change.production.qty"].create(
+            {
+                "mo_id": man_order.id,
+                "product_qty": man_order.product_qty,
+            }
+        ).change_prod_qty()
+        self.assertEqual(
+            len(man_order.move_raw_ids.filtered(lambda x: x.state != "cancel")), 3
+        )
--- mrp_production_change_qty_fix/wizard/mrp_production.py
+++ mrp_production_change_qty_fix/wizard/mrp_production.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
-
 import logging
+
+from odoo import models
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +10,6 @@ logger = logging.getLogger(__name__)
 class ChangeProductionQty(models.TransientModel):
     _inherit = "change.production.qty"
 
-    @api.multi
     def change_prod_qty(self):
         res = super(ChangeProductionQty, self).change_prod_qty()
         for wizard in self:
@@ -20,10 +18,10 @@ class ChangeProductionQty(models.TransientModel):
             production = wizard.mo_id
             for move_raw in production.move_raw_ids:
                 if not move_raw.bom_line_id and move_raw.product_uom_qty != 0:
-                    if move_raw.state == 'draft':
+                    if move_raw.state == "draft":
                         move_raw.unlink()
-                    elif move_raw.state != 'done':
+                    elif move_raw.state != "done":
                         move_raw._action_cancel()
                     else:
-                        logger.info('Move %s is already done' % move_raw.id)
+                        logger.info("Move %s is already done" % move_raw.id)
         return res
