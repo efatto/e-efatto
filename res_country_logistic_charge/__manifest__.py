b8f2973c8d397f5e7d6d33df2ddc65d3fd6b6641
sergiocorato <sergiocorato@gmail.com>
2023-10-11 09:49:38 +0200

    [IMP] res_country_logistic_charge: black, isort, prettier and Migration to 14.0

--- res_country_logistic_charge/__manifest__.py
+++ res_country_logistic_charge/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Country group logistic charge',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
+    "name": "Country group logistic charge",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    'license': 'AGPL-3',
-    'category': 'other',
-    'depends': [
-        'base',
+    "license": "AGPL-3",
+    "category": "other",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'views/res_country_group.xml',
+    "data": [
+        "views/res_country_group.xml",
     ],
-    'description': "Add a percentage value of logistic costs to be paid when purchasing"
-                   " from countries in a country group.",
-    'installable': True,
+    "summary": "Add a percentage value of logistic costs to be paid when purchasing"
+    " from countries in a country group.",
+    "installable": True,
 }
