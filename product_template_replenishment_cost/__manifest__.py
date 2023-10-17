7448e29f447ca4c8d5a934036cb8522f808004b7
sergiocorato <sergiocorato@gmail.com>
2023-10-17 16:12:39 +0200

    [IMP] product_template_replenishment_cost: black, isort, prettier and Migration to 14.0

--- product_template_replenishment_cost/__manifest__.py
+++ product_template_replenishment_cost/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Product Template Replenishment Cost',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
+    "name": "Product Template Replenishment Cost",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    'license': 'AGPL-3',
-    'category': 'Products',
-    'depends': [
-        'product',
+    "license": "AGPL-3",
+    "category": "Products",
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_product.xml',
-        'views/product_template.xml',
+    "data": [
+        "views/product_product.xml",
+        "views/product_template.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- product_template_replenishment_cost/readme/CONFIGURE.rst
+++ product_template_replenishment_cost/readme/CONFIGURE.rst
@@ -2,4 +2,3 @@ Il campo è configurabile nel prodotto o nel template:
 
 .. image:: ../static/description/costo.png
     :alt: Costo
-
