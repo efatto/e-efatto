561f1a9a8ff5e8010138c40d91f56543ae40ccce
sergiocorato <sergiocorato@gmail.com>
2023-10-17 08:25:23 +0200

    [IMP] mrp_production_child_origin: black, isort, prettier and Migration to 14.0

--- mrp_production_child_origin/__manifest__.py
+++ mrp_production_child_origin/__manifest__.py
@@ -1,17 +1,16 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'MRP Child Origin',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
+    "name": "MRP Child Origin",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    'description': 'Extend origin in child production to ease search.',
-    'license': 'AGPL-3',
-    'category': 'Manufacture',
-    'depends': [
-        'mrp',
+    "summary": "Extend origin in child production to ease search.",
+    "license": "AGPL-3",
+    "category": "Manufacture",
+    "depends": [
+        "mrp",
     ],
-    'data': [
-    ],
-    'installable': True,
+    "data": [],
+    "installable": True,
 }
