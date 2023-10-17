90b7dc217c430add0280b50c964855e5e1b11c34
sergiocorato <sergiocorato@gmail.com>
2023-10-17 08:25:23 +0200

    [IMP] mrp_production_child_origin: black, isort, prettier and Migration to 14.0

--- mrp_production_child_origin/models/stock_rule.py
+++ mrp_production_child_origin/models/stock_rule.py
@@ -2,15 +2,32 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
     def _prepare_mo_vals(
-            self, product_id, product_qty, product_uom, location_id, name, origin,
-            values, bom):
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        company_id,
+        values,
+        bom,
+    ):
         res = super()._prepare_mo_vals(
-            product_id, product_qty, product_uom, location_id, name, origin,
-            values, bom)
-        mo = self.env['mrp.production'].search([('name', '=', origin)])
-        if mo and mo.origin and mo.origin not in res['origin']:
-            res['origin'] = '/'.join([res['origin'], mo.origin])
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            company_id,
+            values,
+            bom,
+        )
+        mo = self.env["mrp.production"].search([("name", "=", origin)])
+        if mo and mo.origin and mo.origin not in res["origin"]:
+            res["origin"] = "/".join([res["origin"], mo.origin])
         return res
