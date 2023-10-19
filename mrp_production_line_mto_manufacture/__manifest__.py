45ee3702e51492815fc57510288f8c44b327eedf
sergiocorato <sergiocorato@gmail.com>
2023-10-19 15:03:41 +0200

    [IMP] mrp_production_line_mto_manufacture: black, isort, prettier and Migration to 14.0

--- mrp_production_line_mto_manufacture/__manifest__.py
+++ mrp_production_line_mto_manufacture/__manifest__.py
@@ -1,17 +1,17 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Production Group Line to Produce MTO',
-    'summary': 'Highlight line to produce MTO on MO components',
-    'version': '12.0.1.0.0',
-    'category': 'MRP',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'license': 'AGPL-3',
-    'maintainers': ['sergiocorato'],
-    'depends': ['mrp'],
-    'data': [
-        'views/mrp_production.xml',
+    "name": "Production Group Line to Produce MTO",
+    "summary": "Highlight line to produce MTO on MO components",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "license": "AGPL-3",
+    "maintainers": ["sergiocorato"],
+    "depends": ["mrp"],
+    "data": [
+        "views/mrp_production.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
