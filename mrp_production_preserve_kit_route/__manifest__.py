c066c55d90a47f345c33cf1d3db1b625720d3173
sergiocorato <sergiocorato@gmail.com>
2023-10-24 11:25:53 +0200

    [IMP] mrp_production_preserve_kit_route: black, isort, prettier and Migration to 14.0

--- mrp_production_preserve_kit_route/__manifest__.py
+++ mrp_production_preserve_kit_route/__manifest__.py
@@ -1,16 +1,15 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'MRP Preserve route for kit components',
-    'version': '12.0.1.0.1',
-    'development_status': 'Beta',
-    'license': 'AGPL-3',
-    'category': 'Manufacturing',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'depends': [
-        'mrp_production_demo',
-        'purchase_stock',
+    "name": "MRP Preserve route for kit components",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "depends": [
+        "mrp_production_demo",
+        "purchase_stock",
     ],
-    'installable': True,
+    "installable": True,
 }
--- mrp_production_preserve_kit_route/tests/test_mrp_production_preserve_kit_route.py
+++ mrp_production_preserve_kit_route/tests/test_mrp_production_preserve_kit_route.py
@@ -5,87 +5,121 @@ from odoo.addons.mrp_production_demo.tests.common_data import TestProductionData
 
 
 class TestMrpProductionPreserveKitRoute(TestProductionData):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        vendor = cls.env['res.partner'].create({
-            'name': 'Partner #2',
-        })
-        supplierinfo = cls.env['product.supplierinfo'].create({
-            'name': vendor.id,
-        })
+        vendor = cls.env["res.partner"].create(
+            {
+                "name": "Partner #2",
+            }
+        )
+        supplierinfo = cls.env["product.supplierinfo"].create(
+            {
+                "name": vendor.id,
+            }
+        )
         # Acoustic Bloc Screens, 16 on hand
-        cls.product1 = cls.env.ref('product.product_product_25')
+        cls.product1 = cls.env.ref("product.product_product_25")
         # Large Cabinet, 250 on hand
-        cls.product3 = cls.env.ref('product.product_product_6')
+        cls.product3 = cls.env.ref("product.product_product_6")
         # Drawer Black, 0 on hand
-        cls.product4 = cls.env.ref('product.product_product_16')
-        cls.product2 = cls.env['product.product'].create({
-            'name': 'Component product',
-            'default_code': 'code1234',
-            'type': 'product',
-            'purchase_ok': True,
-            'route_ids': [
-                (4, cls.env.ref('purchase_stock.route_warehouse0_buy').id),
-                (4, cls.env.ref('stock.route_warehouse0_mto').id)],
-            'seller_ids': [(6, 0, [supplierinfo.id])],
-        })
-        cls.product_bom = cls.env['product.product'].create({
-            'name': 'Product with bom',
-            'route_ids': [(4, cls.env.ref('mrp.route_warehouse0_manufacture').id),
-                          (4, cls.env.ref('stock.route_warehouse0_mto').id)],
-            'default_code': 'code123',
-            'type': 'product',
-            'sale_ok': True,
-        })
-        cls.product_kit = cls.env['product.product'].create({
-            'name': 'Product with kit bom',
-            'route_ids': [(4, cls.env.ref('mrp.route_warehouse0_manufacture').id),
-                          (4, cls.env.ref('stock.route_warehouse0_mto').id)],
-            'default_code': 'code1234',
-            'type': 'product',
-            'sale_ok': True,
-        })
-        kit_component_values = [{
-            'product_id': x.id,
-            'product_qty': 1,
-        } for x in cls.product1 | cls.product2]
-        cls.kit = cls.env['mrp.bom'].create({
-            'product_tmpl_id': cls.product_kit.product_tmpl_id.id,
-            'code': cls.product_kit.default_code,
-            'type': 'phantom',
-            'product_qty': 1,
-            'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
-            'bom_line_ids': [
-                (0, 0, x) for x in kit_component_values]
-        })
-        bom_component_values = [{
-            'product_id': x.id,
-            'product_qty': 1,
-        } for x in cls.product3 | cls.product4 | cls.product_kit]
-        cls.bom = cls.env['mrp.bom'].create({
-            'product_tmpl_id': cls.product_bom.product_tmpl_id.id,
-            'code': cls.product_bom.default_code,
-            'type': 'normal',
-            'product_qty': 1,
-            'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
-            'bom_line_ids': [
-                (0, 0, x) for x in bom_component_values]
-        })
+        cls.product4 = cls.env.ref("product.product_product_16")
+        cls.product2 = cls.env["product.product"].create(
+            {
+                "name": "Component product",
+                "default_code": "code1234",
+                "type": "product",
+                "purchase_ok": True,
+                "route_ids": [
+                    (4, cls.env.ref("purchase_stock.route_warehouse0_buy").id),
+                    (4, cls.env.ref("stock.route_warehouse0_mto").id),
+                ],
+                "seller_ids": [(6, 0, [supplierinfo.id])],
+            }
+        )
+        cls.product_bom = cls.env["product.product"].create(
+            {
+                "name": "Product with bom",
+                "route_ids": [
+                    (4, cls.env.ref("mrp.route_warehouse0_manufacture").id),
+                    (4, cls.env.ref("stock.route_warehouse0_mto").id),
+                ],
+                "default_code": "code123",
+                "type": "product",
+                "sale_ok": True,
+            }
+        )
+        cls.product_kit = cls.env["product.product"].create(
+            {
+                "name": "Product with kit bom",
+                "route_ids": [
+                    (4, cls.env.ref("mrp.route_warehouse0_manufacture").id),
+                    (4, cls.env.ref("stock.route_warehouse0_mto").id),
+                ],
+                "default_code": "code1234",
+                "type": "product",
+                "sale_ok": True,
+            }
+        )
+        kit_component_values = [
+            {
+                "product_id": x.id,
+                "product_qty": 1,
+            }
+            for x in cls.product1 | cls.product2
+        ]
+        cls.kit = cls.env["mrp.bom"].create(
+            {
+                "product_tmpl_id": cls.product_kit.product_tmpl_id.id,
+                "code": cls.product_kit.default_code,
+                "type": "phantom",
+                "product_qty": 1,
+                "product_uom_id": cls.env.ref("uom.product_uom_unit").id,
+                "bom_line_ids": [(0, 0, x) for x in kit_component_values],
+            }
+        )
+        bom_component_values = [
+            {
+                "product_id": x.id,
+                "product_qty": 1,
+            }
+            for x in cls.product3 | cls.product4 | cls.product_kit
+        ]
+        cls.bom = cls.env["mrp.bom"].create(
+            {
+                "product_tmpl_id": cls.product_bom.product_tmpl_id.id,
+                "code": cls.product_bom.default_code,
+                "type": "normal",
+                "product_qty": 1,
+                "product_uom_id": cls.env.ref("uom.product_uom_unit").id,
+                "bom_line_ids": [(0, 0, x) for x in bom_component_values],
+            }
+        )
 
     def test_so_bom_kit(self):
-        man_order = self.env['mrp.production'].create({
-            'name': 'MO-Test',
-            'product_id': self.product_bom.id,
-            'product_uom_id': self.product_bom.uom_id.id,
-            'product_qty': 1,
-            'bom_id': self.bom.id,
-        })
+        man_order = self.env["mrp.production"].create(
+            {
+                "name": "MO-Test",
+                "product_id": self.product_bom.id,
+                "product_uom_id": self.product_bom.uom_id.id,
+                "product_qty": 1,
+                "bom_id": self.bom.id,
+            }
+        )
         self.assertEqual(len(man_order.move_raw_ids), 4)
-        self.assertEqual(set(man_order.move_raw_ids.filtered(
-            lambda x: x.product_id != self.product2
-        ).mapped('procure_method')), {'make_to_stock'})
-        self.assertEqual(set(man_order.move_raw_ids.filtered(
-            lambda x: x.product_id == self.product2
-        ).mapped('procure_method')), {'make_to_order'})
+        self.assertEqual(
+            set(
+                man_order.move_raw_ids.filtered(
+                    lambda x: x.product_id != self.product2
+                ).mapped("procure_method")
+            ),
+            {"make_to_stock"},
+        )
+        self.assertEqual(
+            set(
+                man_order.move_raw_ids.filtered(
+                    lambda x: x.product_id == self.product2
+                ).mapped("procure_method")
+            ),
+            {"make_to_order"},
+        )
