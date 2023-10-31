8198984b7d5b96c7a0e5b03607c95b5fe460931e
sergiocorato <sergiocorato@gmail.com>
2023-10-31 18:07:56 +0100

    [IMP] purchase_order_company_amount: black, isort, prettier and Migration to 14.0

--- purchase_order_company_amount/models/purchase_order.py
+++ purchase_order_company_amount/models/purchase_order.py
@@ -2,25 +2,24 @@ from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     company_currency_id = fields.Many2one(
-        related='company_id.currency_id',
+        related="company_id.currency_id",
         string="Company Currency",
     )
     amount_untaxed_company_currency = fields.Monetary(
         compute="_compute_amount_untaxed_company_currency",
         store=True,
-        currency_field='company_currency_id',
+        currency_field="company_currency_id",
     )
 
-    @api.multi
-    @api.depends('currency_id', 'amount_untaxed', 'date_planned')
+    @api.depends("currency_id", "amount_untaxed", "date_planned")
     def _compute_amount_untaxed_company_currency(self):
         for order in self:
             order.amount_untaxed_company_currency = order.currency_id._convert(
                 order.amount_untaxed,
                 order.company_id.currency_id,
                 company=order.company_id,
-                date=order.date_planned
+                date=order.date_planned,
             )
