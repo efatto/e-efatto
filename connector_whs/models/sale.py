84a65d446b7eaf0398fafad29e79e98c8bc93997
sergiocorato <sergiocorato@gmail.com>
2023-11-14 21:55:32 +0100

    [IMP] add new logic for sql parameter

--- connector_whs/models/sale.py
+++ connector_whs/models/sale.py
@@ -1,11 +1,16 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import fields, models
+
+from odoo.addons.connector_whs.models.stock import EXTRA_PROCUREMENT_PRIORITIES
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    priority = fields.Selection(
+        selection_add=EXTRA_PROCUREMENT_PRIORITIES)
 
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
@@ -20,3 +25,10 @@ class SaleOrder(models.Model):
         for order in self:
             order.picking_ids.cancel_whs_list()
         return res
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    priority = fields.Selection(
+        selection_add=EXTRA_PROCUREMENT_PRIORITIES)
