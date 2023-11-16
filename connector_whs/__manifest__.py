fdcd407492d635f2da1a063be744319925ffc211
sergiocorato <sergiocorato@gmail.com>
2023-11-16 10:17:54 +0100

    [FIX] sqlalchemy version

--- connector_whs/__manifest__.py
+++ connector_whs/__manifest__.py
@@ -32,5 +32,5 @@
         "data/cron.xml",
     ],
     "installable": True,
-    "external_dependencies": {"python": ["sqlalchemy==1.4.50"]},
+    "external_dependencies": {"python": ["sqlalchemy==1.3.24"]},
 }
