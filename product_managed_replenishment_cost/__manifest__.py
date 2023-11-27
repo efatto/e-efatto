3f7e5e05ad02d18099d429a3678caf1d8e11b6ef
sergiocorato <sergiocorato@gmail.com>
2023-11-27 12:46:23 +0100

    [FIX] remove depends

--- product_managed_replenishment_cost/__manifest__.py
+++ product_managed_replenishment_cost/__manifest__.py
@@ -10,7 +10,6 @@
     "depends": [
         "l10n_it_intrastat_tariff",
         "mrp_subcontracting",
-        "web_progress",
         "product_logistics_uom",
         "product_template_replenishment_cost",
         "purchase_discount",
