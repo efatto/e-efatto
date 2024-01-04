ac3690ec29202a47efa0c2582071d91aee1c5ae1
sergiocorato <sergiocorato@gmail.com>
2024-01-04 08:32:08 +0100

    [IMP] add check for multiple lists

--- connector_whs/models/hyddemo_whs_liste.py
+++ connector_whs/models/hyddemo_whs_liste.py
@@ -84,6 +84,7 @@ class HyddemoWhsListe(models.Model):
     client_order_ref = fields.Text()  # size=50)
     product_customer_code = fields.Char(size=250)
     whs_list_absent = fields.Boolean()
+    whs_list_multiple = fields.Boolean()
     whs_list_log = fields.Text()
 
     def whs_unlink_lists(self, datasource_id):
