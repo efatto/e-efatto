e3d5ef0b9090a5a396d0f69797402881b685872b
sergiocorato <sergiocorato@gmail.com>
2024-02-06 10:16:33 +0100

    [FIX] tests

--- purchase_autocomplete_qty_to_invoice/models/account_move.py
+++ purchase_autocomplete_qty_to_invoice/models/account_move.py
@@ -17,20 +17,20 @@ class AccountMove(models.Model):
             # put only purchase order line with qty to invoice
             self.line_ids.unlink()
             # Copy purchase lines.
-            po_lines = purchase_id.order_line - self.line_ids.mapped(
-                'purchase_line_id')
-            new_lines = self.env['account.move.line']
-            sequence = max(
-                self.line_ids.mapped('sequence')) + 1 if self.line_ids else 10
+            po_lines = purchase_id.order_line - self.line_ids.mapped("purchase_line_id")
+            new_lines = self.env["account.move.line"]
+            sequence = (
+                max(self.line_ids.mapped("sequence")) + 1 if self.line_ids else 10
+            )
             for line in po_lines.filtered(
-                    lambda l: not l.display_type and l.qty_to_invoice != 0):
+                lambda l: not l.display_type and l.qty_to_invoice != 0
+            ):
                 line_vals = line._prepare_account_move_line(self)
-                line_vals.update({'sequence': sequence})
+                line_vals.update({"sequence": sequence})
                 new_line = new_lines.new(line_vals)
                 sequence += 1
                 new_line.account_id = new_line._get_computed_account()
                 new_line._onchange_price_subtotal()
                 new_lines += new_line
             new_lines._onchange_mark_recompute_taxes()
-        self._onchange_currency()
         return res
