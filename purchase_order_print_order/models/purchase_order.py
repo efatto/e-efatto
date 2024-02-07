a58992164b40ee39e6880301aafe11119bd3ed49
sergiocorato <sergiocorato@gmail.com>
2024-02-07 14:38:28 +0100

    [FIX] pre-commit

--- purchase_order_print_order/models/purchase_order.py
+++ purchase_order_print_order/models/purchase_order.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import models
 
 
 class PurchaseOrder(models.Model):
@@ -6,4 +6,4 @@ class PurchaseOrder(models.Model):
 
     def print_quotation(self):
         super().print_quotation()
-        return self.env.ref('purchase.action_report_purchase_order').report_action(self)
+        return self.env.ref("purchase.action_report_purchase_order").report_action(self)
