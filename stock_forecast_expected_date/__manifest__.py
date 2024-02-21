d6538093cf9ffe1694354712d3827de15a62008d
sergiocorato <sergiocorato@gmail.com>
2024-02-21 18:58:54 +0100

    [FIX] bump version

--- stock_forecast_expected_date/__manifest__.py
+++ stock_forecast_expected_date/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Make stock forecast expected date searchable",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Stock Management",
     "license": "AGPL-3",
     "author": "Sergio Corato",
