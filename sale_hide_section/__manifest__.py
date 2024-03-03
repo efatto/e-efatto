a068f12f033b23148c44120830e478e1940cbcba
sergiocorato <sergiocorato@gmail.com>
2024-03-03 21:37:21 +0100

    [ADD] module sale_hide_section

---
+++ sale_hide_section/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Widget to hide lines inside a section",
+    "version": "12.0.1.0.0",
+    "category": "Web",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_management",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/sale_views.xml",
+    ],
+    "installable": True
+}
