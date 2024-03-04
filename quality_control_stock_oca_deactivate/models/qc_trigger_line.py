bca512f2e70bb5bf5154521bb76a969bf81395eb
sergiocorato <sergiocorato@gmail.com>
2024-03-04 15:19:02 +0100

    [ADD] modules for quality control

---
+++ quality_control_stock_oca_deactivate/models/qc_trigger_line.py
@@ -0,0 +1,11 @@
+
+from odoo import fields, models
+
+
+class QcTriggerLine(models.AbstractModel):
+    _inherit = "qc.trigger.line"
+
+    success_number_to_deactivation = fields.Integer(
+        string="Deactivate after successfull tests number",
+        default=0)
+    active = fields.Boolean(string="Active", default=True)
