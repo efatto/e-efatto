d71a90468495b319e34495a527090f53ea442ff8
sergiocorato <sergiocorato@gmail.com>
2024-03-10 16:08:47 +0100

    [FIX] module name

--- quality_control_stock_oca_deactivate/__manifest__.py
+++ quality_control_stock_oca_deactivate/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Quality Control Stock OCA Block & Deactivate",
+    "name": "Quality Control Stock OCA Auto Deactivation",
     "version": "14.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
