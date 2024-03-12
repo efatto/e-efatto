2258eec23e4dde31cb33c70b43070842b15475e2
sergiocorato <sergiocorato@gmail.com>
2024-03-12 12:05:06 +0100

    [ADD] module to move failed stock move to qc location

---
+++ quality_control_stock_oca_failed/models/stock_picking.py
@@ -0,0 +1,20 @@
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def _action_done(self):
+        res = super()._action_done()
+        if self.qc_inspections_ids:
+            qc_inspection_failed_ids = self.qc_inspections_ids.filtered(
+                lambda x: x.state == "failed"
+            )
+            for qc_inspection_failed in qc_inspection_failed_ids:
+                if qc_inspection_failed.object_id._name == "stock.move":
+                    move = qc_inspection_failed.object_id
+                    wh = qc_inspection_failed.picking_id.picking_type_id.warehouse_id
+                    vals = {"location_dest_id": wh.wh_qc_stock_loc_id.id}
+                    move.move_line_ids.write(vals)
+                    move.write(vals)
+        return res
---
+++ quality_control_stock_oca_failed/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module create quality control check before validation of stock picking.
