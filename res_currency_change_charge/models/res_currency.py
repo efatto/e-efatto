7d819e686c6b602232956bc7fab7a08060788c6e
sergiocorato <sergiocorato@gmail.com>
2024-03-18 18:49:32 +0100

    [ADD] module to set currency change charge

---
+++ res_currency_change_charge/models/res_currency.py
@@ -0,0 +1,10 @@
+
+from odoo import fields, models
+
+
+class ResCurrency(models.Model):
+    _inherit = "res.currency"
+
+    change_charge_percentage = fields.Float(
+        help="Currency change charge for supplier with this currency."
+    )
