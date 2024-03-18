549eaab901f505d86040254f3fb44e31b2186619
sergiocorato <sergiocorato@gmail.com>
2024-03-18 18:49:32 +0100

    [ADD] module to set currency change charge

---
+++ res_currency_change_charge/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Currency change charge",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "license": "AGPL-3",
+    "category": "other",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_currency.xml",
+    ],
+    "summary": "Add a percentage value of currency change costs to be paid when "
+               "purchasing from supplier.",
+    "installable": True,
+}
