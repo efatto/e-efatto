1a26d4e68b520d4ce5e2f5db0804f946e97a3eb9
sergiocorato <sergiocorato@gmail.com>
2024-03-19 17:34:38 +0100

    [MOVE] module sale_order_disable_onchange_qty

---
+++ sale_order_disable_onchange_qty/models/sale.py
@@ -0,0 +1,12 @@
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.onchange('product_uom', 'product_uom_qty')
+    def product_uom_change(self):
+        if self.price_unit and not self._context.get("recalculate_prices"):
+            return
+        return super().product_uom_change()
