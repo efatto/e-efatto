b8f630c449dae3e1b58f7826f3d2bdfe0957c668
sergiocorato <sergiocorato@gmail.com>
2024-03-19 19:30:36 +0100

    [IMP] version

--- connector_whs/__manifest__.py
+++ connector_whs/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Connector WHS MSSQL",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
     "category": "other",
