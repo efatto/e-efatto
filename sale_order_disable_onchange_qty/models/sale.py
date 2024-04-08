a6e75fc34422b0f8bbfaf1ed0b02691a5085c847
sergiocorato <sergiocorato@gmail.com>
2024-04-08 15:56:46 +0200

    [MIG] sale_order_disable_onchange_qty: Migration to 14.0

--- sale_order_disable_onchange_qty/models/sale.py
+++ sale_order_disable_onchange_qty/models/sale.py
@@ -1,11 +1,10 @@
-
 from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    @api.onchange('product_uom', 'product_uom_qty')
+    @api.onchange("product_uom", "product_uom_qty")
     def product_uom_change(self):
         if self.price_unit and not self._context.get("recalculate_prices"):
             return
