7b780ce9b86bc38088dd5149ede6d234516235af
sergiocorato <sergiocorato@gmail.com>
2024-04-08 14:59:04 +0200

    [IMP] mrp_production_raw_show_child: black, isort, prettier and Migration to 14.0

--- mrp_production_raw_show_child/__manifest__.py
+++ mrp_production_raw_show_child/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Mrp production show created production in raw',
-    'version': '12.0.1.0.0',
-    'category': 'Manufacture',
-    'license': 'AGPL-3',
-    'summary': "Show created production on raw products.",
-    'author': "Sergio Corato",
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'depends': [
-        'mrp',
+    "name": "Mrp production show created production in raw",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "license": "AGPL-3",
+    "summary": "Show created production on raw products.",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "depends": [
+        "mrp",
     ],
-    'data': [
-        'views/mrp_production.xml',
+    "data": [
+        "views/mrp_production.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
