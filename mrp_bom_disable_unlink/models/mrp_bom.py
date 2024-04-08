f6fbcb7dae4b2bf531c1e8a8d24d782c358c5d82
sergiocorato <sergiocorato@gmail.com>
2024-04-08 14:16:54 +0200

    [IMP] mrp_bom_disable_unlink: black, isort, prettier and Migration to 14.0

--- mrp_bom_disable_unlink/models/mrp_bom.py
+++ mrp_bom_disable_unlink/models/mrp_bom.py
@@ -1,14 +1,16 @@
-
-from odoo import api, models, _
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
+    _inherit = "mrp.bom"
 
-    @api.multi
     def unlink(self):
-        if self.env['mrp.production'].search([('bom_id', 'in', self.ids)], limit=1):
-            raise UserError(_('You can not delete a Bill of Material used in '
-                              'manufacturing orders.'))
-        return super(MrpBom, self).unlink()
+        if self.env["mrp.production"].search([("bom_id", "in", self.ids)], limit=1):
+            raise UserError(
+                _(
+                    "You can not delete a Bill of Material used in "
+                    "manufacturing orders."
+                )
+            )
+        return super().unlink()
