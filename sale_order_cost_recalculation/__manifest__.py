b332281a62b3931d9fbcfc284cf0c6ad58fc3ccf
sergiocorato <sergiocorato@gmail.com>
2024-04-08 17:23:22 +0200

    [IMP] sale_order_cost_recalculation: black, isort, prettier and Migration to 14.0

--- sale_order_cost_recalculation/__manifest__.py
+++ sale_order_cost_recalculation/__manifest__.py
@@ -1,21 +1,22 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale order cost recalculation',
-    'version': '12.0.1.0.5',
-    'category': 'other',
-    'author': 'Sergio Corato',
-    'license': 'AGPL-3',
-    'summary': 'Add recalculation of cost when recalculating prices on sale order.',
-    'website': 'https://github.com/sergiocorato/e-efatto',
-    'depends': [
-        'sale_margin_security',
-        'sale_order_price_recalculation',
+    "name": "Sale order cost recalculation",
+    "version": "14.0.1.0.0",
+    "category": "other",
+    "author": "Sergio Corato",
+    "license": "AGPL-3",
+    "summary": "Add recalculation of cost when recalculating prices on sale order.",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "depends": [
+        "sale_margin_security",
+        "sale_order_price_recalculation",
+        "stock_account",
     ],
-    'data': [
-        'views/sale_margin_view.xml',
-        'views/product_template.xml',
+    "data": [
+        "views/sale_margin_view.xml",
+        "views/product_template.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
