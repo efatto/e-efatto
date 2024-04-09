6c6ede895a16f5ef0bf28de1067eeedaa6007427
sergiocorato <sergiocorato@gmail.com>
2024-04-09 11:27:35 +0200

    [IMP] stock_production_lot_info: black, isort, prettier and Migration to 14.0

--- stock_production_lot_info/models/stock_production_lot.py
+++ stock_production_lot_info/models/stock_production_lot.py
@@ -1,26 +1,34 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
+
 from odoo.addons.stock.models.product import OPERATORS
 
 
 class ProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
+    _inherit = "stock.production.lot"
 
-    product_qty = fields.Float(search='_search_product_qty')
+    product_qty = fields.Float(search="_search_product_qty")
 
     def _search_product_qty(self, operator, value):
         lot_ids = []
-        quants = self.env['stock.quant'].read_group([
-            ('location_id.usage', 'in', ['internal', 'transit']),
-            ('lot_id', '!=', False),
-        ], ['lot_id', 'quantity'], ['lot_id'])
+        quants = self.env["stock.quant"].read_group(
+            [
+                ("location_id.usage", "in", ["internal", "transit"]),
+                ("lot_id", "!=", False),
+                ("available_quantity", ">", 0),
+            ],
+            ["lot_id", "quantity"],
+            ["lot_id"],
+        )
         if operator in OPERATORS:
-            lot_ids = [x['lot_id'][0] for x in quants if OPERATORS[operator](
-                x['quantity'], value)]
-        return [('id', 'in', lot_ids)]
+            lot_ids = [
+                x["lot_id"][0]
+                for x in quants
+                if OPERATORS[operator](x["quantity"], value)
+            ]
+        return [("id", "in", lot_ids)]
 
-    @api.multi
     def name_get(self):
         result = []
         for lot in self:
