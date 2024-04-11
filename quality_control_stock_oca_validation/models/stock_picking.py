4e6cb13d6c31be1c3420330ab072ae0478509c6e
sergiocorato <sergiocorato@gmail.com>
2024-04-11 15:09:05 +0200

    [FIX] sudo unlink liste and bypass check for not done moves

--- quality_control_stock_oca_validation/models/stock_picking.py
+++ quality_control_stock_oca_validation/models/stock_picking.py
@@ -6,16 +6,18 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def _action_done(self):
-        if self.qc_inspections_ids:
-            qc_inspection_todo_ids = self.qc_inspections_ids.filtered(
-                lambda x: x.state not in ["success", "failed"]
-            )
-            if qc_inspection_todo_ids:
-                raise ValidationError(
-                    _(
-                        "The stock picking cannot be validated as the following quality "
-                        "control check are not completed: %s"
+        for picking in self:
+            for inspection in picking.qc_inspections_ids:
+                if (
+                    inspection.object_id._name == "stock.move"
+                    and inspection.object_id.quantity_done > 0
+                    and inspection.state not in ["success", "failed"]
+                ):
+                    raise ValidationError(
+                        _(
+                            "The stock picking cannot be validated as the following "
+                            "quality control check are not completed: %s"
+                        )
+                        % inspection.name
                     )
-                    % ",".join(qc_inspection_todo_ids.mapped("name"))
-                )
         return super()._action_done()
