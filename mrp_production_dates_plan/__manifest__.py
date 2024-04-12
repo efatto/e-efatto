00965ad4aad89526a27f2daaecf4fc5323f4b656
sergiocorato <sergiocorato@gmail.com>
2024-04-12 17:32:33 +0200

    [ADD] module to ignore manufacturing lead

---
+++ mrp_production_dates_plan/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Dates ignore Manufacturing Lead",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "summary": "Ignore manufacturing_lead on setting dates in production on "
+               "product moves dates.",
+    "license": "AGPL-3",
+    "category": "Manufacture",
+    "depends": [
+        "mrp",
+    ],
+    "data": [],
+    "installable": True,
+}
---
+++ mrp_production_dates_plan/models/stock_rule.py
@@ -0,0 +1,18 @@
+from odoo import fields, models
+from odoo.tools.date_utils import relativedelta
+
+
+class StockRule(models.Model):
+    _inherit = "stock.rule"
+
+    def _get_date_planned(self, product_id, company_id, values):
+        # ignore company_id.manufacturing_lead for date planned
+        format_date_planned = fields.Datetime.from_string(values['date_planned'])
+        date_planned = format_date_planned - relativedelta(
+            days=product_id.produce_delay)
+        if date_planned == format_date_planned:
+            date_planned = date_planned - relativedelta(hours=1)
+        super()._get_date_planned(
+            product_id, company_id, values
+        )
+        return date_planned
