87b3f2e742ccac3381ecd682fbc26dfe2541c8bc
sergiocorato <sergiocorato@gmail.com>
2024-04-18 11:56:07 +0200

    [FIX] clean

--- connector_whs/models/hyddemo_mssql_log.py
+++ connector_whs/models/hyddemo_mssql_log.py
@@ -537,7 +537,7 @@ class HyddemoMssqlLog(models.Model):
         4. delete whs lists and db lists on state '3' ("Da NON elaborare") older than 3
         months
         5. delete whs lists and db lists on state '2' with move on state 'done' or
-        'cancel' and tipo_mov in ['mrpin', 'mprout'] older than 1 day
+        'cancel' and tipo_mov in ['mrpin', 'mprout'] older than 30 days
         6. delete orphan db lists > done in whs_read_and_synchronize_list
         :param datasource_id: id of datasource (aka dbsource)
         :return:
@@ -592,7 +592,7 @@ class HyddemoMssqlLog(models.Model):
         )
         self._clean_lists(dbsource, hyddemo_whs_lists)
         # 5.
-        date_limit_mrp = fields.Datetime.now() - relativedelta(days=1)
+        date_limit_mrp = fields.Datetime.now() - relativedelta(days=30)
         hyddemo_whs_lists = self.env["hyddemo.whs.liste"].search(
             [
                 ("stato", "=", "2"),
