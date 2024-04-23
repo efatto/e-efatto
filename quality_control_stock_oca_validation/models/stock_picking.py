c1aadd27aaddd6e78a408e1447f1042aa792ba59
sergiocorato <sergiocorato@gmail.com>
2024-04-23 16:18:06 +0200

    [FIX] block validation of picking if move lines are done too (with quality control not done)

--- quality_control_stock_oca_validation/models/stock_picking.py
+++ quality_control_stock_oca_validation/models/stock_picking.py
@@ -8,10 +8,11 @@ class StockPicking(models.Model):
     def _action_done(self):
         for picking in self:
             for inspection in picking.qc_inspections_ids:
-                if (
+                if inspection.state not in ["success", "failed"] and (
                     inspection.object_id._name == "stock.move"
                     and inspection.object_id.quantity_done > 0
-                    and inspection.state not in ["success", "failed"]
+                    or inspection.object_id._name == "stock.move.line"
+                    and inspection.object_id.qty_done > 0
                 ):
                     raise ValidationError(
                         _(
