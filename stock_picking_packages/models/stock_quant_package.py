fd1577cd909c88144d6ca495e1ca15147a2b436c
sergiocorato <sergiocorato@gmail.com>
2024-05-10 09:55:39 +0200

    [IMP] add custom fields

--- stock_picking_packages/models/stock_quant_package.py
+++ stock_picking_packages/models/stock_quant_package.py
@@ -5,3 +5,5 @@ class StockQuantPackage(models.Model):
     _inherit = "stock.quant.package"
 
     dimensions = fields.Text(string="Dimensions")
+    weight_custom = fields.Float(string="Weight custom")
+    appearance = fields.Text(string="Appearance")
