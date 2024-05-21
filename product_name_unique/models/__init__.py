f45d5afce421d428a2b5a79ed3666216db740b73
sergiocorato <sergiocorato@gmail.com>
2024-05-21 09:39:05 +0200

    [IMP] product name unique ability to exclude by category

--- product_name_unique/models/__init__.py
+++ product_name_unique/models/__init__.py
@@ -1,5 +1,3 @@
-# Copyright (C) 2018 - 2021, Open Source Integrators
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from . import product_category
 from . import product
