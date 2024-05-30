ee79247a5ec247f31c6e0ac920f569fff2976b5b
sergiocorato <sergiocorato@gmail.com>
2024-05-30 21:16:30 +0200

    [IMP] put in this module only translation for pricelist field

--- product_managed_pricelist_replenishment_cost/models/product_pricelist.py
+++ product_managed_pricelist_replenishment_cost/models/product_pricelist.py
@@ -6,11 +6,18 @@ class PricelistItem(models.Model):
 
     # re-translate this field
     base = fields.Selection(
+        selection=[
+            ("list_price", "Sales Price"),
+            ("standard_price", "Cost"),
+            ("pricelist", "Other Pricelist"),
+            ("managed_replenishment_cost",
+             "Landed with adjustment/depreciation/testing"),
+        ],
         help="Base price for computation.\n"
         "Sales Price: The base price will be the Sales Price.\n"
         "Cost Price : The base price will be the cost price.\n"
         "Other Pricelist : Computation of the base price based on "
         "another Pricelist.\n"
-        "Managed Replenishment Cost: The base price will be the Landed with "
-        "adjustment/depreciation/testing price."
+        "Landed with adjustment/depreciation/testing: The base price will be the "
+        "Landed with adjustment/depreciation/testing price.",
     )
