ed77914281e9f9e32edfc1f70b3a2f32e8c2d4ce
sergiocorato <sergiocorato@gmail.com>
2024-05-30 17:15:55 +0200

    [FIX] help of modified field

--- product_pricelist_replenishment_cost/models/product_pricelist.py
+++ product_pricelist_replenishment_cost/models/product_pricelist.py
@@ -7,4 +7,11 @@ class PricelistItem(models.Model):
     base = fields.Selection(
         selection_add=[("managed_replenishment_cost", "Managed Replenishment Cost")],
         ondelete={"managed_replenishment_cost": "set default"},
+        help='Base price for computation.\n'
+             'Sales Price: The base price will be the Sales Price.\n'
+             'Cost Price : The base price will be the cost price.\n'
+             'Other Pricelist : Computation of the base price based on '
+             'another Pricelist.\n'
+             'Managed Replenishment Cost: The base price will be the managed '
+             'replenishment cost price.'
     )
