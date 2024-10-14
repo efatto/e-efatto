f6f4112935091086afda5d057d9096b23857eb54
sergiocorato <sergiocorato@gmail.com>
2024-10-14 08:26:47 +0200

    [FIX] add note to unused module

--- product_purchase_first_seller/__manifest__.py
+++ product_purchase_first_seller/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Product Purchase select first seller",
+    "name": "Product Purchase select first seller UNUSED",
     "version": "14.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
