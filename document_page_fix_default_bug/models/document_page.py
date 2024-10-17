35277a535792191209a1b763087362ff2d3c9143
sergiocorato <sergiocorato@gmail.com>
2024-10-17 10:31:25 +0200

    [FIX] bump version

---
+++ document_page_fix_default_bug/models/document_page.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class DocumentPage(models.Model):
+    _inherit = "document.page"
+
+    @api.model
+    def create(self, vals):
+        # Fix for v. 12.0 as context is passed to all models, in this
+        # case "default_parent_id" is catched from mail.message which try
+        # to create a message with an unexisting parent_id equal to document.page
+        # object id.
+        new_context = self.env.context.copy()
+        if self.env.context.get("default_parent_id", False):
+            new_context.pop("default_parent_id")
+            self = self.with_context(new_context)
+        res = super(DocumentPage, self).create(vals)
+        return res
