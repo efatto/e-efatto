446fab173b2f7e854a177bb9d3d910101342977c
sergiocorato <sergiocorato@gmail.com>
2024-11-04 16:43:49 +0100

    [FIX] manifest info

--- connector_wms_whs/tests/test_connector_wms_whs.py
+++ connector_wms_whs/tests/test_connector_wms_whs.py
@@ -1,5 +1,3 @@
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import tagged
 from odoo.addons.connector_whs.tests.test_connector_wms import TestConnectorWMS
