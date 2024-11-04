4916cef060a413a93189029657a16d6d9068ea50
sergiocorato <sergiocorato@gmail.com>
2024-11-04 16:21:36 +0100

    [IMP] move priority field to whs module

--- connector_wms_whs/tests/test_connector_wms_whs.py
+++ connector_wms_whs/tests/test_connector_wms_whs.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import tagged
-from odoo.addons.connector_whs.tests.test_connector_wms import TestConnectorWhs
+from odoo.addons.connector_whs.tests.test_connector_wms import TestConnectorWMS
 
 
 @tagged("-standard", "test_wms")
-class TestConnectorWmsModula(TestConnectorWhs):
+class TestConnectorWmsModula(TestConnectorWMS):
     @staticmethod
     def _get_connection_string():
         return "connection_wms_whs.txt"
