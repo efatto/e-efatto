97dc077c7e1780b0d6e72f9a574c01118550ffba
sergiocorato <sergiocorato@gmail.com>
2024-11-21 10:53:45 +0100

    [IMP] add info on lots

--- quality_control_stock_oca_notes/models/qc_inspection.py
+++ quality_control_stock_oca_notes/models/qc_inspection.py
@@ -1,11 +1,18 @@
-from odoo import models
+from odoo import fields, models
 
 
 class QcInspection(models.Model):
     _inherit = "qc.inspection"
 
+    lot_internal = fields.Text(string="Internal lot")
+    lot_supplier = fields.Char(string="Supplier lot")
+
     def _prepare_inspection_header(self, object_ref, trigger_line):
         res = super()._prepare_inspection_header(object_ref, trigger_line)
         if trigger_line.note:
             res.update({"internal_notes": trigger_line.note})
+        if trigger_line.lot_internal:
+            res.update({"lot_internal": trigger_line.lot_internal})
+        if trigger_line.lot_supplier:
+            res.update({"lot_supplier": trigger_line.lot_supplier})
         return res
