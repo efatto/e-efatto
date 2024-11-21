0bafc280f7b5d251a46f2ef6fca232534d784dae
sergiocorato <sergiocorato@gmail.com>
2024-11-21 10:35:25 +0100

    [IMP] Modula WMS connector cancel ability

--- connector_wms_modula/models/hyddemo_whs_liste.py
+++ connector_wms_modula/models/hyddemo_whs_liste.py
@@ -18,51 +18,57 @@ tipo_operazione_dict = {
 class HyddemoWhsListe(models.Model):
     _inherit = "hyddemo.whs.liste"
 
-
-    def whs_unlink_lists(self, dbsource):
+    @api.multi
+    def whs_unlink_lists(self, dbsource, db_type="IMP"):
         # do no call super() and put specific code
-        dbsource.with_context(no_return=True).execute_mssql(
-            sqlquery=sql_text(
-                "DELETE FROM IMP_ORDINI WHERE ORD_ORDINE=:ORD_ORDINE"),
-            sqlparams=dict(ORD_ORDINE=self.num_lista),
-            metadata=None)
-        dbsource.with_context(no_return=True).execute_mssql(
-            sqlquery=sql_text(
-                "DELETE FROM IMP_ORDINI_RIGHE WHERE RIG_ORDINE=:RIG_ORDINE"),
-            sqlparams=dict(RIG_ORDINE=self.num_lista),
-            metadata=None)
-        _logger.info('WHS LOG: unlink order and rows: %s' % self.num_lista)
-        self.unlink()
+        for num_lista in set(self.mapped("num_lista")):
+            current_whs_lists = self.filtered(lambda x: x.num_lista == num_lista)
+            dbsource.with_context(no_return=True).execute_mssql(
+                sqlquery=sql_text(
+                    f"DELETE FROM {db_type}_ORDINI WHERE ORD_ORDINE=:ORD_ORDINE"),
+                sqlparams=dict(ORD_ORDINE=num_lista),
+                metadata=None)
+            dbsource.with_context(no_return=True).execute_mssql(
+                sqlquery=sql_text(
+                    f"DELETE FROM {db_type}_ORDINI_RIGHE WHERE RIG_ORDINE=:RIG_ORDINE"),
+                sqlparams=dict(RIG_ORDINE=num_lista),
+                metadata=None)
+            _logger.info('WHS LOG: unlink order and rows: %s' % num_lista)
+            if db_type == "IMP":
+                current_whs_lists.unlink()
 
     def whs_cancel_lists(self, dbsource):
         # do no call super() and put specific code
         # update lists to WMS, as they are possibly not elaborated
-        dbsource.whs_insert_read_and_synchronize_list()
+        # todo this call will use current situation of whs lists, so valid!
+        #  dbsource.whs_insert_read_and_synchronize_list()
         # check if the lists exist, to unlink or add an order to delete
-        res = dbsource.execute_mssql(
-            sqlquery=sql_text(
-                "SELECT ORD_ORDINE FROM IMP_ORDINI WHERE ORD_OPERAZIONE='I' "
-                "AND ORD_ORDINE=:ORD_ORDINE"),
-            sqlparams=dict(ORD_ORDINE=self.num_lista),
-            metadata=None,
-        )
-        if res and res[0]:
-            # lista exists, so it's not elaborated from WMS, so unlink it directly
-            # (this will unlink its rows too)
-            self.whs_unlink_lists(dbsource)
-        else:
-            # lista does not exist, so order to WMS to unlink it
-            # (this will unlink its rows too)
-            dbsource.with_context(no_return=True).execute_mssql(
+        for num_lista in set(self.mapped("num_lista")):
+            current_whs_lists = self.filtered(lambda x: x.num_lista == num_lista)
+            res = dbsource.execute_mssql(
                 sqlquery=sql_text(
-                    "INSERT INTO IMP_ORDINI (ORD_OPERAZIONE, ORD_ORDINE) VALUES "
-                    "('D', :ORD_ORDINE)"),
-                    sqlparams=dict(ORD_ORDINE=self.num_lista),
-                    metadata=None)
-            _logger.info('WMS Modula LOG: delete Lista %s' % (
-                self.num_lista
-            ))
-            self.write({'stato': '3'})
+                    "SELECT ORD_ORDINE FROM IMP_ORDINI WHERE ORD_OPERAZIONE='I' "
+                    "AND ORD_ORDINE=:ORD_ORDINE"),
+                sqlparams=dict(ORD_ORDINE=num_lista),
+                metadata=None,
+            )
+            if res and res[0]:
+                # lista exists, so it's not elaborated from WMS, so unlink it directly
+                # (this will unlink its rows too)
+                current_whs_lists.whs_unlink_lists(dbsource)
+            else:
+                # lista does not exist, so order to WMS to unlink it
+                # (this will unlink its rows too)
+                dbsource.with_context(no_return=True).execute_mssql(
+                    sqlquery=sql_text(
+                        "INSERT INTO IMP_ORDINI (ORD_OPERAZIONE, ORD_ORDINE) VALUES "
+                        "('D', :ORD_ORDINE)"),
+                        sqlparams=dict(ORD_ORDINE=num_lista),
+                        metadata=None)
+                _logger.info('WMS Modula LOG: delete Lista %s' % (
+                    num_lista
+                ))
+                current_whs_lists.write({'stato': '3'})
 
     @api.model
     def whs_check_lists(self, num_lista, dbsource):
@@ -122,21 +128,21 @@ VALUES (
                 execute_params_order[lista.num_lista] = {
                     'ORD_OPERAZIONE': 'I',  # I=Insert/Update; D=Delete; A=Add if row not exists
                     # H=Add if header not exists; Q=Always add in queue; R=Replace
-                    'ORD_ORDINE': self.num_lista[:20],  # char 20
-                    'ORD_DES': self.riferimento[:50] if self.riferimento else '',  # char 50
-                    'ORD_PRIOHOST': self.priorita,  # decimal(16,0)
-                    'ORD_TIPOOP': tipo_operazione_dict[self.tipo],  # char 5: P,V,I,E
-                    'ORD_CLIENTE': self.ragsoc[:50],  # char 50
+                    'ORD_ORDINE': lista.num_lista[:20],  # char 20
+                    'ORD_DES': lista.riferimento[:50] if lista.riferimento else '',  # char 50
+                    'ORD_PRIOHOST': lista.priorita,  # decimal(16,0)
+                    'ORD_TIPOOP': tipo_operazione_dict[lista.tipo],  # char 5: P,V,I,E
+                    'ORD_CLIENTE': lista.ragsoc[:50],  # char 50
                 }
             product = lista.product_id
             if not execute_params_order_line.get(lista.num_lista):
                 execute_params_order_line[lista.num_lista] = {}
             execute_params_order_line[lista.num_lista][lista.riga] = {
-                'RIG_ORDINE': self.num_lista[:20],  # char 20
-                'RIG_HOSTINF': self.riga,  # char 100
+                'RIG_ORDINE': lista.num_lista[:20],  # char 20
+                'RIG_HOSTINF': lista.riga,  # char 100
                 'RIG_ARTICOLO': product.default_code[:50] if product.default_code
                 else 'prodotto %s senza codice' % product.id,  # char 50
-                'RIG_QTAR': self.qta,  # decimal(11,3)
+                'RIG_QTAR': lista.qta,  # decimal(11,3)
                 # RIG_PRIO int serve?
             }
         return execute_params_order, execute_params_order_line
