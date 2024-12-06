c4026d995ece4f836ea51d8c1995f0bd471fdca7
sergiocorato <sergiocorato@gmail.com>
2024-12-06 16:23:49 +0100

    [FIX] remove unused in v. 12.0 mrp_manual_procurement_subcontractor

--- mrp_manual_procurement_subcontractor/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import mrp_production
-from . import procurement_group
-from . import product
-from . import product_replenish
-from . import purchase_order
-from . import stock_rule
--- mrp_manual_procurement_subcontractor/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_mrp_manual_procurement_subcontractor
