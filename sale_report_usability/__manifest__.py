a2c79923a51f0aa6e7ab03e7b4025ad26f661c8f
sergiocorato <sergiocorato@gmail.com>
2024-12-11 11:38:54 +0100

    [IMP] sale_report_usability: black, isort, prettier

--- sale_report_usability/__manifest__.py
+++ sale_report_usability/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale report usability',
-    'version': '12.0.1.0.0',
-    'author': 'Sergio Corato',
+    "name": "Sale report usability",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
     "website": "https://github.com/sergiocorato/e-efatto",
-    'license': 'AGPL-3',
-    'category': 'other',
-    'depends': [
-        'sale',
+    "license": "AGPL-3",
+    "category": "other",
+    "depends": [
+        "sale",
     ],
-    'data': [
-        'report/sale_report.xml',
+    "data": [
+        "report/sale_report.xml",
     ],
-    'description': "Add generic date to compare",
-    'installable': True,
+    "description": "Add generic date to compare",
+    "installable": True,
 }
