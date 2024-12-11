1ed20a037e234a321d94a673dbec21db436319b3
sergiocorato <sergiocorato@gmail.com>
2024-12-11 12:21:22 +0100

    [IMP] add dates in sale order

--- sale_report_usability/report/sale_report.py
+++ sale_report_usability/report/sale_report.py
@@ -4,20 +4,26 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    generic_date = fields.Date("Order Date (without year)", readonly=True)
+    generic_date = fields.Date(
+        "Order Date (with current year)",
+        help="Used to compare dates on multiple years",
+        readonly=True,
+    )
     generic_confirmation_date = fields.Date(
-        "Confirmation Date (without year)", readonly=True
+        "Confirmation Date (with current year)",
+        help="Used to compare dates on multiple years",
+        readonly=True,
     )
 
     def _query(self, with_clause="", fields=None, groupby="", from_clause=""):
         fields = fields or {}
         fields["generic_date"] = (
-            ", make_date(date_part('year', CURRENT_DATE)::int, "
+            ", make_date(2000, "
             "date_part('month', date_order)::int, "
             "date_part('day', date_order)::int) as generic_date"
         )
         fields["generic_confirmation_date"] = (
-            ", make_date(date_part('year', CURRENT_DATE)::int, "
+            ", make_date(2000, "
             "date_part('month', confirmation_date)::int, "
             "date_part('day', confirmation_date)::int) as generic_confirmation_date"
         )
