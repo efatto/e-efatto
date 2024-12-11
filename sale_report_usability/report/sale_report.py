03c589cab154ffebf5333a58d85f12a17f6a75c3
sergiocorato <sergiocorato@gmail.com>
2024-12-11 11:38:54 +0100

    [IMP] sale_report_usability: black, isort, prettier

--- sale_report_usability/report/sale_report.py
+++ sale_report_usability/report/sale_report.py
@@ -6,21 +6,21 @@ from odoo import fields, models
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    generic_date = fields.Date(
-        'Order Date (without year)', readonly=True)
+    generic_date = fields.Date("Order Date (without year)", readonly=True)
     generic_confirmation_date = fields.Date(
-        'Confirmation Date (without year)', readonly=True)
+        "Confirmation Date (without year)", readonly=True
+    )
 
-    def _query(self, with_clause='', fields=None, groupby='', from_clause=''):
+    def _query(self, with_clause="", fields=None, groupby="", from_clause=""):
         fields = fields or {}
-        fields['generic_date'] = \
-            ", make_date(date_part('year', CURRENT_DATE)::int, " \
-            "date_part('month', date_order)::int, " \
+        fields["generic_date"] = (
+            ", make_date(date_part('year', CURRENT_DATE)::int, "
+            "date_part('month', date_order)::int, "
             "date_part('day', date_order)::int) as generic_date"
-        fields['generic_confirmation_date'] = \
-            ", make_date(date_part('year', CURRENT_DATE)::int, " \
-            "date_part('month', confirmation_date)::int, " \
-            "date_part('day', confirmation_date)::int) as generic_confirmation_date"
-        return super(SaleReport, self)._query(
-            with_clause, fields, groupby, from_clause
         )
+        fields["generic_confirmation_date"] = (
+            ", make_date(date_part('year', CURRENT_DATE)::int, "
+            "date_part('month', confirmation_date)::int, "
+            "date_part('day', confirmation_date)::int) as generic_confirmation_date"
+        )
+        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
