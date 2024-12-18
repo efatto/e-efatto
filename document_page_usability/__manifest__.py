4ddb62a36da1474083a397f48229cfb90a52ca53
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:25:26 +0100

    [UPD] repo slug

--- document_page_usability/__manifest__.py
+++ document_page_usability/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Remove required from fields: Draft name and Draft summary",
     "author": "Sergio Corato",
     "license": "AGPL-3",
-    "website": "https://github.com/sergiocorato/e-efatto",
+    "website": "https://github.com/efatto/e-efatto",
     "category": "Uncategorized",
     "version": "14.0.1.0.0",
     "depends": ["document_page"],
