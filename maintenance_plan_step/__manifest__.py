759441a58d4e3401ba739f9ee491c5eb41135a10
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:25:26 +0100

    [UPD] repo slug

--- maintenance_plan_step/__manifest__.py
+++ maintenance_plan_step/__manifest__.py
@@ -6,7 +6,7 @@
     "license": "AGPL-3",
     "category": "Manufacturing",
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/e-efatto",
+    "website": "https://github.com/efatto/e-efatto",
     "summary": "Add ability to create planned maintenance by step.",
     "depends": [
         "maintenance_plan",
