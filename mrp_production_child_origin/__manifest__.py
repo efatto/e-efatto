115f4c29a3d73e5f22d5de986d47c4d6ce031559
sergiocorato <sergiocorato@gmail.com>
2024-12-18 12:25:26 +0100

    [UPD] repo slug

--- mrp_production_child_origin/__manifest__.py
+++ mrp_production_child_origin/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "MRP Child Origin",
     "version": "14.0.1.0.0",
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/e-efatto",
+    "website": "https://github.com/efatto/e-efatto",
     "summary": "Extend origin in child production to ease search.",
     "license": "AGPL-3",
     "category": "Manufacture",
