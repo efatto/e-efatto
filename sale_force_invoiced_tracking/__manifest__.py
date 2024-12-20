ade490447e805f7164cfec68af9e55fd856891d5
sergiocorato <sergiocorato@gmail.com>
2024-12-20 17:44:15 +0100

    [FIX] tracking

---
+++ sale_force_invoiced_tracking/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sale track invoice status",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "Sergio Corato",
+    "website": "https://github.com/sergiocorato/e-efatto",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "sale_force_invoiced",
+    ],
+    "data": [
+    ],
+    "installable": True
+}
