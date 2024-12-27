b145a034596ddc312713810c956c80caad056950
sergiocorato <sergiocorato@gmail.com>
2024-12-27 16:32:13 +0100

    [IMP] split repair module from whs

---
+++ connector_wms_whs_repair/models/repair.py
@@ -0,0 +1,69 @@
+# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class RepairOrder(models.Model):
+    _inherit = 'repair.order'
+
+    @api.multi
+    def _get_whs_repair_operation(self, num_lista, move, riga, tipo, tipo_mov):
+        # overridable method
+        return dict(
+            data_lista=fields.Datetime.now(),
+            move_id=move.id,
+            num_lista=num_lista,
+            product_id=move.product_id.id,
+            qta=move.product_uom_qty,
+            riferimento=self.name,
+            riga=riga,
+            stato='1',
+            tipo=tipo,
+            tipo_mov=tipo_mov,
+        )
+
+    @api.multi
+    def action_repair_end(self):
+        res = super(RepairOrder, self).action_repair_end()
+        whsliste_obj = self.env['hyddemo.whs.liste']
+        for repair in self:
+            location_id = repair.location_id
+            dbsource = self.env['base.external.dbsource'].search([
+                ('location_id', '=', location_id.id)
+            ])
+            if not dbsource:
+                # This location is not linked to WMS System
+                continue
+            add_operations = repair.operations.filtered(
+                lambda x: x.move_id
+                and x.move_id.state == "done"
+                and x.move_id.product_id.type == "product"
+                and x.move_id.location_id == location_id
+                and not x.move_id.product_id.exclude_from_whs
+            )
+            remove_operations = repair.operations.filtered(
+                lambda x: x.move_id
+                and x.move_id.state == "done"
+                and x.move_id.product_id.type == "product"
+                and x.move_id.location_dest_id == location_id
+                and not x.move_id.product_id.exclude_from_whs
+            )
+            if add_operations:
+                num_lista = self.env['ir.sequence'].next_by_code('hyddemo.whs.liste')
+                for riga, op in enumerate(add_operations, start=1):
+                    move = op.move_id
+                    whsliste_data = repair._get_whs_repair_operation(
+                        num_lista, move, riga, '1', 'ripout'
+                    )
+                    whsliste_obj.create(whsliste_data)
+            if remove_operations:
+                num_lista = self.env['ir.sequence'].next_by_code('hyddemo.whs.liste')
+                for riga, op in enumerate(remove_operations, start=1):
+                    move = op.move_id
+                    whsliste_data = repair._get_whs_repair_operation(
+                        num_lista, move, riga, '2', 'ripin'
+                    )
+                    whsliste_obj.create(whsliste_data)
+
+        return res
