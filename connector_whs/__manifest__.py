a3f94f113fad6aadb3c6482042074c0c0ea7acb2
sergiocorato <sergiocorato@gmail.com>
2025-01-07 09:43:51 +0100

    [IMP] bump versions

--- connector_whs/__manifest__.py
+++ connector_whs/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Connector WMS MSSQL',
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.2.1',
     'author': 'Sergio Corato',
     'website': 'https://github.com/sergiocorato/e-efatto',
     'category': 'other',
