ebaed413f871cc6755d26f1899d46a2d9050a296
sergiocorato <sergiocorato@gmail.com>
2025-01-08 11:31:18 +0100

    [IMP] moved function to whs specific module

--- connector_wms_whs/models/base_external_dbsource.py
+++ connector_wms_whs/models/base_external_dbsource.py
@@ -1,6 +1,10 @@
-from odoo import models, api
+import logging
 
+from odoo.exceptions import UserError
+from odoo import models, api, _
 from sqlalchemy import text as sql_text
+
+_logger = logging.getLogger(__name__)
 
 
 class BaseExternalDbsource(models.Model):
@@ -119,3 +123,43 @@ class BaseExternalDbsource(models.Model):
             'Id': last_id + 1,
         }
         return execute_params
+
+    @api.multi
+    def whs_check_lists(self):
+        res = super().whs_check_lists()
+        """
+        Funzione lanciabile manualmente per marcare le liste in Odoo che non sono
+        più presenti in WMS in quanto cancellate, per verifiche
+        :return: True
+        """
+        for dbsource in self:
+            connection = dbsource.connection_open_mssql()
+            if not connection:
+                raise UserError(_('Failed to open connection!'))
+            whs_lists = self.env['hyddemo.whs.liste'].search([
+                ('stato', 'in', ['1', '2']),
+            ])
+            i = 0
+            imax = len(whs_lists)
+            step = 1
+            for whs_list in whs_lists:
+                whs_liste_query = \
+                    "SELECT NumLista, NumRiga, Qta, QtaMovimentata, Elaborato " \
+                    "FROM HOST_LISTE " \
+                    "WHERE NumLista = '%s' AND NumRiga = '%s'" % (
+                        whs_list.num_lista, whs_list.riga)
+                esiti_liste = dbsource.execute_mssql(
+                    sqlquery=sql_text(whs_liste_query), sqlparams=None, metadata=None
+                )
+                # esiti_liste[0] contains result
+                if not esiti_liste[0]:
+                    whs_list.whs_list_absent = True
+                else:
+                    whs_list.whs_list_absent = False
+                i += 1
+                if i * 100.0 / imax > step:
+                    _logger.info(
+                        'WMS LOG: Execution {0}% '.format(
+                            int(i * 100.0 / imax)))
+                    step += 1
+        return res
