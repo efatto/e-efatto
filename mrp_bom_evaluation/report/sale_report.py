98786cc60325731882b1be10ebb580c474960de7
sergiocorato <sergiocorato@gmail.com>
2025-02-17 07:23:25 +0100

    [IMP] add costs in sale report

---
+++ mrp_bom_evaluation/report/sale_report.py
@@ -0,0 +1,18 @@
+
+from odoo import fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    purchase_price = fields.Float('Cost')
+    estimated_purchase_price = fields.Float('Estimated Cost')
+
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+        fields['purchase_price'] = """
+, SUM(l.purchase_price / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE
+s.currency_rate END) AS purchase_price"""
+        fields['estimated_purchase_price'] = """
+, SUM(l.estimated_purchase_price / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0
+ELSE s.currency_rate END) AS estimated_purchase_price"""
+        return super()._query(with_clause, fields, groupby, from_clause)
