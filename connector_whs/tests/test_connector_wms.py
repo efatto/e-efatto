6850d9796820ceab12aefc4d44bad70562fdaff6
sergiocorato <sergiocorato@gmail.com>
2025-03-05 08:39:32 +0100

    [IMP] add custom location for WMS

--- connector_whs/tests/test_connector_wms.py
+++ connector_whs/tests/test_connector_wms.py
@@ -12,14 +12,19 @@ class CommonConnectorWMS(TransactionCase):
     def setUp(self):
         super().setUp()
         self.dbsource_model = self.env["base.external.dbsource"]
-        self.src_location = self.env.ref('stock.stock_location_stock')
         self.dest_location = self.env.ref('stock.stock_location_customers')
+        self.src_location = self.env.ref('stock.stock_location_stock')
+        self.wms_location = self.env["stock.location"].create({
+            "name": "WMS Location (child of default internal location)",
+            "location_id": self.src_location.id,
+        })
         self.manufacture_location = self.env["stock.location"].search([
             ("usage", "=", "production")
         ], limit=1)[0]
         self.procurement_model = self.env['procurement.group']
         self.partner = self.env.ref('base.res_partner_2')
-        # Create product with 16 on hand
+        # Create product with 11 on hand on WMS location and 5 in default Stock location
+        # Odoo gets products from all internal locations in the warehouse by default
         self.product1 = self.env['product.product'].create([{
             'name': 'test product1',
             'default_code': 'PRODUCT1',
@@ -28,8 +33,13 @@ class CommonConnectorWMS(TransactionCase):
         self.StockQuant = self.env['stock.quant']
         self.quant_product1 = self.StockQuant.create([{
             'product_id': self.product1.id,
+            'location_id': self.wms_location.id,
+            'quantity': 11.0,
+        }])
+        self.quant_product1 = self.StockQuant.create([{
+            'product_id': self.product1.id,
             'location_id': self.src_location.id,
-            'quantity': 16.0,
+            'quantity': 5.0,
         }])
         # Create product with 8 on hand
         self.product2 = self.env['product.product'].create([{
@@ -39,8 +49,20 @@ class CommonConnectorWMS(TransactionCase):
         }])
         self.quant_product2 = self.StockQuant.create([{
             'product_id': self.product2.id,
-            'location_id': self.src_location.id,
+            'location_id': self.wms_location.id,
             'quantity': 8.0,
+        }])
+        # create product excluded from WMS with 10 on hand
+        self.product_excluded = self.env['product.product'].create([{
+            'name': 'test product excluded from WMS',
+            'default_code': 'PRODUCT1',
+            'type': 'product',
+            'exclude_from_whs': True,
+        }])
+        self.quant_product_excluded = self.StockQuant.create([{
+            'product_id': self.product_excluded.id,
+            'location_id': self.src_location.id,
+            'quantity': 10.0,
         }])
         # Large Cabinet, 250 on hand
         self.product3 = self.env.ref('product.product_product_6')
