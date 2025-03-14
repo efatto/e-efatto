8f87a939aec3f24d4ff47439adb800cb33e27910
sergiocorato <sergiocorato@gmail.com>
2025-03-14 15:37:16 +0100

    [IMP] new module partner ref in stock from purchase

---
+++ purchase_stock_partner_ref/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase partner ref in stock picking",
+    "version": "14.0.1.0.0",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "category": "Products",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase_stock",
+    ],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+    "summary": "Partner ref in purchase order is now visible in stock picking.",
+    "installable": True,
+}
