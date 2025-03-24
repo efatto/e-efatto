111754db1649bdf1d4c2eea3909a6834a05bc09a
sergiocorato <sergiocorato@gmail.com>
2025-03-24 16:14:27 +0100

    [FIX] pre-commit

--- mrp_manual_procurement_subcontractor/models/supplierinfo.py
+++ mrp_manual_procurement_subcontractor/models/supplierinfo.py
@@ -1,11 +1,11 @@
-
 from odoo import fields, models
 
 
 class SupplierInfo(models.Model):
-    _inherit = 'product.supplierinfo'
+    _inherit = "product.supplierinfo"
 
     autoconfirm_purchase = fields.Boolean(
-        string='Autoconfirm Subc. PO',
+        string="Autoconfirm Subc. PO",
         help="Select this option to make the RdP for the subcontracted product directly"
-             " confirmed.")
+        " confirmed.",
+    )
