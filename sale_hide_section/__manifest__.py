37dee82cf12f836a00eae06dc7b04f613b50dc0b
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- sale_hide_section/__manifest__.py
+++ sale_hide_section/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "12.0.1.0.0",
     "category": "Web",
     "author": "Sergio Corato",
-    "website": "https://github.com/sergiocorato/e-efatto",
+    "website": "https://github.com/efatto/e-efatto",
     "summary": "Ability to hide sale lines inside a section in a sale order",
     "license": "AGPL-3",
     "depends": [
