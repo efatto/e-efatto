5c68f3d91d3d1a7e4a35878f030d45cd1b2970a9
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- mrp_production_calendar/__manifest__.py
+++ mrp_production_calendar/__manifest__.py
@@ -7,7 +7,7 @@
     'license': 'AGPL-3',
     'category': 'Manufacturing',
     'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-efatto',
+    'website': 'https://github.com/efatto/e-efatto',
     'description': 'Add computed field to show duration of production and workorder '
                    'on views.',
     'depends': [
