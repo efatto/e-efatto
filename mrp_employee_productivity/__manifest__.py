dfa9fc8c36d36be95bc7370f80b0186f92e4974b
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- mrp_employee_productivity/__manifest__.py
+++ mrp_employee_productivity/__manifest__.py
@@ -6,7 +6,7 @@
     'license': 'AGPL-3',
     'category': 'Manufacturing',
     'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-efatto',
+    'website': 'https://github.com/efatto/e-efatto',
     'description': 'View employee productivity time in employee view.',
     'depends': [
         'hr',
