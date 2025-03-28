0f2a117ae237b038ceec335c47cead865e3fdc8e
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- product_attachments_button/__manifest__.py
+++ product_attachments_button/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Product Attachments Button',
     'version': '12.0.1.0.0',
     'author': 'Sergio Corato',
-    "website": "https://github.com/sergiocorato/e-efatto",
+    "website": "https://github.com/efatto/e-efatto",
     'license': 'AGPL-3',
     'category': 'Products',
     'depends': [
