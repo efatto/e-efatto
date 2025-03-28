e88a1903a79ae8993f08a5047398d7b4de991630
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- hr_timesheet_productivity/__manifest__.py
+++ hr_timesheet_productivity/__manifest__.py
@@ -6,7 +6,7 @@
     'license': 'AGPL-3',
     'category': 'Manufacturing',
     'author': 'Sergio Corato',
-    'website': 'https://github.com/sergiocorato/e-efatto',
+    'website': 'https://github.com/efatto/e-efatto',
     'description': 'Add view of timesheet combined with productivity times.',
     'depends': [
         'hr_timesheet',
