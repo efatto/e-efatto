44c5273dc979c81851fb941a5307cba90a5fc716
sergiocorato <sergiocorato@gmail.com>
2025-03-28 17:05:16 +0100

    [FIX] web repo

--- connector_wms_whs_repair/__manifest__.py
+++ connector_wms_whs_repair/__manifest__.py
@@ -9,7 +9,7 @@
     Glue module when connector to WMS and repair are intalled.
     """,
     'author': "Sergio Corato",
-    'website': 'https://github.com/sergiocorato/e-efatto',
+    'website': 'https://github.com/efatto/e-efatto',
     'depends': [
         'connector_wms_whs',
         'repair',
