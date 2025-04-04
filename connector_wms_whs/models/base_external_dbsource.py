ab3651dffe2bb28ee9d3ffa5d94563378163e6b0
sergiocorato <sergiocorato@gmail.com>
2025-04-04 17:17:06 +0200

    [IMP] connector_wms_whs: black, isort, prettier

--- connector_wms_whs/models/base_external_dbsource.py
+++ connector_wms_whs/models/base_external_dbsource.py
@@ -1,8 +1,9 @@
 import logging
 
-from odoo.exceptions import UserError
-from odoo import models, api, _
 from sqlalchemy import text as sql_text
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -10,29 +11,27 @@ _logger = logging.getLogger(__name__)
 class BaseExternalDbsource(models.Model):
     _inherit = "base.external.dbsource"
 
-    @api.multi
     def _pre_insert_product_query(self):
-        pre_insert_product_query = \
+        self.ensure_one()
+        pre_insert_product_query = (
             "DELETE FROM HOST_ARTICOLI WHERE Elaborato = 2 OR Elaborato = 0"
+        )
         self.with_context(no_return=True).execute_mssql(
-            sqlquery=sql_text(pre_insert_product_query),
-            sqlparams=None, metadata=None
+            sqlquery=sql_text(pre_insert_product_query), sqlparams=None, metadata=None
         )
         return True
 
-    @api.multi
     def _post_insert_product_query(self, last_id):
         # Set record from Elaborato=0 to Elaborato=1 to be processable from WHS
         self.ensure_one()
-        update_product_query = \
+        update_product_query = (
             "UPDATE HOST_ARTICOLI SET Elaborato = 1 WHERE Elaborato = 0"
+        )
         self.with_context(no_return=True).execute_mssql(
-            sqlquery=sql_text(update_product_query),
-            sqlparams=None, metadata=None
+            sqlquery=sql_text(update_product_query), sqlparams=None, metadata=None
         )
         return True
 
-    @api.multi
     def _get_insert_product_query(self):
         insert_product_query = """
         INSERT INTO HOST_ARTICOLI (
@@ -76,7 +75,6 @@ class BaseExternalDbsource(models.Model):
         """
         return insert_product_query
 
-    @api.multi
     def _prepare_host_articoli_values(
         self, product, location_id, last_id, operation="A"
     ):
@@ -93,37 +91,40 @@ class BaseExternalDbsource(models.Model):
         super()._prepare_host_articoli_values(
             product, location_id, last_id, operation=operation
         )
-        ops = self.env['stock.warehouse.orderpoint'].search([
-            ('location_id', '=', location_id),
-            ('product_id', '=', product.id),
-        ])
+        ops = self.env["stock.warehouse.orderpoint"].search(
+            [
+                ("location_id", "=", location_id),
+                ("product_id", "=", product.id),
+            ]
+        )
         if len(ops) > 1:
             pass
         product_min_qty = ops[0].product_min_qty if ops else 0
         execute_params = {
-            'Elaborato': 0,
-            'TipoOperazione': operation,
-            'Codice': product.default_code[:75] if product.default_code
-            else 'articolo senza codice',
-            'Descrizione': product.name[:70],
-            'Peso': product.weight * 1000 if product.weight else 0.0,  # digits=(18, 5)
-            'Barcode': product.barcode[:30] if product.barcode else ' ',
-            'UM': 'PZ' if product.uom_id.name == 'Unit(s)'
+            "Elaborato": 0,
+            "TipoOperazione": operation,
+            "Codice": product.default_code[:75]
+            if product.default_code
+            else "articolo senza codice",
+            "Descrizione": product.name[:70],
+            "Peso": product.weight * 1000 if product.weight else 0.0,  # digits=(18, 5)
+            "Barcode": product.barcode[:30] if product.barcode else " ",
+            "UM": "PZ"
+            if product.uom_id.name == "Unit(s)"
             else product.uom_id.name[:10],
-            'TipoConfezione': 0,
-            'CategoriaMerc': ' ',  # size=10
-            'MantieniDinamici': 1,
-            'Ubicazione': ' ',
-            'Altezza': 0,
-            'Larghezza': 0,
-            'Profondita': 0,
-            'DescrizioneBreve': ' ',
-            'ScortaMin': product_min_qty,  # digits=(18, 3)
-            'Id': last_id + 1,
+            "TipoConfezione": 0,
+            "CategoriaMerc": " ",  # size=10
+            "MantieniDinamici": 1,
+            "Ubicazione": " ",
+            "Altezza": 0,
+            "Larghezza": 0,
+            "Profondita": 0,
+            "DescrizioneBreve": " ",
+            "ScortaMin": product_min_qty,  # digits=(18, 3)
+            "Id": last_id + 1,
         }
         return execute_params
 
-    @api.multi
     def whs_check_lists(self):
         """
         Funzione lanciabile manualmente per marcare le liste in Odoo che non sono
@@ -134,19 +135,22 @@ class BaseExternalDbsource(models.Model):
         for dbsource in self:
             connection = dbsource.connection_open_mssql()
             if not connection:
-                raise UserError(_('Failed to open connection!'))
-            whs_lists = self.env['hyddemo.whs.liste'].search([
-                ('stato', 'in', ['1', '2']),
-            ])
+                raise UserError(_("Failed to open connection!"))
+            whs_lists = self.env["hyddemo.whs.liste"].search(
+                [
+                    ("stato", "in", ["1", "2"]),
+                ]
+            )
             i = 0
             imax = len(whs_lists)
             step = 1
             for whs_list in whs_lists:
-                whs_liste_query = \
-                    "SELECT NumLista, NumRiga, Qta, QtaMovimentata, Elaborato " \
-                    "FROM HOST_LISTE " \
-                    "WHERE NumLista = '%s' AND NumRiga = '%s'" % (
-                        whs_list.num_lista, whs_list.riga)
+                whs_liste_query = (
+                    "SELECT NumLista, NumRiga, Qta, QtaMovimentata, Elaborato "
+                    "FROM HOST_LISTE "
+                    "WHERE NumLista = '%s' AND NumRiga = '%s'"
+                    % (whs_list.num_lista, whs_list.riga)
+                )
                 esiti_liste = dbsource.execute_mssql(
                     sqlquery=sql_text(whs_liste_query), sqlparams=None, metadata=None
                 )
@@ -158,7 +162,7 @@ class BaseExternalDbsource(models.Model):
                 i += 1
                 if i * 100.0 / imax > step:
                     _logger.info(
-                        'WMS LOG: Execution {0}% '.format(
-                            int(i * 100.0 / imax)))
+                        "WMS LOG: Execution {}% ".format(int(i * 100.0 / imax))
+                    )
                     step += 1
         return res
--- quality_control_stock_oca_deactivate/models/product_template.py
+++ quality_control_stock_oca_deactivate/models/product_template.py
@@ -2,7 +2,7 @@ from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     has_inactive_triggers = fields.Boolean(
         compute="_compute_has_inactive_triggers",
@@ -15,8 +15,10 @@ class ProductTemplate(models.Model):
             record.has_inactive_triggers = bool(
                 self.env["qc.trigger.product_template_line"]
                 .with_context(active_test=False)
-                .search([
-                    ("active", "=", False),
-                    ("product_template", "=", record.id),
-                ])
+                .search(
+                    [
+                        ("active", "=", False),
+                        ("product_template", "=", record.id),
+                    ]
+                )
             )
