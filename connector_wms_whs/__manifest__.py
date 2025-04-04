2c2f9c59023f7ad604c2b7aa05764ba6d51283b8
sergiocorato <sergiocorato@gmail.com>
2025-04-04 17:17:06 +0200

    [IMP] connector_wms_whs: black, isort, prettier

--- connector_wms_whs/__manifest__.py
+++ connector_wms_whs/__manifest__.py
@@ -1,24 +1,24 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Connector WHS WMS',
-    'version': '12.0.1.0.0',
-    'category': 'Warehouse Management',
-    'license': 'AGPL-3',
-    'summary': """
+    "name": "Connector WHS WMS",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "summary": """
     Add custom method to connect to WhSystem WMS.
     """,
-    'author': "Sergio Corato",
-    'website': 'https://github.com/efatto/e-efatto',
-    'depends': [
-        'connector_whs',
-        'sale_order_priority',
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "connector_whs",
+        "sale_order_priority",
     ],
-    'data': [
-        'views/hyddemo_whs_liste.xml',
-        'wizard/view_wizard_sync_stock_whs_mssql.xml'
+    "data": [
+        "views/hyddemo_whs_liste.xml",
+        "wizard/view_wizard_sync_stock_whs_mssql.xml",
     ],
-    'installable': True,
+    "installable": True,
     "excludes": "connector_wms_modula",
     "external_dependencies": {"python": ["sqlalchemy"]},  # "sqlalchemy==1.3.24"
 }
