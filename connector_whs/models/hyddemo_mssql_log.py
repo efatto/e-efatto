52ecb573a19c05456d97d23b6e0e370f18d0fb6c
sergiocorato <sergiocorato@gmail.com>
2025-04-08 19:06:31 +0200

    [FIX] tests reusing methods

--- connector_whs/models/hyddemo_mssql_log.py
+++ connector_whs/models/hyddemo_mssql_log.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HyddemoMssqlLog(models.Model):
@@ -21,173 +21,6 @@ class HyddemoMssqlLog(models.Model):
         "hyddemo.mssql.log.line", "hyddemo_mssql_log_id", "Log lines"
     )
 
-# DA QUI SPOSTATE
-
-#
-#     def whs_insert_list_to_elaborate(self, datasource_id):
-#         """
-#         Write on mssql the lists in stato 1 created from stock and repair in
-#         hyddemo.whs.liste to be elaborated from WHS
-#         :param datasource_id:
-#         :return:
-#         """
-#         dbsource_obj = self.env["base.external.dbsource"]
-#         dbsource = dbsource_obj.browse(datasource_id)
-#         connection = dbsource.connection_open_mssql()
-#         if not connection:
-#             raise UserError(_("Failed to open connection!"))
-#
-#         hyddemo_whs_lists = self.env["hyddemo.whs.liste"].search(
-#             [
-#                 ("stato", "=", "1"),
-#             ]
-#         )
-#         for lista in hyddemo_whs_lists:
-#             insert_esiti_liste_params = self._prepare_host_liste_values(lista)
-#             insert_query = self.get_insert_query(insert_esiti_liste_params)
-#             if insert_esiti_liste_params:
-#                 self.execute_query(
-#                     dbsource, sql_text(insert_query), insert_esiti_liste_params
-#                 )
-#         # Update lists on mssql from 0 to 1 to be elaborated from WHS all in the same
-#         # time
-#         if hyddemo_whs_lists:
-#             set_liste_to_elaborate_query = (
-#                 "UPDATE HOST_LISTE SET Elaborato=1 WHERE Elaborato=0 "
-#                 "AND (%s)"
-#                 % (
-#                     " OR ".join(
-#                         "(NumLista='%s' AND NumRiga='%s')" % (y.num_lista, y.riga)
-#                         for y in hyddemo_whs_lists
-#                     )
-#                 )
-#             )
-#             dbsource.with_context(no_return=True).execute_mssql(
-#                 sqlquery=sql_text(set_liste_to_elaborate_query),
-#                 sqlparams=None,
-#                 metadata=None,
-#             )
-#             hyddemo_whs_lists.write({"stato": "2"})
-#
-#     @staticmethod
-#     def get_insert_query(insert_esiti_liste_params):
-#         if "idCliente" in insert_esiti_liste_params:
-#             if "RagioneSociale" in insert_esiti_liste_params:
-#                 insert_query = insert_host_liste_query.format(
-#                     idCliente="idCliente,",
-#                     idClientes=":idCliente,",
-#                     RagioneSociale="RagioneSociale,",
-#                     RagioneSociales=":RagioneSociale,",
-#                 )
-#             else:
-#                 insert_query = insert_host_liste_query.format(
-#                     idCliente="idCliente,",
-#                     idClientes=":idCliente,",
-#                     RagioneSociale="",
-#                     RagioneSociales="",
-#                 )
-#         elif "RagioneSociale" in insert_esiti_liste_params:
-#             insert_query = insert_host_liste_query.format(
-#                 RagioneSociale="RagioneSociale,",
-#                 RagioneSociales=":RagioneSociale,",
-#                 idCliente="",
-#                 idClientes="",
-#             )
-#         else:
-#             insert_query = insert_host_liste_query.format(
-#                 RagioneSociale="", RagioneSociales="", idCliente="", idClientes=""
-#             )
-#         insert_query = insert_query.replace("\n", " ")
-#         return insert_query
-#
-#     def execute_query(self, dbsource, insert_query, insert_esiti_liste_params):
-#         res = dbsource.with_context(no_return=True).execute_mssql(
-#             sqlquery=insert_query,
-#             sqlparams=insert_esiti_liste_params,
-#             metadata=None,
-#         )
-#         if not res:
-#             time.sleep(1)
-#             self.execute_query(dbsource, insert_query, insert_esiti_liste_params)
-#         return res
-#
-#     @staticmethod
-#     def _prepare_host_liste_values(hyddemo_whs_list):
-#         product = hyddemo_whs_list.product_id
-#         parent_product_id = (
-#             hyddemo_whs_list.parent_product_id
-#             if hyddemo_whs_list.parent_product_id
-#             else False
-#         )
-#         execute_params = {
-#             "NumLista": hyddemo_whs_list.num_lista[:50],  # char 50
-#             "NumRiga": hyddemo_whs_list.riga,  # char 50 but is an integer
-#             "DataLista": hyddemo_whs_list.data_lista.strftime("%Y.%m.%d"),
-#             # formato aaaa.mm.gg datalista
-#             "Riferimento": hyddemo_whs_list.riferimento[:50]
-#             if hyddemo_whs_list.riferimento
-#             else "",  # char 50
-#             "TipoOrdine": hyddemo_whs_list.tipo,  # int
-#             "Causale": 10 if hyddemo_whs_list.tipo == "1" else 20,  # int
-#             "Priorita": hyddemo_whs_list.priorita,  # int
-#             "RichiestoEsito": 1,  # int
-#             "Stato": 0,  # int
-#             "ControlloEvadibilita": 0,  # int
-#             "Vettore": hyddemo_whs_list.vettore[:30]
-#             if hyddemo_whs_list.vettore
-#             else "",  # char 30
-#             "Indirizzo": hyddemo_whs_list.indirizzo[:50]
-#             if hyddemo_whs_list.indirizzo
-#             else "",  # char 50
-#             "Cap": hyddemo_whs_list.cap[:10] if hyddemo_whs_list.cap else "",  # char 10
-#             "Localita": hyddemo_whs_list.localita[:50]
-#             if hyddemo_whs_list.localita
-#             else "",  # char 50
-#             "Provincia": hyddemo_whs_list.provincia[:2]
-#             if hyddemo_whs_list.provincia
-#             else "",  # char 2
-#             "Nazione": hyddemo_whs_list.nazione[:50]
-#             if hyddemo_whs_list.nazione
-#             else "",  # char 50
-#             "Articolo": product.default_code[:30]
-#             if product.default_code
-#             else "prodotto senza codice",  # char 30
-#             "DescrizioneArticolo": product.name[:70]
-#             if product.name
-#             else product.default_code[:70]
-#             if product.default_code
-#             else "prodotto senza nome",  # char 70
-#             "Qta": hyddemo_whs_list.qta,  # numeric(18,3)
-#             "PesoArticolo": product.weight * 1000 if product.weight else 0,  # int
-#             "UMArticolo": "PZ"
-#             if product.uom_id.name == "Unit(s)"
-#             else product.uom_id.name[:10],  # char 10
-#             "IdTipoArticolo": 0,  # int
-#             "Elaborato": 0,  # 0 per poi scrivere 1 tutte insieme  # int
-#             "AuxTesto1": hyddemo_whs_list.client_order_ref[:50]
-#             if hyddemo_whs_list.client_order_ref
-#             else "",  # char 50
-#             "AuxTestoRiga1": hyddemo_whs_list.product_customer_code[:250]
-#             if hyddemo_whs_list.product_customer_code
-#             else "",  # char 250
-#             "AuxTestoRiga2": hyddemo_whs_list.product_customer_code[:250]
-#             if hyddemo_whs_list.product_customer_code
-#             else "",  # char 250
-#             "AuxTestoRiga3": (
-#                 parent_product_id.default_code[:250]
-#                 if parent_product_id.default_code
-#                 else parent_product_id.name[:250]
-#             )
-#             if parent_product_id
-#             else "",  # char 250
-#         }
-#         if hyddemo_whs_list.cliente:  # char 30
-#             execute_params.update({"idCliente": hyddemo_whs_list.cliente[:30]})
-#         if hyddemo_whs_list.ragsoc:  # char 100
-#             execute_params.update({"RagioneSociale": hyddemo_whs_list.ragsoc[:100]})
-#         return execute_params
-# # FIN QUI SPOSTATE!
-
 
 class HyddemoMssqlLogLine(models.Model):
     _name = "hyddemo.mssql.log.line"
@@ -197,13 +30,15 @@ class HyddemoMssqlLogLine(models.Model):
     qty_wrong = fields.Float(
         string="Odoo Q.ty (wrong)",
         help="This quantity is assumed as wrong and overriden by WMS quantity if "
-             "'Synchronize stock inventory' is set.")
+        "'Synchronize stock inventory' is set.",
+    )
     qty = fields.Float(string="WMS Q.ty")
     weight = fields.Float(string="WMS Weight")
     weight_wrong = fields.Float(
         string="Odoo Weight (wrong)",
         help="This weight is assumed as wrong and overriden by WMS weight if "
-             "'Synchronize stock inventory' is set.")
+        "'Synchronize stock inventory' is set.",
+    )
     product_id = fields.Many2one("product.product")
     type = fields.Selection(
         [
--- connector_whs/tests/test_connector_wms.py
+++ connector_whs/tests/test_connector_wms.py
@@ -1,10 +1,11 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase
-from odoo.tests import tagged
-from odoo.tools import mute_logger
 import time
+
+from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 
 
 @tagged("-standard", "test_wms")
@@ -14,71 +15,107 @@ class CommonConnectorWMS(TransactionCase):
         self.dbsource_model = self.env["base.external.dbsource"]
         self.dest_location = self.env.ref("stock.stock_location_customers")
         self.src_location = self.env.ref("stock.stock_location_stock")  # noqa
-        self.wms_location = self.env["stock.location"].create({
-            "name": "WMS Location (child of default internal location)",
-            "location_id": self.src_location.id,
-        })
-        self.manufacture_location = self.env["stock.location"].search([
-            ("usage", "=", "production")
-        ], limit=1)[0]
+        self.wms_location = self.env["stock.location"].create(
+            {
+                "name": "WMS Location (child of default internal location)",
+                "location_id": self.src_location.id,
+            }
+        )
+        self.manufacture_location = self.env["stock.location"].search(
+            [("usage", "=", "production")], limit=1
+        )[0]
         self.procurement_model = self.env["procurement.group"]
         self.partner = self.env.ref("base.res_partner_2")
         # Create product with 11 on hand on WMS location and 5 in default Stock location
         # Odoo gets products from all internal locations in the warehouse by default
-        self.product1 = self.env["product.product"].create([{
-            "name": "test product1",
-            "default_code": "PRODUCT1",
-            "type": "product",
-        }])
+        self.product1 = self.env["product.product"].create(
+            [
+                {
+                    "name": "test product1",
+                    "default_code": "PRODUCT1",
+                    "type": "product",
+                }
+            ]
+        )
         self.StockQuant = self.env["stock.quant"]
-        self.quant_product1 = self.StockQuant.create([{
-            "product_id": self.product1.id,
-            "location_id": self.wms_location.id,
-            "quantity": 11.0,
-        }])
-        self.quant_product1 = self.StockQuant.create([{
-            "product_id": self.product1.id,
-            "location_id": self.src_location.id,
-            "quantity": 5.0,
-        }])
+        self.quant_product1 = self.StockQuant.create(
+            [
+                {
+                    "product_id": self.product1.id,
+                    "location_id": self.wms_location.id,
+                    "quantity": 11.0,
+                }
+            ]
+        )
+        self.quant_product1 = self.StockQuant.create(
+            [
+                {
+                    "product_id": self.product1.id,
+                    "location_id": self.src_location.id,
+                    "quantity": 5.0,
+                }
+            ]
+        )
         # Create product with 8 on hand
-        self.product2 = self.env["product.product"].create([{
-            "name": "test product2",
-            "default_code": "PRODUCT2",
-            "type": "product",
-        }])
-        self.quant_product2 = self.StockQuant.create([{
-            "product_id": self.product2.id,
-            "location_id": self.wms_location.id,
-            "quantity": 8.0,
-        }])
+        self.product2 = self.env["product.product"].create(
+            [
+                {
+                    "name": "test product2",
+                    "default_code": "PRODUCT2",
+                    "type": "product",
+                }
+            ]
+        )
+        self.quant_product2 = self.StockQuant.create(
+            [
+                {
+                    "product_id": self.product2.id,
+                    "location_id": self.wms_location.id,
+                    "quantity": 8.0,
+                }
+            ]
+        )
         # create product excluded from WMS with 10 on hand
-        self.product_excluded = self.env["product.product"].create([{
-            "name": "test product excluded from WMS",
-            "default_code": "PRODUCT1",
-            "type": "product",
-            "exclude_from_whs": True,
-        }])
-        self.quant_product_excluded = self.StockQuant.create([{
-            "product_id": self.product_excluded.id,
-            "location_id": self.src_location.id,
-            "quantity": 10.0,
-        }])
+        self.product_excluded = self.env["product.product"].create(
+            [
+                {
+                    "name": "test product excluded from WMS",
+                    "default_code": "PRODUCT1",
+                    "type": "product",
+                    "exclude_from_whs": True,
+                }
+            ]
+        )
+        self.quant_product_excluded = self.StockQuant.create(
+            [
+                {
+                    "product_id": self.product_excluded.id,
+                    "location_id": self.src_location.id,
+                    "quantity": 10.0,
+                }
+            ]
+        )
         # Large Cabinet, 250 on hand
         self.product3 = self.env.ref("product.product_product_6")
         # Drawer Black, 0 on hand
         self.product4 = self.env.ref("product.product_product_16")
         self.product5 = self.env.ref("product.product_product_20")
         self.product1.invoice_policy = "order"
-        self.product1.write({
-            "customer_ids": [
-                (0, 0, {
-                    "name": self.partner.id,
-                    "product_code": "CUSTOMERCODE",
-                    "product_name": "Product customer name",
-                })
-            ]
-        })
+        self.product1.write(
+            {
+                "customer_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.partner.id,
+                            "product_code": "CUSTOMERCODE",
+                            "product_name": "Product customer name",
+                        },
+                    )
+                ]
+            }
+        )
         self.product2.invoice_policy = "order"
         # MRP data
         self.top_product = self.env.ref(
