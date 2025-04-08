2acbbe4d9f81b20b27cd9edd43ff9913a242e25f
sergiocorato <sergiocorato@gmail.com>
2025-04-08 19:06:31 +0200

    [FIX] tests reusing methods

--- connector_wms_whs/models/stock.py
+++ connector_wms_whs/models/stock.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import fields, models
 
 # priority (priorità) da odoo14 i valori corrispondono (0='0', ecc.)
 # odoo14: [('0', 'Normal'), ('1', 'Urgent')]
@@ -19,12 +19,35 @@ class StockMove(models.Model):
     @staticmethod
     def _set_priority(move, whsliste_data):
         if move.sale_line_id.priority:
-            whsliste_data["priorita"] = max(
-                [int(move.sale_line_id.priority), 0]
-            )
+            whsliste_data["priorita"] = max([int(move.sale_line_id.priority), 0])
         elif move.priority:
             whsliste_data["priorita"] = max([int(move.priority), 0])
         return whsliste_data
+
+    def custom_check_mrp(self):
+        # Do not create WMS lists for finished products that have an MTO route
+        # and with category name equal to CUSTOM
+        self.ensure_one()
+        if all(
+            [
+                x
+                in [
+                    self.env.ref("mrp.route_warehouse0_manufacture"),
+                    self.env.ref("stock.route_warehouse0_mto"),
+                ]
+                for x in self.product_id.route_ids
+            ]
+        ):
+            # Never create whs list for OUT or IN related to
+            # CUSTOM manufactured products, only create MO.
+            # The IN will be without whs_list_ids so freely validatable
+            # as production is done.
+            # Same for the OUT, that one will be based only on Odoo
+            # stock current availability (user has to check this one is
+            # correct)
+            if self.procure_method == "make_to_order":
+                return True
+        return False
 
 
 class Picking(models.Model):
