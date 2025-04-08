9583b7d884c1f711b739e84ce4d35defd905fe11
sergiocorato <sergiocorato@gmail.com>
2025-04-08 12:55:41 +0200

    [IMP] move priority to wms whs module

--- connector_whs/models/sale.py
+++ connector_whs/models/sale.py
@@ -1,15 +1,11 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
-
-from odoo.addons.connector_whs.models.stock import EXTRA_PROCUREMENT_PRIORITIES
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    priority = fields.Selection(selection_add=EXTRA_PROCUREMENT_PRIORITIES)
 
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
@@ -31,9 +27,3 @@ class SaleOrder(models.Model):
         for order in self:
             order.picking_ids.cancel_whs_list()
         return res
-
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    priority = fields.Selection(selection_add=EXTRA_PROCUREMENT_PRIORITIES)
--- connector_wms_whs/models/__init__.py
+++ connector_wms_whs/models/__init__.py
@@ -1,3 +1,4 @@
 from . import hyddemo_whs_liste
 from . import base_external_dbsource
+from . import sale
 from . import stock
