9a4ddb1b6498523be193b98b5c378e5576a7e9b6
sergiocorato <sergiocorato@gmail.com>
2025-04-23 19:57:32 +0200

    [FIX] compute only if analytic account exists

--- mrp_bom_evaluation/models/invoice.py
+++ mrp_bom_evaluation/models/invoice.py
@@ -8,8 +8,9 @@ class AccountInvoiceLine(models.Model):
     @api.one
     def button_exclude_extra_cost(self):
         super().button_exclude_extra_cost()
-        sales = self.env["sale.order"].search([
-            ("analytic_account_id", "=", self.account_analytic_id.id)]
-        )
-        if sales:
-            sales._compute_analytic_cost()
+        if self.account_analytic_id:
+            sales = self.env["sale.order"].search([
+                ("analytic_account_id", "=", self.account_analytic_id.id)]
+            )
+            if sales:
+                sales._compute_analytic_cost()
