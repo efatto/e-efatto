e24267e226c0e693bc031c4d4fc99c5b8b17d6e9
sergiocorato <sergiocorato@gmail.com>
2025-06-02 22:27:29 +0200

    [MIG] mrp_production_calendar and refactor logic to date planned on workcenters

--- mrp_production_calendar/models/mrp_workorder.py
+++ mrp_production_calendar/models/mrp_workorder.py
@@ -4,30 +4,22 @@ from odoo import api, fields, models
 class MrpWorkorder(models.Model):
     _inherit = "mrp.workorder"
 
-    estimated_days_duration = fields.Float(
-        compute="_compute_estimated_duration",
+    previous_work_order_ids = fields.Many2many(
+        comodel_name="mrp.workorder",
+        relation="mrp_workorder_previous_rel",
+        column1="workorder_id",
+        column2="previous_work_order_id",
+        compute="_compute_previous_work_order_ids",
         store=True,
-        digits=(18, 6),
-    )
-    estimated_duration = fields.Float(
-        compute="_compute_estimated_duration",
-        store=True,
-        help="Estimated duration in hours splitted on days",
+        help="Previous workorder of the current one",
     )
     origin = fields.Char(related="production_id.origin")
 
-    @api.multi
-    @api.depends(
-        "duration_expected", "workcenter_id.resource_calendar_id.hours_per_day"
-    )
-    def _compute_estimated_duration(self):
-        for workcorder in self:
-            estimated_duration_days = (
-                workcorder.duration_expected
-                / (workcorder.workcenter_id.resource_calendar_id.hours_per_day or 8.0)
-                / 60.0
-                / 5
-                * 7  # parameter to consider 2 holiday days by week
+    @api.depends("production_id.workorder_ids.next_work_order_id")
+    def _compute_previous_work_order_ids(self):
+        for workorder in self:
+            workorder.previous_work_order_ids = (
+                workorder.production_id.workorder_ids.filtered(
+                    lambda w: w.next_work_order_id == workorder
+                )
             )
-            workcorder.estimated_days_duration = estimated_duration_days
-            workcorder.estimated_duration = estimated_duration_days * 24.0
