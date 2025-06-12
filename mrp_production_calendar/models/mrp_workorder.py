562d1c7dee0e63b12bd136e68751a691cb11edd8
sergiocorato <sergiocorato@gmail.com>
2025-06-12 11:34:35 +0200

    [IMP] change date end or start of a workorder will change its duration

--- mrp_production_calendar/models/mrp_workorder.py
+++ mrp_production_calendar/models/mrp_workorder.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class MrpWorkorder(models.Model):
@@ -23,3 +24,28 @@ class MrpWorkorder(models.Model):
                     lambda w: w.next_work_order_id == workorder
                 )
             )
+
+    def write(self, values):
+        # Enable changing duration of a workorder. It will change the end date of the
+        # production too, if it's the last workorder.
+        # todo move next workorders? Ask the user if do it, as workorders could be
+        #  asynchronous
+        if 'date_planned_start' in values or 'date_planned_finished' in values:
+            for workorder in self:
+                start_date = fields.Datetime.to_datetime(
+                    values.get('date_planned_start', workorder.date_planned_start))
+                end_date = fields.Datetime.to_datetime(
+                    values.get('date_planned_finished', workorder.date_planned_finished))
+                if start_date and end_date and start_date > end_date:
+                    raise UserError(_(
+                        'The planned end date of the work order cannot be prior to the '
+                        'planned start date, please correct this to save the work order'
+                        '.')
+                    )
+                if start_date and end_date:
+                    computed_duration = workorder._calculate_duration_expected(
+                        date_planned_start=start_date, date_planned_finished=end_date)
+                    values['duration_expected'] = computed_duration
+        res = super().write(values)
+        # todo is it possible to open a wizard to ask confirm adding it to res?
+        return res
