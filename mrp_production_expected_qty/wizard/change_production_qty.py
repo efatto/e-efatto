34914f5d14b57b662b504f40aeca1491be536cdf
sergiocorato <sergiocorato@gmail.com>
2025-07-12 00:39:32 +0200

    [IMP] mrp_production_expected_qty: black, isort, prettier

--- mrp_production_expected_qty/wizard/change_production_qty.py
+++ mrp_production_expected_qty/wizard/change_production_qty.py
@@ -1,10 +1,9 @@
-from odoo import api, models
+from odoo import models
 
 
 class ChangeProductionQty(models.TransientModel):
-    _inherit = 'change.production.qty'
+    _inherit = "change.production.qty"
 
-    @api.multi
     def change_prod_qty(self):
         for wizard in self:
             # recompute qty for expected bom lines with original linked bom qty
@@ -12,9 +11,12 @@ class ChangeProductionQty(models.TransientModel):
             for move in production.move_raw_ids.filtered(
                 lambda x: x.expected_product_uom_qty and x.bom_line_id
             ):
-                factor = production.product_uom_id._compute_quantity(
-                    wizard.product_qty, production.bom_id.product_uom_id
-                ) / production.bom_id.product_qty
+                factor = (
+                    production.product_uom_id._compute_quantity(
+                        wizard.product_qty, production.bom_id.product_uom_id
+                    )
+                    / production.bom_id.product_qty
+                )
                 if move.expected_product_uom_qty != move.bom_line_id.product_qty:
                     factor *= (
                         move.expected_product_uom_qty
