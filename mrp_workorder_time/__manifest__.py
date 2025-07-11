a136da0c05bebe14db103d0f44648152197da3a6
sergiocorato <sergiocorato@gmail.com>
2025-07-11 18:45:09 +0200

    [IMP] mrp_workorder_time: black, isort, prettier

--- mrp_workorder_time/__manifest__.py
+++ mrp_workorder_time/__manifest__.py
@@ -1,17 +1,17 @@
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'MRP Workcenter Productivity Time',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Manufacturing',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/e-efatto',
-    'depends': [
-        'mrp_production_demo',
+    "name": "MRP Workcenter Productivity Time",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "mrp_production_demo",
     ],
-    'data': [
-        'views/mrp_workorder.xml',
+    "data": [
+        "views/mrp_workorder.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
--- mrp_workorder_time/models/mrp_workcenter_productivity.py
+++ mrp_workorder_time/models/mrp_workcenter_productivity.py
@@ -1,34 +1,31 @@
-# Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.tools.date_utils import relativedelta
 
 
 class MrpWorkcenterProductivity(models.Model):
-    _inherit = 'mrp.workcenter.productivity'
+    _inherit = "mrp.workcenter.productivity"
 
     unit_amount = fields.Float(
-        string='Worked hours', default=0.0,
+        string="Worked hours",
+        default=0.0,
         help="This time will added to date start to compute date end and automatically "
-             "compute duration in seconds. When filled, date start will be reset to "
-             "minutes."
+        "compute duration in seconds. When filled, date start will be reset to "
+        "minutes.",
     )
-    loss_id = fields.Many2one(
-        default=lambda self: self._default_loss_id())
+    loss_id = fields.Many2one(default=lambda self: self._default_loss_id())
 
     def _default_loss_id(self):
-        return self.env['mrp.workcenter.productivity.loss'].search(
-            [('loss_type', '=', 'productive')],
-            limit=1)
+        return self.env["mrp.workcenter.productivity.loss"].search(
+            [("loss_type", "=", "productive")], limit=1
+        )
 
-    @api.onchange('unit_amount')
+    @api.onchange("unit_amount")
     def _onchange_unit_amount(self):
         if self.unit_amount:
             self.date_start = self.date_start.replace(second=0)
             self.date_end = self.date_start + relativedelta(hours=self.unit_amount)
 
-    @api.onchange('date_start')
+    @api.onchange("date_start")
     def _onchange_date_start(self):
         if self.unit_amount:
             self.date_end = self.date_start + relativedelta(hours=self.unit_amount)
