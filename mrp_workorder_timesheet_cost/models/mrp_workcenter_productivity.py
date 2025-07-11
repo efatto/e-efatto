a789fb8af0b5922435bb885a0e7358a2de82e49f
sergiocorato <sergiocorato@gmail.com>
2025-07-11 18:37:52 +0200

    [IMP] mrp_workorder_timesheet_cost: black, isort, prettier

--- mrp_workorder_timesheet_cost/models/mrp_workcenter_productivity.py
+++ mrp_workorder_timesheet_cost/models/mrp_workcenter_productivity.py
@@ -2,12 +2,10 @@ from odoo import api, fields, models
 
 
 class MrpWorkcenterProductivity(models.Model):
-    _inherit = 'mrp.workcenter.productivity'
+    _inherit = "mrp.workcenter.productivity"
 
-    currency_id = fields.Many2one(
-        related='employee_id.currency_id',
-        readonly=True)
-    amount = fields.Monetary('Amount', required=True, default=0.0)
+    currency_id = fields.Many2one(related="employee_id.currency_id", readonly=True)
+    amount = fields.Monetary("Amount", required=True, default=0.0)
 
     @api.model
     def create(self, values):
@@ -16,13 +14,11 @@ class MrpWorkcenterProductivity(models.Model):
             result._productivity_postprocess(values)
         return result
 
-    @api.multi
     def write(self, values):
         result = super().write(values)
         self.filtered(lambda x: x.employee_id)._productivity_postprocess(values)
         return result
 
-    @api.multi
     def _productivity_postprocess(self, values):
         """
         Hook to update record one by one according to the values of a `write`
@@ -39,18 +35,22 @@ class MrpWorkcenterProductivity(models.Model):
 
     @api.multi
     def _productivity_postprocess_values(self, values):
-        """ Get the addionnal values to write on record
-            :param dict values: values for the model's fields, as a dictionary::
-                {'field_name': field_value, ...}
-            :return: a dictionary mapping each record id to its corresponding
-                dictionnary values to write (may be empty).
+        """Get the addionnal values to write on record
+        :param dict values: values for the model's fields, as a dictionary::
+            {'field_name': field_value, ...}
+        :return: a dictionary mapping each record id to its corresponding
+            dictionnary values to write (may be empty).
         """
         result = {id_: {} for id_ in self.ids}
         sudo_self = self.sudo()
         # (re)compute the amount (depending on duration and employee_id for the cost)
         # currency_id is related to employee_id so should never be here - to check
-        if any([field_name in values for field_name in ['duration', 'employee_id',
-                                                        'date_start', 'date_end']]):
+        if any(
+            [
+                field_name in values
+                for field_name in ["duration", "employee_id", "date_start", "date_end"]
+            ]
+        ):
             for productivity in sudo_self:
                 cost = productivity.employee_id.timesheet_cost or 0.0
                 amount = -productivity.duration / 60.0 * cost
@@ -59,8 +59,11 @@ class MrpWorkcenterProductivity(models.Model):
                         amount,
                         productivity.currency_id,
                         self.env.user.company_id,
-                        productivity.date_start)
-                result[productivity.id].update({
-                    'amount': amount,
-                })
+                        productivity.date_start,
+                    )
+                result[productivity.id].update(
+                    {
+                        "amount": amount,
+                    }
+                )
         return result
