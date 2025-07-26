fdd6e6915d166c85cf93a8da0fd87b6e1996a5c1
sergiocorato <sergiocorato@gmail.com>
2025-07-26 15:53:31 +0200

    [MIG] sale_delivery_recreate: Migration to 14.0

--- sale_delivery_recreate/models/sale.py
+++ sale_delivery_recreate/models/sale.py
@@ -1,13 +1,9 @@
-# Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.multi
     def delivery_recreate(self):
         for order in self:
             order.with_context(
