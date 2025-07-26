45e3a12553ced895a6286410b926be85729c01d8
sergiocorato <sergiocorato@gmail.com>
2025-07-26 17:17:43 +0200

    [IMP] sale_order_recalculation_visibility: black, isort, prettier

--- sale_order_recalculation_visibility/__manifest__.py
+++ sale_order_recalculation_visibility/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Sale order price recalculation visibility',
-    'version': '12.0.1.0.0',
-    'category': 'Sale Management',
-    'license': 'AGPL-3',
-    'author': 'Sergio Corato',
-    'summary': 'Show recalculation in sale state too.',
-    'website': 'https://github.com/efatto/e-efatto',
-    'depends': [
-        'sale_order_price_recalculation',
+    "name": "Sale order price recalculation visibility",
+    "version": "14.0.1.0.0",
+    "category": "Sale Management",
+    "license": "AGPL-3",
+    "author": "Sergio Corato",
+    "summary": "Show recalculation in sale state too.",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "sale_order_price_recalculation",
     ],
-    'data': [
-        'security/security.xml',
-        'views/sale_order_view.xml',
+    "data": [
+        "security/security.xml",
+        "views/sale_order_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
