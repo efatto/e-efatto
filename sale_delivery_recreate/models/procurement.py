03c45a861dcee9b5ecc1a2be723b0cf8a5541cb6
sergiocorato <sergiocorato@gmail.com>
2025-08-04 14:11:32 +0200

    [MIG] sale_delivery_recreate to 14.0

--- sale_delivery_recreate/models/procurement.py
+++ sale_delivery_recreate/models/procurement.py
@@ -10,43 +10,55 @@ class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
 
     @api.model
-    def run(
-        self, product_id, product_qty, product_uom, location_id, name, origin, values
-    ):
+    def run(self, procurements, raise_user_error=True):
         if self.env.context.get("delivery_create_only", False):
-            values.setdefault(
-                "company_id",
-                self.env["res.company"]._company_default_get("procurement.group"),
-            )
-            values.setdefault("priority", "1")
-            values.setdefault("date_planned", fields.Datetime.now())
-            rule = self._get_rule(product_id, location_id, values)
-            if not rule:
-                raise UserError(
-                    _(
-                        'No procurement rule found in location "%s" for '
-                        'product "%s".\n Check routes configuration.'
+            new_procs = []
+            Proc = self.env["procurement.group"].Procurement
+            for procurement in procurements:
+                if procurement.values.get("sale_line_id"):
+                    # values.setdefault(
+                    #     "company_id",
+                    #     self.env["res.company"]._company_default_get("procurement.group"),
+                    # )
+                    # values.setdefault("priority", "1")
+                    # values.setdefault("date_planned", fields.Datetime.now())
+                    # rule = self._get_rule(product_id, location_id, values)
+                    # if not rule:
+                    #     raise UserError(
+                    #         _(
+                    #             'No procurement rule found in location "%s" for '
+                    #             'product "%s".\n Check routes configuration.'
+                    #         )
+                    #         % (location_id.display_name, product_id.display_name)
+                    #     )
+                    # action = "pull" if rule.action == "pull_push" else rule.action
+                    # if action == "pull":
+                    #     if hasattr(rule, "_run_%s" % action):
+                    #         getattr(rule, "_run_%s" % action)(
+                    #             product_id,
+                    #             product_qty,
+                    #             product_uom,
+                    #             location_id,
+                    #             name,
+                    #             origin,
+                    #             values,
+                    #         )
+                    #     else:
+                    #         _logger.error(
+                    #             "The method _run_%s doesn't exist on the procument "
+                    #             "rules" % action
+                    #         )
+                    new_procs.append(
+                        Proc(
+                            procurement.product_id,
+                            procurement.product_qty,
+                            procurement.product_uom,
+                            procurement.location_id,
+                            procurement.name,
+                            procurement.origin,
+                            procurement.company_id,
+                            procurement.values,
+                        )
                     )
-                    % (location_id.display_name, product_id.display_name)
-                )
-            action = "pull" if rule.action == "pull_push" else rule.action
-            if action == "pull":
-                if hasattr(rule, "_run_%s" % action):
-                    getattr(rule, "_run_%s" % action)(
-                        product_id,
-                        product_qty,
-                        product_uom,
-                        location_id,
-                        name,
-                        origin,
-                        values,
-                    )
-                else:
-                    _logger.error(
-                        "The method _run_%s doesn't exist on the procument "
-                        "rules" % action
-                    )
-            return True
-        return super().run(
-            product_id, product_qty, product_uom, location_id, name, origin, values
-        )
+            return super().run(new_procs, raise_user_error=raise_user_error)
+        return super().run(procurements, raise_user_error=raise_user_error)
--- sale_delivery_recreate/tests/test_sale_delivery_recreate.py
+++ sale_delivery_recreate/tests/test_sale_delivery_recreate.py
@@ -1,34 +1,35 @@
 from odoo.tests import Form
 from odoo.tests.common import SavepointCase
+from odoo.tools import mute_logger
 
 
 class TestSaleDeliveryRecreate(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.procurement_model = cls.env["procurement.group"]
         cls.partner = cls.env.ref("base.res_partner_2")
         # Acoustic Bloc Screens, 16 on hand
         cls.product1 = cls.env.ref("product.product_product_25")
-        # Cabinet with Doors, 8 on hand
-        cls.product2 = cls.env.ref("product.product_product_10")
         # Large Cabinet, 250 on hand
         cls.product3 = cls.env.ref("product.product_product_6")
         cls.product1.invoice_policy = "order"
-        vendor1 = cls.env["res.partner"].create(
+        vendor = cls.env["res.partner"].create(
             {"name": "AAA", "email": "from.test@example.com"}
         )
-        supplier_info1 = cls.env["product.supplierinfo"].create(
-            {
-                "name": vendor1.id,
-                "price": 50,
-            }
-        )
+        supplier_info_form = Form(cls.env["product.supplierinfo"])
+        supplier_info_form.name = vendor
+        supplier_info_form.price = 50
+        supplier_info = supplier_info_form.save()
         route_buy = cls.env.ref("purchase_stock.route_warehouse0_buy")
         warehouse1 = cls.env.ref("stock.warehouse0")
         route_mto = warehouse1.mto_pull_id.route_id
-        cls.product2.write(
+        route_mto.active = True
+        cls.product2 = cls.env["product.product"].create(
             {
-                "seller_ids": [(6, 0, [supplier_info1.id])],
+                "name": "Test Cabinet",
+                "type": "product",
+                "seller_ids": [(6, 0, [supplier_info.id])],
                 "route_ids": [(6, 0, [route_buy.id, route_mto.id])],
             }
         )
@@ -48,6 +49,10 @@ class TestSaleDeliveryRecreate(SavepointCase):
         order.action_confirm()
         self.assertEqual(order.state, "sale")
         self.assertEqual(len(order.picking_ids), 1)
+        with mute_logger("odoo.addons.stock.models.procurement"):
+            self.procurement_model.run_scheduler()
+        po = self.env["purchase.order"].search([("origin", "=", order.name)])
+        self.assertEqual(sum(po.mapped("order_line.product_qty")), 10)
         order.picking_ids[0].action_cancel()
         order.picking_ids[0].unlink()
         self.assertEqual(len(order.picking_ids), 0)
@@ -70,14 +75,13 @@ class TestSaleDeliveryRecreate(SavepointCase):
         order1 = order_form.save()
         order1.action_confirm()
         self.assertEqual(order1.state, "sale")
+        po1 = self.env["purchase.order"].search([("origin", "=", order1.name)])
+        self.assertEqual(sum(po1.mapped("order_line.product_qty")), 10)
         picking = order1.picking_ids[0]
         self.assertEqual(sum(picking.mapped("move_lines.product_uom_qty")), 15)
         picking.move_lines[0].move_line_ids[0].qty_done = 3
-        backorder_wiz_id = picking.button_validate()["res_id"]
-        backorder_wiz = self.env["stock.backorder.confirmation"].browse(
-            backorder_wiz_id
-        )
-        backorder_wiz.process()
+        res = picking.button_validate()
+        Form(self.env[res["res_model"]].with_context(res["context"])).save().process()
         self.assertEqual(len(order1.picking_ids), 2)
         picking1 = order1.picking_ids - picking
         picking1.action_cancel()
