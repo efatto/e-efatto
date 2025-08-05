54aaf0241a7ae176ce690fe39baeca01e00f3b7f
sergiocorato <sergiocorato@gmail.com>
2025-08-05 09:21:33 +0200

    [IMP] mrp_production_line_group: black, isort, prettier

--- mrp_production_line_group/__manifest__.py
+++ mrp_production_line_group/__manifest__.py
@@ -2,18 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Production Group Lines By Product',
-    'summary': 'Add ability to group lines with same product',
-    'version': '12.0.1.0.1',
-    'category': 'MRP',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/e-efatto',
-    'license': 'AGPL-3',
-    'maintainers': ['sergiocorato'],
-    'depends': ['mrp_production_demo'],
-    'data': [
-        'wizard/production_group_line_wizard.xml',
-        'views/mrp_production.xml',
+    "name": "Production Group Lines By Product",
+    "summary": "Add ability to group lines with same product",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "license": "AGPL-3",
+    "maintainers": ["sergiocorato"],
+    "depends": ["mrp_production_demo"],
+    "data": [
+        "wizard/production_group_line_wizard.xml",
+        "views/mrp_production.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
