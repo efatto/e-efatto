2ec394fe61bf60174e907458e0c1419710fe3047
sergiocorato <sergiocorato@gmail.com>
2025-08-05 19:09:07 +0200

    [MIG] mrp_production_component_delete: Migration to 14.0

--- mrp_production_component_delete/models/stock_move.py
+++ mrp_production_component_delete/models/stock_move.py
@@ -1,7 +1,4 @@
-# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
@@ -9,7 +6,6 @@ class StockMove(models.Model):
 
     is_locked = fields.Boolean(related="raw_material_production_id.is_locked")
 
-    @api.multi
     def delete_production_component(self):
         self.ensure_one()
         self._action_cancel()
