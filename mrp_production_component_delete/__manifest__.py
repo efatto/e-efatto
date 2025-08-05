c664bdc8fe93b0242b98a10887f0587d8725c0d3
sergiocorato <sergiocorato@gmail.com>
2025-08-05 19:08:35 +0200

    [IMP] mrp_production_component_delete: black, isort, prettier

--- mrp_production_component_delete/__manifest__.py
+++ mrp_production_component_delete/__manifest__.py
@@ -1,18 +1,17 @@
 # Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'MRP Production Component Delete',
-    'version': '12.0.1.0.0',
-    'development_status': 'Beta',
-    'license': 'AGPL-3',
-    'category': 'Manufacturing',
-    'author': 'Sergio Corato',
-    'website': 'https://github.com/efatto/e-efatto',
-    'depends': [
-        'mrp',
+    "name": "MRP Production Component Delete",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "mrp",
     ],
-    'data': [
-        'views/mrp.xml',
+    "data": [
+        "views/mrp.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
