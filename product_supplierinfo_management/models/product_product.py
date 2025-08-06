f2ca4a6a26b89e55750a27ecefe0f79d06bc6070
sergiocorato <sergiocorato@gmail.com>
2025-08-06 09:30:27 +0200

    [FIX] pre-commit and view - use of Form() in tests

--- product_supplierinfo_management/models/product_product.py
+++ product_supplierinfo_management/models/product_product.py
@@ -104,7 +104,7 @@ class ProductProduct(models.Model):
                 lines = invoice_line_obj.search(
                     [
                         ("product_id", "=", product.id),
-                        ("move_type", "=", "in_invoice"),
+                        ("move_id.move_type", "=", "in_invoice"),
                         ("invoice_state", "not in", ["draft", "cancel"]),
                     ]
                 ).sorted(key=lambda l: l.move_id.invoice_date, reverse=True)
