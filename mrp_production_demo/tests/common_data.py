5b954041d49794bff71ad3345fba0656f556aac1
sergiocorato <sergiocorato@gmail.com>
2025-08-25 15:04:05 +0200

    [IMP] add method reusable in mrp_production_demo

--- mrp_production_demo/tests/common_data.py
+++ mrp_production_demo/tests/common_data.py
@@ -121,3 +121,8 @@ class TestProductionData(SavepointCase):
         )
         product_qty.change_product_qty()
         return product_qty
+
+    @staticmethod
+    def _auto_fill_consumed_qty(moves):
+        for move in moves:
+            move.quantity_done = move.product_uom_qty
