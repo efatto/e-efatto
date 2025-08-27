7ddbbed104e269f81e4e6d26f88eab6a6cdacd03
sergiocorato <sergiocorato@gmail.com>
2025-08-27 14:53:29 +0200

    [IMP] stock_picking_restrict_cancel_with_orig_move_mrp: black, isort, prettier

--- stock_picking_restrict_cancel_with_orig_move_mrp/models/stock_move.py
+++ stock_picking_restrict_cancel_with_orig_move_mrp/models/stock_move.py
@@ -5,20 +5,23 @@ from odoo import models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     def _action_cancel(self):
-        orig_moves = self.mapped('move_orig_ids')
-        productions = orig_moves.mapped('production_id')
+        orig_moves = self.mapped("move_orig_ids")
+        productions = orig_moves.mapped("production_id")
         if productions and all(
-            production.state in ['cancel', 'confirmed'] for production in productions
+            production.state in ["cancel", "confirmed"] for production in productions
         ):
-            res = super(StockMove, self.with_context(
-                bypass_check_state=True))._action_cancel()
-            finish_moves = productions.mapped('move_finished_ids').filtered(
-                lambda x: x.state not in ('done', 'cancel'))
-            raw_moves = productions.mapped('move_raw_ids').filtered(
-                lambda x: x.state not in ('done', 'cancel'))
+            res = super(
+                StockMove, self.with_context(bypass_check_state=True)
+            )._action_cancel()
+            finish_moves = productions.mapped("move_finished_ids").filtered(
+                lambda x: x.state not in ("done", "cancel")
+            )
+            raw_moves = productions.mapped("move_raw_ids").filtered(
+                lambda x: x.state not in ("done", "cancel")
+            )
             if all(x not in self for x in (finish_moves | raw_moves)):
                 for production in productions:
                     production.action_cancel()
