c51dae37725122627e5655ff17dcc2741f412ae9
sergiocorato <sergiocorato@gmail.com>
2025-08-27 14:41:37 +0200

    [IMP] stock_picking_transfer_date: black, isort, prettier

--- stock_picking_transfer_date/models/stock_move.py
+++ stock_picking_transfer_date/models/stock_move.py
@@ -4,13 +4,13 @@ from odoo import api, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     @api.multi
     def _action_done(self):
         moves_todo = super()._action_done()
         for move in moves_todo:
             if move.picking_id.transfer_date:
-                move.write({'date': move.picking_id.transfer_date})
-                move.move_line_ids.write({'date': move.picking_id.transfer_date})
+                move.write({"date": move.picking_id.transfer_date})
+                move.move_line_ids.write({"date": move.picking_id.transfer_date})
         return moves_todo
