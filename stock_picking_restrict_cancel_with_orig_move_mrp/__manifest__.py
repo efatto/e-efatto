17a91c1823fd260fff0e75a431789b1d15c1f7bc
sergiocorato <sergiocorato@gmail.com>
2025-08-27 14:53:43 +0200

    [MIG] stock_picking_restrict_cancel_with_orig_move_mrp: Migration to 14.0

--- stock_picking_restrict_cancel_with_orig_move_mrp/__manifest__.py
+++ stock_picking_restrict_cancel_with_orig_move_mrp/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2020 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Stock Picking Restrict Cancel with Original Moves",
-    "summary": "Restrict cancellation of dest moves according to origin.",
+    "name": "Stock Picking Restrict Cancel in MRP with Original Moves",
+    "summary": "Restrict cancellation of dest moves according to origin for MRP.",
     "version": "14.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/efatto/e-efatto",
--- stock_picking_restrict_cancel_with_orig_move_mrp/tests/test_restrict_cancel.py
+++ stock_picking_restrict_cancel_with_orig_move_mrp/tests/test_restrict_cancel.py
@@ -1,14 +1,13 @@
-# Copyright 2018 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestRestrictCancelStockMove(TransactionCase):
     def setUp(self):
         super(TestRestrictCancelStockMove, self).setUp()
         self.warehouse = self.env.ref("stock.warehouse0")
-        route_manufacture = self.warehouse.manufacture_pull_id.route_id.id
-        route_mto = self.warehouse.mto_pull_id.route_id.id
+        route_manufacture = self.warehouse.manufacture_pull_id.route_id
+        self.warehouse.mto_pull_id.route_id.active = True
+        route_mto = self.warehouse.mto_pull_id.route_id
         self.uom_unit = self.env.ref("uom.product_uom_unit")
         self.dummy_product = (
             self.env["product.template"]
@@ -18,7 +17,7 @@ class TestRestrictCancelStockMove(TransactionCase):
                     "type": "product",
                     "sale_ok": True,
                     "uom_id": self.uom_unit.id,
-                    "route_ids": [(6, 0, [route_manufacture, route_mto])],
+                    "route_ids": [(6, 0, [route_manufacture.id, route_mto.id])],
                 }
             )
             .product_variant_ids
@@ -51,29 +50,20 @@ class TestRestrictCancelStockMove(TransactionCase):
         )
 
     def test_do_not_restrict(self):
-        sale_order = self.env["sale.order"].create(
-            {
-                "partner_id": self.env.ref("base.res_partner_2").id,
-                "client_order_ref": "Ref SO",
-                "order_line": (
-                    [
-                        (
-                            0,
-                            0,
-                            {
-                                "product_id": self.dummy_product.id,
-                                "price_unit": 50,
-                                "product_uom_qty": 5,
-                            },
-                        ),
-                    ]
-                ),
-            }
-        )
+        sale_order_form = Form(self.env["sale.order"])
+        sale_order_form.partner_id = self.env.ref("base.res_partner_2")
+        sale_order_form.client_order_ref = "Ref SO"
+        with sale_order_form.order_line.new() as order_line:
+            order_line.product_id = self.dummy_product
+            order_line.price_unit = 50
+            order_line.product_uom_qty = 5
+        sale_order = sale_order_form.save()
         sale_order.action_confirm()
         production = self.env["mrp.production"].search(
             [("origin", "=", sale_order.name)]
         )
+        self.assertTrue(production)
+        production.action_confirm()
         self.assertEqual(production.state, "confirmed")
         sale_order.action_cancel()
         self.assertEqual(production.mapped("move_finished_ids.state")[0], "cancel")
