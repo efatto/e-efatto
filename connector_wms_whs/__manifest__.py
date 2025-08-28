ba3af4947e9f26c71af8f12dddbda0f91ecf6057
sergiocorato <sergiocorato@gmail.com>
2025-08-28 15:00:01 +0200

    [IMP] bump version

--- connector_wms_whs/__manifest__.py
+++ connector_wms_whs/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Connector WHS WMS",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Warehouse Management",
     "license": "AGPL-3",
     "summary": """
