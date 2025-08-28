6b7be775526ef78c4d995b19ec3c146a66c574b4
sergiocorato <sergiocorato@gmail.com>
2025-08-28 13:50:34 +0200

    [FIX] migration script

--- product_supplierinfo_management/migrations/13.0.1.0.0/pre-migration.py
+++ product_supplierinfo_management/migrations/13.0.1.0.0/pre-migration.py
@@ -1,5 +1,27 @@
 from openupgradelib import openupgrade
 
+from psycopg2 import sql
+
+
+def drop_constraint(cr, table, column):
+    drop_sql = sql.SQL("ALTER TABLE {} DROP CONSTRAINT {}")
+    cr.execute(
+        """
+            SELECT constraint_name
+            FROM information_schema.table_constraints
+            WHERE constraint_type = 'FOREIGN KEY' AND table_name = %s
+            AND constraint_name like %s
+        """,
+        (table, "%%%s%%" % column),
+    )
+    for constraint in (row[0] for row in cr.fetchall()):
+        openupgrade.logged_query(
+            cr,
+            drop_sql.format(
+                sql.Identifier(table),
+                sql.Identifier(constraint),
+            ),
+        )
 
 def migrate_invoice_line_values(cr, table, column):
     query = """
@@ -14,14 +36,8 @@ WHERE aml.old_invoice_line_id = {table}.{column}
     return openupgrade.logged_query(cr, query)
 
 
-@openupgrade.migrate()
-def migrate(env, installed_version):
+def migrate(cr, installed_version):
     column = "last_supplier_invoice_line_id"
     for table in ["product_product", "product_template"]:
-        openupgrade.delete_sql_constraint_safely(
-            env,
-            "product_supplierinfo_management",
-            table,
-            f"{table}_last_supplier_invoice_line_id_fkey",
-        )
-        migrate_invoice_line_values(env.cr, table, column)
+        drop_constraint(cr, table, column)
+        migrate_invoice_line_values(cr, table, column)
