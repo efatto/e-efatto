98b1c29dcf4683f13a71a1e5d7d8f4f92bc55be0
sergiocorato <sergiocorato@gmail.com>
2025-09-01 19:02:22 +0200

    [IMP] bump version mail_activity_timeline

--- mail_activity_timeline/__manifest__.py
+++ mail_activity_timeline/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Resource Planner Timeline",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     "development_status": "Alpha",
     "category": "Mail",
