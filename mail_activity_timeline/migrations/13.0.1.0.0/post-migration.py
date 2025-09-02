9cdfaee4d93cc08eb2339784e3bc3d9a22ceac8c
sergiocorato <sergiocorato@gmail.com>
2025-09-02 09:58:02 +0200

    [IMP] put migration in v. 13.0 - but this should be unnecessary

--- mail_activity_timeline/migrations/13.0.1.0.0/post-migration.py
+++ mail_activity_timeline/migrations/13.0.1.0.0/post-migration.py
@@ -4,5 +4,5 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "mail_activity_timeline", "migrations/14.0.1.0.0/noupdate_changes.xml"
+        env.cr, "mail_activity_timeline", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
