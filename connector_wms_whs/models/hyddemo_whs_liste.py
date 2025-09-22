4611348300184af4024a47ab361b572371791f0a
sergiocorato <sergiocorato@gmail.com>
2025-09-22 18:25:52 +0200

    [IMP] connector_wms_whs changes to handle new causale 12

--- connector_wms_whs/models/hyddemo_whs_liste.py
+++ connector_wms_whs/models/hyddemo_whs_liste.py
@@ -235,7 +235,9 @@ Elaborato,
 AuxTesto1,
 AuxTestoRiga1,
 AuxTestoRiga2,
-AuxTestoRiga3
+AuxTestoRiga3,
+AuxTestoRiga4,
+AuxTestoRiga5
 )
 VALUES (
 :NumLista,
@@ -266,7 +268,9 @@ VALUES (
 :AuxTesto1,
 :AuxTestoRiga1,
 :AuxTestoRiga2,
-:AuxTestoRiga3
+:AuxTestoRiga3,
+:AuxTestoRiga4,
+:AuxTestoRiga5
 )
 """
         if "idCliente" in params:
@@ -307,6 +311,18 @@ VALUES (
             parent_product_id = (
                 lista.parent_product_id if lista.parent_product_id else False
             )
+            produced_product_code = (
+                (
+                    parent_product_id.default_code[:250]
+                    if parent_product_id.default_code
+                    else parent_product_id.name[:250]
+                )
+                if parent_product_id
+                else ""
+            )
+            product_customer_code = (
+                lista.product_customer_code[:250] if lista.product_customer_code else ""
+            )
             execute_params_order[lista.num_lista][lista.riga] = {
                 "NumLista": lista.num_lista[:50],  # char 50
                 "NumRiga": lista.riga,  # char 50 but is an integer
@@ -316,7 +332,7 @@ VALUES (
                 # char 50
                 "TipoOrdine": lista.tipo[0],  # int - get only the first characther
                 "Causale": lista.tipo + "0" if len(lista.tipo) == 1 else lista.tipo,
-                # int: 10=prelievo, 11=assemblaggio, 20=deposito
+                # int: 10=prelievo, 11=assemblaggio, 12=isola robot, 20=deposito
                 "Priorita": lista.priorita,  # int
                 "RichiestoEsito": 1,  # int
                 "Stato": 0,  # int
@@ -345,19 +361,22 @@ VALUES (
                 "AuxTesto1": lista.client_order_ref[:50]
                 if lista.client_order_ref
                 else "",  # char 50
-                "AuxTestoRiga1": lista.product_customer_code[:250]
-                if lista.product_customer_code
-                else "",  # char 250
-                "AuxTestoRiga2": lista.product_customer_code[:250]
-                if lista.product_customer_code
-                else "",  # char 250
-                "AuxTestoRiga3": (
-                    parent_product_id.default_code[:250]
-                    if parent_product_id.default_code
-                    else parent_product_id.name[:250]
-                )
-                if parent_product_id
-                else "",  # char 250
+                "AuxTestoRiga1": product_customer_code,  # todo cambiarlo?
+                # char 250 -> NoteHost
+                "AuxTestoRiga2": product_customer_code,  # todo cambiarlo?
+                # char 250 -> Distinta
+                "AuxTestoRiga3": produced_product_code,  # todo cambiarlo?
+                # char 250 -> Cod. Art. Cliente
+                "AuxTestoRiga4": "",  # todo cambiarlo?
+                # char 250 -> Descrizione
+                # (mai popolato sulla tabella)
+                "AuxTestoRiga5": produced_product_code,
+                # char 250 -> Codice articolo finito (per lavorazioni robot)
+                # "AuxTestoRiga6": "",  # char 250 -> NON USATO
+                # "Lotto": "",  # -> AnnoSettimana
+                # "Lotto2": "",  # -> Lotto Fornitore
+                # "Lotto3": "",  # -> NumeroDDT
+                # "Lotto4": "",  # -> Disegno
             }
             if lista.cliente:  # char 30
                 execute_params_order[lista.num_lista][lista.riga].update(
