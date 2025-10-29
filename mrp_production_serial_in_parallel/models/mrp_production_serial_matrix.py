3b81604d1ff496a9d38496a1d2c2d2123169908a
sergiocorato <sergiocorato@gmail.com>
2025-10-29 18:35:44 +0100

    [IMP] split work times in serial production

--- mrp_production_serial_in_parallel/models/mrp_production_serial_matrix.py
+++ mrp_production_serial_in_parallel/models/mrp_production_serial_matrix.py
@@ -1,8 +1,48 @@
+from dateutil.relativedelta import relativedelta
+
 from odoo import models
 
 
 class MrpProductionSerialMatrix(models.TransientModel):
     _inherit = "mrp.production.serial.matrix"
+
+    @staticmethod
+    def _split_work_time(production, backorder_ids):
+        for backorder in backorder_ids:
+            for workorder in production.workorder_ids:
+                if workorder.time_ids:
+                    back_workorder = backorder.workorder_ids.filtered(
+                        lambda w: w.sequence == workorder.sequence
+                        and w.name == workorder.name
+                        and w.workcenter_id == workorder.workcenter_id
+                    )
+                    date_start = False
+                    for workorder_time in workorder.time_ids:  # todo check sort!
+                        new_duration = workorder_time.duration / (
+                            len(backorder_ids) + 1
+                        )
+                        if not date_start or workorder_time == workorder.time_ids[0]:
+                            date_start = workorder_time.date_start + relativedelta(
+                                minutes=new_duration
+                            )
+                            # todo usare un calcolatore di tempo dalle risorse? o è
+                            #  inutile essendo che sono tempi sempre all'interno di
+                            #  un orario di lavoro?
+                        new_workorder_time = workorder_time.copy(
+                            default={
+                                "workorder_id": back_workorder.id,
+                                "date_start": date_start,
+                                "duration": new_duration,
+                            }
+                        )
+                        date_start = new_workorder_time.date_end
+        for workorder_time in production.workorder_ids.time_ids:
+            workorder_time.write(
+                {
+                    "duration": workorder_time.duration / (len(backorder_ids) + 1),
+                }
+            )
+        return False
 
     def button_validate(self):
         self.ensure_one()
@@ -27,10 +67,16 @@ class MrpProductionSerialMatrix(models.TransientModel):
             )
         res = super().button_validate()
         if parallel_production:
+            # parallel production is a copy without work times, the production with
+            # work times is the self.production_id, which is in the backorders too
             backorder_ids = (
                 self.production_id.procurement_group_id.mrp_production_ids.filtered(
                     lambda mo: mo.state != "cancel"
                 )
             )
             backorder_ids.write({"parallel_production_id": parallel_production.id})
+            if self.production_id.workorder_ids.time_ids:
+                self._split_work_time(
+                    self.production_id, backorder_ids - self.production_id
+                )
         return res
