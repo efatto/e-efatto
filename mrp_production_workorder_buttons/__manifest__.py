d1d0916df4f54b43b4817c432a4d49fb842a7e6a
sergiocorato <sergiocorato@gmail.com>
2025-10-30 15:39:04 +0100

    [ADD] module mrp_production_workorder_buttons

---
+++ mrp_production_workorder_buttons/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp workorder button in form view",
+    "version": "14.0.1.0.0",
+    "category": "Manufacture",
+    "license": "AGPL-3",
+    "summary": "Show buttons to start/stop workorder in form view",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_workorder.xml",
+    ],
+    "installable": True,
+}
