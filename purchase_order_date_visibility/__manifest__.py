f84793ff7d07d6fa029e6db4e2c31ee41300484d
sergiocorato <sergiocorato@gmail.com>
2025-11-11 09:54:22 +0100

    [IMP] removed remaining days widget from module purchase_order_date_visibility

--- purchase_order_date_visibility/__manifest__.py
+++ purchase_order_date_visibility/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Date Visibility",
     "summary": "Always show date order in purchase order.",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Purchases",
     "website": "https://github.com/efatto/e-efatto",
     "author": "Sergio Corato",
