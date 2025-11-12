9a018a062a424cfbc2c175dc0ac8c1dac0ea8e99
sergiocorato <sergiocorato@gmail.com>
2025-11-12 17:28:20 +0100

    [MIG] iot_input_mrp: Migration to 14.0

--- iot_input_mrp/models/mrp_workcenter.py
+++ iot_input_mrp/models/mrp_workcenter.py
@@ -1,5 +1,3 @@
-# Copyright 2022 Sergio Corato <https://github.com/sergiocorato>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
