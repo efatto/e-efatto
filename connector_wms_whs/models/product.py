a548e11e483afb2556d3ab2624918b216781e327
sergiocorato <sergiocorato@gmail.com>
2025-11-20 19:27:25 +0100

    [IMP] add option to get sync info from product

---
+++ connector_wms_whs/models/product.py
@@ -0,0 +1,34 @@
+from odoo import fields, models, _
+from odoo.addons.connector_whs.models.base_external_dbsource import clean_sql_text
+from odoo.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    def show_whs_syncronization_records(self):
+        res = super().show_whs_syncronization_records()
+        contents = ""
+        dbsource = self.env["base.external.dbsource"].search(
+            [("company_id", "=", self.company_id.id)], limit=1
+        )
+        connection = dbsource.connection_open_mssql()
+        if not connection:
+            raise UserError(_("Failed to open connection!"))
+        sql_result = dbsource.execute_mssql(
+            sqlquery=clean_sql_text(
+                "SELECT * FROM HOST_ARTICOLI WHERE Codice=:Codice"
+            ),
+            sqlparams={"Codice": self.default_code},
+            metadata=None
+        )
+        if sql_result[0]:
+            contents = _(
+                "Product is going to be synchronized with WHS with record: %s."
+                % str(sql_result[0]))
+        else:
+            contents = _("Product is going to be synchronized with WHS.")
+        res["params"].update({
+            "message": contents,
+        })
+        return res
