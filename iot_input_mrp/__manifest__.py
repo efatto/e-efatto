7adea70284deada82f870aac010e63e87e9047a8
sergiocorato <sergiocorato@gmail.com>
2026-01-09 22:11:33 +0000

    [MIG] iot_input_mrp to 16.0

--- iot_input_mrp/__manifest__.py
+++ iot_input_mrp/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "IOT MRP input",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacture",
     "license": "AGPL-3",
     "summary": """
