b645248df5f15e6e246e8936d7b8062dabc36d72
sergiocorato <sergiocorato@gmail.com>
2026-01-10 14:30:33 +0000

    [MIG] mrp_employee_productivity to 16.0

--- connector_wms_whs/tests/test_connector_wms_whs.py
+++ connector_wms_whs/tests/test_connector_wms_whs.py
@@ -384,9 +384,7 @@ class TestConnectorWmsWhs(CommonConnectorWMS):
         self.assertEqual(order1.picking_ids.state, "assigned")
         picking = order1.picking_ids[0]
         self.assertEqual(len(picking.mapped("move_ids.whs_list_ids")), 2)
-        self.assertEqual(
-            len(set(picking.mapped("move_ids.whs_list_ids.num_lista"))), 1
-        )
+        self.assertEqual(len(set(picking.mapped("move_ids.whs_list_ids.num_lista"))), 1)
 
         # check WMS list is added
         self.dbsource.whs_insert_read_and_synchronize_list()
