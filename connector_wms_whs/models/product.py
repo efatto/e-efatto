f5b54b651d85923c321d2b1c32d42c947bd1b050
sergiocorato <sergiocorato@gmail.com>
2026-02-16 20:49:41 +0100

    [IMP] connector_wms_whs get complete info for product

--- connector_wms_whs/models/product.py
+++ connector_wms_whs/models/product.py
@@ -23,14 +23,39 @@ class ProductProduct(models.Model):
         if not connection:
             raise UserError(_("Failed to open connection!"))
         sql_result = dbsource.execute_mssql(
-            sqlquery=clean_sql_text("SELECT * FROM HOST_ARTICOLI WHERE Codice=:Codice"),
+            sqlquery=clean_sql_text(
+                """
+SELECT * FROM HOST_ARTICOLI ha
+INNER JOIN (
+    SELECT hg.Articolo
+    FROM (
+        SELECT *, ROW_NUMBER() OVER(PARTITION BY Articolo ORDER BY id DESC) Corr
+        FROM HOST_GIACENZE
+    ) hg
+    WHERE hg.Corr = 1
+) hglast
+ON ha.Codice = hglast.Articolo
+INNER JOIN (
+    SELECT hap.id
+    FROM (
+        SELECT *, ROW_NUMBER() OVER(PARTITION BY Codice ORDER BY id DESC) Corr1
+        FROM HOST_ARTICOLI
+    ) hap
+    WHERE hap.Corr1 = 1
+) hglasta
+ON ha.id = hglasta.id
+WHERE ha.Codice=:Codice
+                """
+            ),
             sqlparams={"Codice": self.default_code},
-            metadata=None,
+            metadata=True,
         )
         if sql_result[0]:
+            cols = sql_result[1]
+            rows_with_headers = [dict(zip(cols, row)) for row in sql_result[0]]
             contents = _(
                 "Product is going to be synchronized with WHS with record: %s."
-                % str(sql_result[0])
+                % str(rows_with_headers)
             )
         else:
             contents = _("Product is going to be synchronized with WHS.")
