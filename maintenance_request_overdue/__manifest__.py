53038c07c0b01df70e35157479053bae5121f1a7
sergiocorato <sergiocorato@gmail.com>
2026-03-10 17:56:11 +0100

    [ADD] new module maintenance_request_overdue

---
+++ maintenance_request_overdue/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 Sergio Corato <https://github.com/sergiocorato>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Maintenance requests overdue",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Sergio Corato",
+    "website": "https://github.com/efatto/e-efatto",
+    "depends": [
+        "maintenance",
+    ],
+    "data": [
+        "views/maintenance.xml",
+    ],
+    "installable": True,
+}
