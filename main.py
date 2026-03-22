acf517f93ded3bcde4645e84706b6531d1f39d31
sergiocorato <sergiocorato@gmail.com>
2026-03-22 22:13:31 +0100

    [FIX] main.py in e-efatto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello from e-efatto18-0!")
+    logger.info("Hello from e-efatto18-0!")  # noqa
 
 
 if __name__ == "__main__":
