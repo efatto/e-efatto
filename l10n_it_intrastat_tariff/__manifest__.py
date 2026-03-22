d884c1d9eea37dfbbe99943be8b14e3d565cd4a0
sergiocorato <sergiocorato@gmail.com>
2026-03-22 17:46:25 +0100

    [MIG] l10n_it_intrastat_tariff: Migration to 18.0

--- l10n_it_intrastat_tariff/__manifest__.py
+++ l10n_it_intrastat_tariff/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Intrastat Customs Tariff",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
     "license": "AGPL-3",
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from e-efatto18-0!")
+
+
+if __name__ == "__main__":
+    main()
