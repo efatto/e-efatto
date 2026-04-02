c1be49e13bc59ec44f4c4f802b1b25462d1906ec
sergiocorato <sergiocorato@gmail.com>
2026-04-02 16:08:53 +0200

    [MIG] mrp_employee_productivity: Migration to 18.0

--- mrp_employee_productivity/__manifest__.py
+++ mrp_employee_productivity/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Employee MRP Workcenter Productivity Time",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Manufacturing",
     "author": "Sergio Corato",
