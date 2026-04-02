437499c1dafc0b9b0ae629e61a07033af1d2c6f7
sergiocorato <sergiocorato@gmail.com>
2026-04-02 16:08:45 +0200

    [MIG] l10n_it_country_data: Migration to 18.0

--- l10n_it_country_data/__manifest__.py
+++ l10n_it_country_data/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Italy - Country data",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
