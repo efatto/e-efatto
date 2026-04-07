9c86548e691735419e4a43abee126c1606e8ddd5
sergiocorato <sergiocorato@gmail.com>
2026-04-07 21:28:30 +0200

    [IMP] bump version mrp_production_lot_reserve

--- mrp_production_lot_reserve/__manifest__.py
+++ mrp_production_lot_reserve/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "MRP Production Reserve Lots",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "category": "Manufacturing",
     "author": "Sergio Corato",
