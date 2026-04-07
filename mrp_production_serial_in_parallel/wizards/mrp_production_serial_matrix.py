661dba1c0e34b3169e177cc9a0a4603917b55708
sergiocorato <sergiocorato@gmail.com>
2026-04-07 21:17:16 +0200

    [FIX] reset mess from previous production in mrp_production_serial_in_parallel

--- mrp_production_serial_in_parallel/wizards/mrp_production_serial_matrix.py
+++ mrp_production_serial_in_parallel/wizards/mrp_production_serial_matrix.py
@@ -99,6 +99,9 @@ class MrpProductionSerialMatrix(models.TransientModel):
 
     def button_validate(self):
         self.ensure_one()
+        # ensure no mess is coming from previous MOs writing 0 into producing qty
+        self.production_id.qty_producing = 0
+        self.production_id._onchange_product_qty()
         self.production_id._check_reserved_lot_qty()
         parallel_production = self._set_parallel_production()
         if self.lot_selection_warning_count > 0:
@@ -215,7 +218,8 @@ class MrpProductionSerialMatrix(models.TransientModel):
     def button_prepare(self):
         self.ensure_one()
         # ensure no mess is coming from previous MOs writing 0 into producing qty
-        self.production_id.write({"qty_producing": 0})
+        self.production_id.qty_producing = 0
+        self.production_id._onchange_product_qty()
         self.production_id._check_reserved_lot_qty()
         parallel_production = self._set_parallel_production()
         if self.lot_selection_warning_count > 0:
