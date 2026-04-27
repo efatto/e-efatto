48c40fecadd8685b33ddd95d7f2e2ce708853c55
sergiocorato <sergiocorato@gmail.com>
2026-04-27 10:41:09 +0200

    [MIG] product_template_listprice_zero_default: Migration to 18.0

--- product_template_listprice_zero_default/__manifest__.py
+++ product_template_listprice_zero_default/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2016-2020 Sergio Corato
+# Copyright 2026 Sergio Corato <https://github.com/sergiocorato>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product template list_price default zero",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "other",
     "author": "Sergio Corato",
     "summary": "Product template set default list price to zero",
