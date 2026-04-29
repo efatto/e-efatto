3f19eec330bcde9fcbaebd1e8d8f6de19debe796
sergiocorato <sergiocorato@gmail.com>
2026-04-29 20:00:59 +0200

    [MIG] mrp_production_cancel_confirm: Migration to 18.0

--- mrp_production_cancel_confirm/__manifest__.py
+++ mrp_production_cancel_confirm/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Mrp production cancel confirm",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Manufacture",
     "license": "AGPL-3",
     "summary": """
