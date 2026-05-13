78680aa86a526f0dc04beb43e94f814c0239dbe6
sergiocorato <sergiocorato@gmail.com>
2026-05-13 18:49:22 +0200

    [IMP] better test in connector_wms_modula for extra PO qty

--- connector_wms_modula/tests/test_connector_wms_modula.py
+++ connector_wms_modula/tests/test_connector_wms_modula.py
@@ -1045,11 +1045,12 @@ class TestConnectorWmsModula(CommonConnectorWMS):
         po_line_to_change.write({"product_qty": 27})
         pickings = purchase.picking_ids.filtered(lambda x: x.state == "assigned")
         pickings.action_assign()
-        po_whs_list = po_line_to_change.mapped("move_ids.whs_list_ids").filtered(
+        extra_po_whs_list = po_line_to_change.mapped("move_ids.whs_list_ids").filtered(
             lambda x: x.qta == 7
         )
+        self.assertTrue(extra_po_whs_list, "Missing extra PO whs list!")
         self.dbsource.whs_insert_read_and_synchronize_list()
-        result_liste = self._select_wms_liste(po_whs_list, db_type="IMP")
+        result_liste = self._select_wms_liste(extra_po_whs_list, db_type="IMP")
         # WMS list is created for the increased qty
         self.assertEqual(str(result_liste[0]), "[(Decimal('7.000'),)]")
 
