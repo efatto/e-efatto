8f6b79615af3e76ef55a04291fae3d2ebc8f466b
sergiocorato <sergiocorato@gmail.com>
2026-05-14 21:54:58 +0200

    [FIX] connector_whs production with components with tracking

--- connector_whs/__manifest__.py
+++ connector_whs/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Connector WHS MSSQL",
-    "version": "14.0.2.0.2",
+    "version": "14.0.2.0.3",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
     "category": "other",
