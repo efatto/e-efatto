0871598a5782d160d6273d999357bcd20467e782
sergiocorato <sergiocorato@gmail.com>
2026-05-28 08:43:03 +0200

    [FIX] install l10n_it_account_xmlid_fix from l10n_it_country_data anyway

--- l10n_it_country_data/migrations/18.0.1.0.0/pre-migration.py
+++ l10n_it_country_data/migrations/18.0.1.0.0/pre-migration.py
@@ -8,25 +8,21 @@ _logger = logging.getLogger(__name__)
 @openupgrade.migrate()
 def migrate(env, version):
     _logger.info("Running l10n_it_country_data pre-migration")
-    l10n_it_module = env["ir.module.module"].search([("name", "=", "l10n_it")])
-    if not l10n_it_module or l10n_it_module.state == "uninstalled":
-        if not l10n_it_module:
-            _logger.info("l10n_it module not found, skipping pre-migration")
-        _logger.info("l10n_it module is uninstalled, searching and installing "
-                     "l10n_it_account_xmlid_fix")
-        module_fix = env["ir.module.module"].search(
-            [("name", "=", "l10n_it_account_xmlid_fix")]
-        )
-        if module_fix.state == "uninstalled":
-            _logger.info("l10n_it_account_xmlid_fix module is uninstalled, "
-                         "attempting to install")
-            module_fix.button_immediate_install()
-            _logger.info("l10n_it_account_xmlid_fix module installed")
-        elif module_fix.state == "installed":
-            _logger.info("l10n_it_account_xmlid_fix module already installed")
-        else:
-            _logger.info(
-                "l10n_it_account_xmlid_fix not present or in unexpected state: %s",
-                module_fix.state)
+    ir_module_obj = env["ir.module.module"]
+    ir_module_obj.update_list()
+    l10n_it_module = ir_module_obj.search([("name", "=", "l10n_it")])
+    _logger.info("l10n_it module is %s", l10n_it_module.state)
+    module_fix = ir_module_obj.search(
+        [("name", "=", "l10n_it_account_xmlid_fix")]
+    )
+    if module_fix.state == "uninstalled":
+        _logger.info("l10n_it_account_xmlid_fix module is uninstalled, "
+                     "attempting to install")
+        module_fix.button_install()
+        _logger.info("l10n_it_account_xmlid_fix module installed")
+    elif module_fix.state == "installed":
+        _logger.info("l10n_it_account_xmlid_fix module already installed")
     else:
-        _logger.info("l10n_it module is %s", l10n_it_module.state)
+        _logger.info(
+            "l10n_it_account_xmlid_fix not present or in unexpected state: %s",
+            module_fix.state)
