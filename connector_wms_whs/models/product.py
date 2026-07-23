483e9534086279510e1095a9bd9ccad2c05fce16
sergiocorato <sergiocorato@gmail.com>
2026-07-23 15:37:48 +0200

    [UPD] info from product WHS tables

--- connector_wms_whs/models/product.py
+++ connector_wms_whs/models/product.py
@@ -18,26 +18,11 @@ class ProductProduct(models.Model):
         sql_result = dbsource.execute_mssql(
             sqlquery=clean_sql_text(
                 """
-SELECT * FROM HOST_ARTICOLI ha
-INNER JOIN (
-    SELECT hg.Articolo
-    FROM (
-        SELECT *, ROW_NUMBER() OVER(PARTITION BY Articolo ORDER BY id DESC) Corr
-        FROM HOST_GIACENZE
-    ) hg
-    WHERE hg.Corr = 1
-) hglast
-ON ha.Codice = hglast.Articolo
-INNER JOIN (
-    SELECT hap.id
-    FROM (
-        SELECT *, ROW_NUMBER() OVER(PARTITION BY Codice ORDER BY id DESC) Corr1
-        FROM HOST_ARTICOLI
-    ) hap
-    WHERE hap.Corr1 = 1
-) hglasta
-ON ha.id = hglasta.id
+SELECT ha.Codice, ha.Descrizione AS Descrizione, ha.id AS id_articoli, 0 AS id_giacenza, ha.Peso AS peso_articoli, 0 AS peso_giacenza, 0 AS qta FROM HOST_ARTICOLI ha
 WHERE ha.Codice=:Codice
+UNION
+SELECT hg.Articolo, '' AS Descrizione, 0 AS id_articoli, hg.id AS id_giacenza, 0 AS peso_articoli, hg.Peso AS peso_giacenza, hg.Qta AS qta FROM HOST_GIACENZE hg
+WHERE hg.Articolo=:Codice
                 """
             ),
             sqlparams={"Codice": self.default_code},
@@ -47,11 +32,11 @@ WHERE ha.Codice=:Codice
             cols = sql_result[1]
             rows_with_headers = [dict(zip(cols, row)) for row in sql_result[0]]
             contents = _(
-                "Product is going to be synchronized with WHS with record: %s."
+                "Product info in table HOST_ARTICOLI and HOST_GIACENZE: %s."
                 % str(rows_with_headers)
             )
         else:
-            contents = _("Product is going to be synchronized with WHS.")
+            contents = _("No info found for this product.")
         res["params"].update(
             {
                 "message": contents,
