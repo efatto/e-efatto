f8bd2d059b895f975d410c08f4259426213d2dc1
Sergio Corato <sergiocorato@gmail.com>
2026-09-08 09:20:21 +0200

    [MIG] purchase_stock_partner_ref: Migration to 18.0

--- purchase_stock_partner_ref/__manifest__.py
+++ purchase_stock_partner_ref/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Purchase partner ref in stock picking",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
     "category": "Products",
