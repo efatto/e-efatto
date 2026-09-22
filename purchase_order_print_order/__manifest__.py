df506cccadb338b39f2281bbcd066beba0f5ee75
Sergio Corato <sergiocorato@gmail.com>
2026-09-22 19:10:08 +0200

    [MIG] purchase_order_print_order: Migration to 18.0

--- purchase_order_print_order/__manifest__.py
+++ purchase_order_print_order/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Print Always Order",
     "summary": "Replase print RfQ button with print Order.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/efatto/e-efatto",
     "author": "Sergio Corato",
