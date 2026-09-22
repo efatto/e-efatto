7286c8d91af523b630f6f0e4085bf431902c21d2
Sergio Corato <sergiocorato@gmail.com>
2026-09-22 09:40:28 +0200

    [MIG] res_currency_change_charge: Migration to 18.0

--- res_currency_change_charge/__manifest__.py
+++ res_currency_change_charge/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Currency change charge",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
     "license": "AGPL-3",
@@ -13,7 +13,5 @@
     "data": [
         "views/res_currency.xml",
     ],
-    "summary": "Add a percentage value of currency change costs to be paid when "
-    "purchasing from supplier.",
     "installable": True,
 }
---
+++ res_currency_change_charge/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module add a percentage value of currency change costs to be paid when purchasing from a supplier.
