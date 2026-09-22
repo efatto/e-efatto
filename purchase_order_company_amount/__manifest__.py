0462c69c17969cfa5b8b15d478cf023c2ffd475a
Sergio Corato <sergiocorato@gmail.com>
2026-09-22 18:00:18 +0200

    [MIG] purchase_order_company_amount: Migration to 18.0

--- purchase_order_company_amount/__manifest__.py
+++ purchase_order_company_amount/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Order Company Amount",
     "summary": "Add company currency untaxed amount to purchase order.",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/efatto/e-efatto",
     "author": "Sergio Corato",
