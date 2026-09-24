00495313b2a6b2b9e1eb1c564744d9b725b318ba
Sergio Corato <sergiocorato@gmail.com>
2026-09-24 11:16:42 +0200

    [MIG] mrp_production_child_origin: Migration to 18.0

--- mrp_production_child_origin/__manifest__.py
+++ mrp_production_child_origin/__manifest__.py
@@ -2,10 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "MRP Child Origin",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sergio Corato",
     "website": "https://github.com/efatto/e-efatto",
-    "summary": "Extend origin in child production to ease search.",
     "license": "AGPL-3",
     "category": "Manufacture",
     "depends": [
---
+++ mrp_production_child_origin/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module extends origin in child production to ease search.
