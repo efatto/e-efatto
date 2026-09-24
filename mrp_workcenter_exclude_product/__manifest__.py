c43c9df874b0979204e250acc6565c14b6f3765d
Sergio Corato <sergiocorato@gmail.com>
2026-09-24 16:02:47 +0200

    [MIG] mrp_workcenter_exclude_product: Migration to 18.0

--- mrp_workcenter_exclude_product/__manifest__.py
+++ mrp_workcenter_exclude_product/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Workcenter Exclude Products",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Manufacturing",
     "author": "Sergio Corato",
